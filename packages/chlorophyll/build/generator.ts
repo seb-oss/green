bb21a60007ebf0883fd817e2244d892ca0be5502
Johan Öbrink <johan.obrink@gmail.com>
2021-06-10 15:10:57 +0200

    fix: 🐛 Changed scss camelCase vars to param-case

--- packages/chlorophyll/build/generator.ts
+++ packages/chlorophyll/build/generator.ts
@@ -1,13 +1,17 @@
+import { paramCase } from 'change-case'
+
 const rxShadePrefix = /^s\d{3}$/
 const handleShadePrefix = (key: string): string => {
   if (rxShadePrefix.test(key)) return key.substring(1)
   else return key
 }
 
+const parseKey = (key: string): string => paramCase(handleShadePrefix(key))
+
 export const generate = (obj: any, ...prefix: string[]): string[] => (
   Object.entries(obj)
     .flatMap(([key, value]) => {
-      const prefixes = prefix.concat(handleShadePrefix(key))
+      const prefixes = prefix.concat(parseKey(key))
 
       if (typeof value === 'object') return generate(value, ...prefixes)
       return `$${prefixes.join('-')}: ${value};`
