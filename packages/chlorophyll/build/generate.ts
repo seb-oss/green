6c1e5c4c99f2b16d7d28914ecc52c92df00950e2
Johan Öbrink <johan.obrink@gmail.com>
2021-06-10 11:57:04 +0200

    fix: 🐛 Changed target of generate to tokens

--- packages/chlorophyll/build/generate.ts
+++ packages/chlorophyll/build/generate.ts
@@ -1,10 +1,13 @@
-import { writeFile } from 'fs/promises'
+import { mkdir, writeFile } from 'fs/promises'
 import { generateScss } from './generateScss'
 import { colors } from '../src/tokens'
 
 const run = async () => {
+  const folder = './src/scss/tokens'
+  await mkdir(folder, { recursive: true })
+
   const scssColors = generateScss(colors, 'color')
-  await writeFile('./src/scss/_colors.scss', scssColors.join('\n') + '\n', 'utf-8')
+  await writeFile(`${folder}/_colors.scss`, scssColors.join('\n') + '\n', 'utf-8')
 }
 
 run()
