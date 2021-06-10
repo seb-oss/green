5695b51153a1c0eda2d02090b1381b5f16be898f
Johan Öbrink <johan.obrink@gmail.com>
2021-06-10 14:29:34 +0200

    feat: 🎸 Added index and themes to scss tokens

--- packages/chlorophyll/build/generate.ts
+++ packages/chlorophyll/build/generate.ts
@@ -1,22 +1,30 @@
 import { mkdir, writeFile } from 'fs/promises'
 import { generate } from './generator'
-import { colors, fonts, shapes, typography } from '../src/tokens'
+import { colors, fonts, shapes, themes, typography } from '../src/tokens'
 
-const generateAndSave = async (folder: string, data: any, prefix: string, filename: string): Promise<void> => {
+const generateAndSave = async (folder: string, data: any, prefix: string, filename: string): Promise<string> => {
   const scss = generate(data, prefix)
-  await writeFile(`${folder}/${filename}.scss`, scss.join('\n') + '\n', 'utf-8')
+  await writeFile(`${folder}/_${filename}.scss`, scss.join('\n') + '\n', 'utf-8')
+  return filename
+}
+
+const generateAndSaveIndex = async (folder: string, ...files: string[]): Promise<void> => {
+  const forwards = files.map((file) => `@forward '${file}';`)
+  await writeFile(`${folder}/_index.scss`, forwards.join('\n') + '\n', 'utf-8')
 }
 
 const run = async () => {
   const folder = './src/scss/tokens'
   await mkdir(folder, { recursive: true })
 
-  await Promise.all([
-    generateAndSave(folder, colors, 'color', '_colors'),
-    generateAndSave(folder, fonts, 'font', '_fonts'),
-    generateAndSave(folder, shapes, 'shape', '_shapes'),
-    generateAndSave(folder, typography, 'typography', '_typography'),
+  const files = await Promise.all([
+    generateAndSave(folder, colors, 'color', 'colors'),
+    generateAndSave(folder, fonts, 'font', 'fonts'),
+    generateAndSave(folder, shapes, 'shape', 'shapes'),
+    generateAndSave(folder, themes, 'theme', 'themes'),
+    generateAndSave(folder, typography, 'typography', 'typography'),
   ])
+  await generateAndSaveIndex(folder, ...files)
 }
 
 run()
