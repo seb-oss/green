f65fb1ee3b91e074d792842a44e81ba1be0b201b
vsjolander <vilhelm.sjolander@gmail.com>
2021-06-15 17:03:16 +0200

    feat: 🎸 Design tokens, mixins and classes for typography
    Design tokens and maps for typography that are used in mixins to create
    typography css. Also moved Chlorophyll css into hot module reloading.
    
    ✅ Closes: #45

---
+++ packages/chlorophyll-storybook/src/stories/css/typography/Typography.stories.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+    import Typography from './Typography.svelte'
+</script>
+
+<Meta
+    title="CSS/Typography"
+    component={Typography}
+/>
+
+<Template let:args>
+    <Typography {...args}/>
+</Template>
+
+
+<Story
+    name="Typography"
+/>
