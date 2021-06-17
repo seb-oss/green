fe3610d196c5d39f5987385625210616433e27d1
Johan Öbrink <johan.obrink@gmail.com>
2021-06-17 13:58:45 +0200

    feat: 🎸 Added mixins for link and button link

---
+++ packages/chlorophyll-storybook/src/stories/css/link/Link.stories.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+  import Link from './Link.svelte'
+
+</script>
+
+<Meta
+  title="CSS/Button/Stories"
+  component={Link}
+  argTypes={{
+    label: { control: 'text' },
+    type: {
+      control: { type: 'select', options: ['', 'button'] },
+    },
+  }}
+/>
+
+<Template let:args>
+  <Link {...args} />
+</Template>
+
+<Story
+  source={`<a href="#">Normal</a>`}
+  name="Normal"
+  args={{
+    type: '',
+    label: 'Normal',
+  }}
+/>
---
+++ packages/chlorophyll-storybook/src/stories/css/link/Link.svelte
@@ -0,0 +1,19 @@
+<script>
+  /**
+   * A Link Component
+   * @component
+   */
+
+  /**
+   * Link type
+   */
+  export let type = ''
+
+  /**
+   * Link contents
+   */
+  export let label = 'Link'
+
+</script>
+
+<a href="#" class={type}>{label}</a>
