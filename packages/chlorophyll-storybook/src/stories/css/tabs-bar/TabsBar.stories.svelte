4fab24b468d625ea41cfcd23bdd2780e6faa2721
Joacim Magnusson <joacim.magnusson@seb.se>
2021-06-18 12:42:13 +0200

    feat(tabs): add base styles for tabs

---
+++ packages/chlorophyll-storybook/src/stories/css/tabs-bar/TabsBar.stories.svelte
@@ -0,0 +1,21 @@
+<script>
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import TabsBar from "./TabsBar.svelte";
+</script>
+
+<Meta
+  title="CSS/TabsBar/Stories"
+  component={TabsBar}
+  argTypes={{}}
+/>
+
+
+<Template let:args>
+  <TabsBar {...args}/>
+</Template>
+
+<Story
+  source={`<div></div>`}
+  name="Default"
+  args={{}}
+/>
