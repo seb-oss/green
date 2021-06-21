424e4da87a2cd7321b44fea8c4c134da53c9aabe
Johan Öbrink <johan.obrink@gmail.com>
2021-06-21 13:46:49 +0200

    feat: 🎸 Added story for default value

--- packages/chlorophyll-storybook/src/stories/css/button/Button.stories.svelte
+++ packages/chlorophyll-storybook/src/stories/css/button/Button.stories.svelte
@@ -1,29 +1,47 @@
 <script>
-    import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
-    import Button from "./Button.svelte";
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+  import Button from './Button.svelte'
+
 </script>
 
 <Meta
-        title="CSS/Button/Stories"
-        component={Button}
-        argTypes={{
-            label: { control: "text" },
-            type: {
-              control: { type: "select", options: ["primary", "secondary"] },
-            }
-        }}
+  title="CSS/Button/Stories"
+  component={Button}
+  argTypes={{
+    label: { control: 'text' },
+    type: {
+      control: { type: 'select', options: ['primary', 'secondary'] },
+    },
+  }}
 />
 
-
 <Template let:args>
-    <Button {...args}/>
+  <Button {...args} />
 </Template>
 
 <Story
-    source={`<button class="btn btn-primary">Primary</button>`}
-    name="Primary"
-    args={{
-    type: "primary",
-    label: "Primary",
+  source={`<button>Default</button>`}
+  name="Default"
+  args={{
+    type: '',
+    label: 'Default',
+  }}
+/>
+
+<Story
+  source={`<button class="primary">Primary</button>`}
+  name="Primary"
+  args={{
+    type: 'primary',
+    label: 'Primary',
+  }}
+/>
+
+<Story
+  source={`<button class="secondary">Secondary</button>`}
+  name="Secondary"
+  args={{
+    type: 'secondary',
+    label: 'Secondary',
   }}
 />
