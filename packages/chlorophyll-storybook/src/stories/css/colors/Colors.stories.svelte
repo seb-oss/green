4f80868419f46ca344c91744a4e65730e07121fc
Robert Hjalmers <robert.hjalmers@gmail.com>
2021-06-22 16:33:02 +0200

    chore(docs): add story for colors
    closes #49

---
+++ packages/chlorophyll-storybook/src/stories/css/colors/Colors.stories.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+  import Colors from './Colors.svelte'
+
+</script>
+
+<Meta
+  title="CSS/Colors/Stories"
+  component={Colors}
+  argTypes={{
+    label: { control: 'text' },
+    type: {
+      control: { type: 'select', options: ['', 'button'] },
+    },
+  }}
+/>
+
+<Template let:args>
+  <Colors {...args} />
+</Template>
+
+<Story
+  source={`<a href="#">Normal</a>`}
+  name="Normal"
+  args={{
+    type: '',
+    label: 'Normal',
+  }}
+/>
