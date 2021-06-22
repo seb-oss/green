ee0ac2f7f6ac98d1aad366ac4221160a5f5c7997
Johan Öbrink <johan.obrink@gmail.com>
2021-06-22 10:11:00 +0200

    feat: 🎸 Simple story
    #54

---
+++ packages/chlorophyll-storybook/src/stories/css/card/Card.stories.svelte
@@ -0,0 +1,37 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+  import Card from './Card.svelte'
+
+</script>
+
+<Meta
+  title="CSS/Card/Stories"
+  component={Card}
+  argTypes={{
+    headline: { control: 'text' },
+    body: { control: 'text' },
+    buttonText: { control: 'text' },
+  }}
+/>
+
+<Template let:args>
+  <Card {...args} />
+</Template>
+
+<Story
+  source={`
+    <div class="card">
+      <h2>Headline</h2>
+      <p>
+        Lorem ipsum dolor sit amet
+      </p>
+      <a class="button primary" href="#">Go somewhere</a>
+    </div>
+  `}
+  name="Nobutton"
+  args={{
+    headline: 'Headline',
+    body: 'Lorem ipsum dolor sit amet',
+    buttonText: 'Go somewhere',
+  }}
+/>
