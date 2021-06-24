925ec04ac0959eab0a34b07d48c75a1d6a69bb3e
Johan Öbrink <johan.obrink@gmail.com>
2021-06-24 14:14:40 +0200

    Initial try

---
+++ packages/chlorophyll/src/stories/button/Button.stories.svelte
@@ -0,0 +1,40 @@
+<script>
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+  import Button from './Button.svelte'
+
+  function handleClick() {}
+</script>
+
+<Meta
+  title="Components/Buttons"
+  component={Button}
+  argTypes={{
+    type: { control: 'select', options: ['none', 'button', 'submit', 'reset'] },
+    variant: {
+      control: 'select',
+      options: ['none', 'primary', 'secondary', 'ghost'],
+    },
+  }}
+/>
+
+<Template let:args>
+  <Button {...args} on:click={handleClick} />
+</Template>
+
+<Story name="Button" args={{ label: 'Default button' }} />
+
+<Story name="Types">
+  <p>Types</p>
+
+  <Button type="button" label="Button" />
+  <Button type="submit" label="Submit" />
+  <Button type="reset" label="Reset" />
+</Story>
+
+<Story name="Variants">
+  <p>Variants</p>
+
+  <Button variant="primary" label="Primary" />
+  <Button variant="secondary" label="Secondary" />
+  <Button variant="ghost" label="Ghost" />
+</Story>
---
+++ packages/chlorophyll/src/stories/button/Button.svelte
@@ -0,0 +1,25 @@
+<script>
+  import { createEventDispatcher } from 'svelte'
+
+  export let type = ''
+  export let variant = ''
+  export let label = 'Button'
+
+  const dispatch = createEventDispatcher()
+
+  function onClick(event) {
+    dispatch('click', event)
+  }
+
+  let props = {}
+  if (type && type !== 'none') {
+    props.type = type
+  }
+  if (variant && variant !== 'none') {
+    props.class = variant
+  }
+</script>
+
+<button {...props} on:click={onClick}>
+  {label}
+</button>
