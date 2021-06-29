97bfadddc5c9c7de74219119fc6da002d178fe83
Robert Hjalmers <robert.hjalmers@gmail.com>
2021-06-29 16:26:04 +0200

    docs(close): add basic story
    ref #60

---
+++ packages/chlorophyll/src/stories/close/Close.svelte
@@ -0,0 +1,24 @@
+<script>
+  import { createEventDispatcher } from 'svelte'
+
+  /**
+   * A Close Component
+   * @component
+   */
+
+  /**
+   * Button type
+   */
+
+  const dispatch = createEventDispatcher()
+
+  function onClick(event) {
+    dispatch('click', event)
+  }
+
+
+</script>
+
+<button class='close' on:click={onClick}>
+  <span class='sr-only'>Close</span>
+</button>
