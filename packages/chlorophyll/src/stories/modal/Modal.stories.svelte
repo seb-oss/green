305858a802d8ca24e8550f001d171183a19e78eb
Robert Hjalmers <robert.hjalmers@gmail.com>
2021-06-29 10:07:12 +0200

    docs(modal): add basic story
    ref #59

---
+++ packages/chlorophyll/src/stories/modal/Modal.stories.svelte
@@ -0,0 +1,31 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+  import Alert from './Modal.svelte'
+
+</script>
+
+<Meta
+  title="Components/Modal/Stories"
+  component={Alert}
+  argTypes={{
+    content: { control: 'text' },
+    type: {
+      control: {
+        type: 'select',
+        options: ['', 'slide-out', 'take-over', 'dialog'],
+      },
+    },
+  }}
+/>
+
+<Template let:args>
+  <Alert {...args} />
+</Template>
+
+<Story
+  name="default"
+  args={{
+    type: '',
+
+  }}
+/>
---
+++ packages/chlorophyll/src/stories/modal/Modal.svelte
@@ -0,0 +1,46 @@
+<script>
+  /**
+   * Modal Component
+   * @component
+   */
+
+  /**
+   * Modal type
+   */
+  export let type = 'slide-out'
+
+  /**
+   * Modal contents
+   */
+  export let content = 'Lorem ipsum dolar sit amet modal body content.'
+
+  /**
+   * Modal heading
+   */
+  export let heading = 'Modal heading'
+
+  /**
+   * Modal footer
+   */
+  export let footer = ''
+
+  /**
+   * Modal size
+   */
+  export let size = ''
+
+
+</script>
+
+<div role='dialog' class="modal">
+  <div class='header'>
+    <h3>{ heading }</h3>
+    <button class='close'>close</button>
+  </div>
+  <div class='body'>
+    <p>{ content }</p>
+  </div>
+  <div class='footer'>
+    <button>Secondary</button><button>Primary</button>
+  </div>
+</div>
