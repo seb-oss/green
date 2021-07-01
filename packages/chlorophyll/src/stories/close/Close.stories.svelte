28c0239be2a1e16a048476cd43eb298c61ffc309
Robert Hjalmers <hjalmers@users.noreply.github.com>
2021-07-01 10:01:03 +0200

    Feature/close (#110)
    * docs(close): add basic story
    
    * feat(close): add initial version
    
    * fix: mixin for colors and contrast
    
    closes #60

---
+++ packages/chlorophyll/src/stories/close/Close.stories.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+  import Close from './Close.svelte'
+
+  function handleClick() {}
+</script>
+
+<Meta
+  title="Components/Close"
+  component={Close}
+  argTypes={{}}
+/>
+
+<Template let:args>
+  <Close {...args} on:click={handleClick} />
+</Template>
+
+<Story name="Close" args={{}} />
