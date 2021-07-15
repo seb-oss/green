99b2e5820c1e7b9d8275361266cddafc5977703e
vsjolander <vilhelm.sjolander@gmail.com>
2021-07-15 15:40:58 +0200

    feat: 🎸 Adding icons
    Adding scss and stories for icons
    
    ✅ Closes: 117

---
+++ packages/chlorophyll/src/stories/icon/Icon.stories.svelte
@@ -0,0 +1,45 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+  import Icon from './Icon.svelte'
+  import Alert from '../alert/Alert.svelte'
+
+</script>
+
+<Meta
+  title="Components/Icon/Stories"
+  component={Icon}
+  argTypes={{
+    size: {
+    control: {
+      type: 'select',
+        options: ['small', 'medium', 'large'],
+      },
+    },
+  }}
+/>
+
+<Template let:args>
+  <Icon {...args} />
+</Template>
+
+<Story
+  name="Default"
+  args={{
+    size: 'medium',
+  }}
+/>
+
+<Story
+  name="Icon in button"
+>
+  <button class="primary">Primary<Icon htmlClass="icon ms-3" /></button>
+  <br />
+  <br />
+  <button class="primary icon-only"><Icon /></button>
+</Story>
+
+<Story
+  name="Icon in alert"
+>
+  <Alert  Icon={Icon} type='success' content='Succees' heading='' button='' primaryButton='' link=''   }} />
+</Story>
