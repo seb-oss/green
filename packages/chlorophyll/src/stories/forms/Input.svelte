e61761391ab34f447c7eb20ee4fd33ee26153bb8
Robert Hjalmers <robert.hjalmers@gmail.com>
2021-07-29 07:16:54 +0200

    docs(input): add story for sizes
    #132

---
+++ packages/chlorophyll/src/stories/forms/Input.svelte
@@ -0,0 +1,24 @@
+<script>
+
+  /**
+   * A Input Component
+   * @component
+   */
+
+  /**
+   * Input type
+   */
+  export let type = ''
+  export let size = ''
+  export let label = 'Input'
+
+  let props = {}
+  if (type && type !== 'none') {
+    props.type = type
+  }
+  if (size && size !== 'none') {
+    props.class = size
+  }
+</script>
+
+<input {...props} type='text'>
