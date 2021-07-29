938d9b4edd5a0df0fcf1824a913131e8726ad4ae
Robert Hjalmers <hjalmers@users.noreply.github.com>
2021-07-29 07:59:33 +0200

    Feature/input type text (#133)
    * docs(input): add story for sizes
    
    #132
    
    * fix(utility): use important! for size modifier
    
    * fix(button): handle different sizes
    
    * fix(input): add border color and size modifier for small input
    
    closes #132
    
    * docs(input): add correct types

---
+++ packages/chlorophyll/src/stories/forms/Input.stories.svelte
@@ -0,0 +1,23 @@
+<script>
+  import { Meta, Story, Template } from '@storybook/addon-svelte-csf'
+  import Input from './Input.svelte'
+
+</script>
+
+<Meta
+  title="Components/Forms/Input"
+  component={Input}
+  argTypes={{
+    type: { control: 'select', options: ['none', 'text', 'password', 'number', 'email', 'tel', 'search', 'url'] },
+    size: {
+      control: 'select',
+      options: ['none', 'size-sm', 'size-lg'],
+    },
+  }}
+/>
+
+<Template let:args>
+  <Input {...args} />
+</Template>
+
+<Story name="Input" args={{ label: 'Default input' }} />
