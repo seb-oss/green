7dd3c240c35271a0ed0b5097c2f0b1c6e3af5b9d
Robert Hjalmers <robert.hjalmers@gmail.com>
2021-08-12 15:53:07 +0200

    test(green-angular): add simple test for button

---
+++ packages/green-angular/projects/green-angular/src/lib/button/button/button.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ButtonComponent } from './button.component'
+
+
+describe('GreenAngularComponent', () => {
+  let component: ButtonComponent;
+  let fixture: ComponentFixture<ButtonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ButtonComponent ]
+    })
+      .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
