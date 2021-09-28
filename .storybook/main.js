a11e869d737a3afd994e92277304d83e63c6bbdf
Robert Hjalmers <robert.hjalmers@gmail.com>
2021-09-28 11:29:09 +0200

    build: load fonts from npm package
    ref #209

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const CopyPlugin = require('copy-webpack-plugin');
+
 module.exports = {
   core: {
     builder: "webpack5",
@@ -17,10 +19,23 @@ module.exports = {
     },
   ],
   // uncomment the property below if you want to apply some webpack config globally
-  // webpackFinal: async (config, { configType }) => {
-  //   // Make whatever fine-grained changes you need that should apply to all storybook configs
+  webpackFinal: async (config, { configType }) => {
+    // Make whatever fine-grained changes you need that should apply to all storybook configs
 
-  //   // Return the altered config
-  //   return config;
-  // },
+    config = {
+      ...config,
+      plugins: [
+        ...config.plugins,
+        new CopyPlugin({
+          patterns: [{
+            from: './node_modules/@sebgroup/fonts/fonts', to: '../fonts'}
+          ]}
+        )
+      ]
+    }
+    console.log(config);
+
+    // Return the altered config
+    return config;
+  },
 }
