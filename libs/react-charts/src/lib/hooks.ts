698f642050b6daa507dd40d24c220224a33c7ff9
Johan Öbrink <johan.obrink@gmail.com>
2021-10-22 15:09:33 +0200

    fix: 🐛 Removed console.logs :o

--- libs/react-charts/src/lib/hooks.ts
+++ libs/react-charts/src/lib/hooks.ts
@@ -6,26 +6,20 @@ export const useChart = (ref: MutableRefObject<null>, settings: ChartSettings) =
   const [info, setInfo] = useState<ChartInfo>({})
 
   useEffect(() => {
-    console.log('ref or chart update')
     if (!chart && ref.current) {
-      console.log('create chart')
       setChart(create(ref.current, settings))
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [ref, chart])
 
   useEffect(() => {
-    console.log('settings update')
     if (!settings || !chart || chart.settings === settings) return
-    console.log('update chart')
     setChart(chart.update(settings))
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [settings])
 
   useEffect(() => {
-    console.log('info update')
     if (chart && chart.info) {
-      console.log('update info')
       setInfo(chart.info)
     }
   }, [chart, chart?.info])
