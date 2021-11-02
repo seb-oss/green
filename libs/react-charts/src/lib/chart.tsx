0d9bdf3f3231ff5875087cfd546fe2d00169bd90
Robert Hjalmers <hjalmers@users.noreply.github.com>
2021-11-02 13:36:20 +0100

    Feature/235 add styling for chart legend   top r (#255)
    * feat(chart): add basic styling for legend
    
    * fix(list): exclude .legend from base-list
    
    * style: prettier
    
    * feat(chart): add support for basic positioning of legend
    
    closes #235

--- libs/react-charts/src/lib/chart.tsx
+++ libs/react-charts/src/lib/chart.tsx
@@ -27,16 +27,17 @@ export function Chart({ settings, theme }: ChartProps) {
       )}
 
       {info?.legend?.placement !== 'none' && (
-        <div>
+        <div className={'legend-container ' + info?.legend?.placement}>
           <ul className="legend">
             {info?.legend?.items.map((i, ix) => (
               <li
+                style={{ '--color': i.color } as unknown as CSSProperties}
                 key={`legend--${ix}`}
                 onMouseOver={() => chart?.focus(i.title)}
                 onMouseOut={() => chart?.revert()}
                 onMouseUp={() => chart?.toggle(i.title)}
               >
-                {i.title}
+                <span>{i.title}</span>
               </li>
             ))}
           </ul>
