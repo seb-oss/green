3b954d397b367669ea01fa7cd5e01336bb78e41c
Robert Hjalmers <robert.hjalmers@gmail.com>
2021-11-04 21:07:07 +0100

    docs(angular-charts): add styles from charts

--- libs/angular-charts/.storybook/preview.js
+++ libs/angular-charts/.storybook/preview.js
@@ -1,4 +1,5 @@
 import { parameters as baseParameters } from '../../../.storybook/preview'
+import '!style-loader!css-loader!postcss-loader!sass-loader!./scss-loader.scss'
 
 export const parameters = {
   ...baseParameters,
