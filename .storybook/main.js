d033e058c43c4fad97888baf6b68efcce962795e
Joacim Magnusson <joacim@isogram.se>
2021-11-19 16:02:40 +0100

    chore(storybook-theme): re-add extra webpack font config

--- .storybook/main.js
+++ .storybook/main.js
@@ -49,6 +49,18 @@ module.exports = {
         ...config.module,
         rules: [
           ...config.module.rules,
+          {
+            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            use: [
+              {
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]',
+                  outputPath: '../../fonts/'
+                }
+              }
+            ]
+          }
           ]
       }
     }
