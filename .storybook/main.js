7b36c6f81a29d03e099df8180db9f21be00dd04c
Joacim Magnusson <joacim.magnusson@seb.se>
2021-11-20 14:33:54 +0100

    Storybook theme improvements (#266)
    * chore(storybook-theme): storybook SEB theme
    
    * chore(storybook-theme): add missing webpack plugin
    
    * chore(storybook-theme): re-add extra webpack font config

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 const EventHooksPlugin = require('event-hooks-webpack-plugin');
 const copyfiles = require('copyfiles');
 const fs = require('fs')
-
+const path = require('path')
 
 module.exports = {
   core: {
@@ -12,6 +12,7 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "storybook-dark-mode",
+    path.resolve("./.storybook/addons/run-in-iframe/run-in-iframe-preset.js"),
     {
       name: '@storybook/addon-postcss',
       options: {
@@ -59,7 +60,8 @@ module.exports = {
                 }
               }
             ]
-          }]
+          }
+          ]
       }
     }
 
