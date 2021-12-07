3a72e823bcd8266a485f55c2c67e37325531e3d6
Robert Hjalmers <hjalmers@users.noreply.github.com>
2021-12-07 20:58:02 +0100

    Feature/refactor paths (#283)
    * refactor(chlorophyll): update structure and use relative paths
    
    closes #281
    
    * build(charts): output scss and compiled css
    
    closes #280
    
    * chore(deps): update
    
    * chore(extract): add build and release config

---
+++ apps/angular-lib-dev/src/app/green-charts/green-charts.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core'
+import { ChartSettings } from '@sebgroup/green-charts'
+
+@Component({
+  selector: 'green-green-charts',
+  template: `<ngg-chart [settings]="settings"></ngg-chart>`,
+  styleUrls: ['./green-charts.component.css'],
+})
+export class GreenChartsComponent {
+  settings: ChartSettings = {
+    data: [
+      {
+        name: 'Antal',
+        type: 'bar',
+        values: [234, 197, 97, 12],
+      },
+    ],
+    categories: [
+      'Signerade avtal (SEB)',
+      'Påbörjade trials (Oxceed)',
+      'Aktiverade prenumerationer',
+      'Avslutade prenumerationer (endast efter avslutad ordinarie pren.)',
+    ],
+  }
+}
