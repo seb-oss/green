9a0991b1642a0b8cbc35078237275cb5ae04f5d0
Johan Öbrink <johan.obrink@gmail.com>
2021-12-15 12:06:32 +0100

    feat: 🎸 More props and auto-aria
    Moved text to `children`, added props `id`, `target`, `rel` and `role`.
    Added role shortcut `external`. Added `button` prop with possibility of
    choosing buttton variant. Setting as button also adds `role`

--- libs/react/src/lib/link/link.spec.tsx
+++ libs/react/src/lib/link/link.spec.tsx
@@ -1,15 +1,70 @@
 import { render, screen } from '@testing-library/react'
+import { notDeepEqual } from 'assert'
 import Link from './link'
 
 describe('Link', () => {
-  it('renders', () => {
-    render(<Link text="Foo" />)
+  it('renders text', () => {
+    render(<Link>Foo</Link>)
     expect(screen.getByText('Foo')).toBeVisible()
   })
-
-  it('renders', () => {
-    render(<Link text="Foo" link="test.com" />)
-    const link: HTMLAnchorElement = screen.getByText('Foo') as HTMLAnchorElement
-    expect(link.href).toBe('http://localhost/test.com')
+  it('renders id', () => {
+    render(<Link id="foo">Foo</Link>)
+    const link: HTMLAnchorElement = screen.getByText('Foo')
+    expect(link.id).toEqual('foo')
+  })
+  it('renders href', () => {
+    render(<Link href="https://test.com/">Foo</Link>)
+    const link: HTMLAnchorElement = screen.getByText('Foo')
+    expect(link.href).toEqual('https://test.com/')
+  })
+  it('renders target', () => {
+    render(<Link href="https://test.com/" target="_blank">Foo</Link>)
+    const link: HTMLAnchorElement = screen.getByText('Foo')
+    expect(link.target).toEqual('_blank')
+  })
+  it('renders non role', () => {
+    render(<Link href="https://test.com/">Foo</Link>)
+    const link: HTMLAnchorElement = screen.getByText('Foo')
+    expect(link.getAttribute('role')).not.toBeTruthy()
+  })
+  it('renders role', () => {
+    render(<Link href="https://test.com/" role="menuitem">Foo</Link>)
+    const link: HTMLAnchorElement = screen.getByText('Foo')
+    expect(link.getAttribute('role')).toEqual('menuitem')
+  })
+  it('renders boolean button prop', () => {
+    render(<Link button>Foo</Link>)
+    const link: HTMLAnchorElement = screen.getByText('Foo')
+    expect(link.className).toEqual('button')
+  })
+  it('renders named button prop', () => {
+    render(<Link button="ghost">Foo</Link>)
+    const link: HTMLAnchorElement = screen.getByText('Foo')
+    expect(link.className).toEqual('button ghost')
+  })
+  it('sets role button', () => {
+    render(<Link button>Foo</Link>)
+    const link: HTMLAnchorElement = screen.getByText('Foo')
+    expect(link.getAttribute('role')).toEqual('button')
+  })
+  it('renders rel', () => {
+    render(<Link rel="noreferrer">Foo</Link>)
+    const link: HTMLAnchorElement = screen.getByText('Foo')
+    expect(link.rel).toEqual('noreferrer')
+  })
+  it('handles external', () => {
+    render(<Link external>Foo</Link>)
+    const link: HTMLAnchorElement = screen.getByText('Foo')
+    expect(link.rel).toEqual('external')
+  })
+  it('handles rel and external', () => {
+    render(<Link rel="noreferrer" external>Foo</Link>)
+    const link: HTMLAnchorElement = screen.getByText('Foo')
+    expect(link.rel).toEqual('noreferrer external')
+  })
+  it('handles multiple rels and external', () => {
+    render(<Link rel={['alternate', 'author', 'bookmark', 'external']} external>Foo</Link>)
+    const link: HTMLAnchorElement = screen.getByText('Foo')
+    expect(link.rel).toEqual('alternate author bookmark external')
   })
 })
--- libs/react/src/lib/link/link.tsx
+++ libs/react/src/lib/link/link.tsx
@@ -1,21 +1,61 @@
-import React, { HTMLProps } from 'react'
+import React, { HTMLProps, PropsWithChildren, useEffect, useState } from 'react'
+
+type Rel = 'alternate'
+  | 'author'
+  | 'bookmark'
+  | 'external'
+  | 'help'
+  | 'license'
+  | 'next'
+  | 'nofollow'
+  | 'noopener'
+  | 'noreferrer'
+  | 'opener'
+  | 'prev'
+  | 'search'
+  | 'tag'
+
+  type Role = 'button'
+    | 'checkbox'
+    | 'menuitem'
+    | 'menuitemcheckbox'
+    | 'menuitemradio'
+    | 'option'
+    | 'radio'
+    | 'switch'
+    | 'tab'
+    | 'treeitem'
 
 interface LinkProps {
-  link?: string
-  text?: string
+  id?: string
+  href?: string
+  target?: '_self' | '_blank' | '_parent' | '_top' | string
+  button?: boolean | 'primary' | 'secondary' | 'ghost'
+  rel?: Rel | Rel[]
+  role?: Role
+  external?: boolean
 }
 
-export const Link = ({ link, text }: LinkProps) => {
-  const props: HTMLProps<HTMLAnchorElement> = {}
-  if (link) props.href = link
-
-  return (
-    <div>
-      <a href={link} rel="noreferrer">
-        {text}
-      </a>
-    </div>
-  )
+export const Link = ({ href, target, button, rel, role, external, id, children }: PropsWithChildren<LinkProps>) => {
+  const [props, setProps] = useState<HTMLProps<HTMLAnchorElement>>({})
+  useEffect(() => {
+    const className = (button)
+      ? ((typeof button === 'string') ? `button ${button}` : 'button')
+      : undefined
+    const rels = (rel) ? ((rel instanceof Array) ? rel : [rel]) : []
+    if (external && !rels.includes('external')) rels.push('external')
+    
+    const newProps: HTMLProps<HTMLAnchorElement> = {
+      role: role || ((button) ? 'button' : undefined),
+      rel: rels.join(' ') || undefined,
+      className,
+      href,
+      id,
+      target,
+    }
+    setProps(newProps)
+  }, [href, target, button, rel, external, id])
+  return <a {...props}>{ children }</a>
 }
 
 export default Link
