fef857f88a7743d3fa20743aad112e82a8024588
Ethan Quek <ethan.qsh@gmail.com>
2021-12-28 14:29:09 +0800

    refactor: 💡 replace html anchor with link component

--- libs/react/src/lib/navbar/navbar.tsx
+++ libs/react/src/lib/navbar/navbar.tsx
@@ -1,7 +1,8 @@
 import { NavbarVariant } from '@sebgroup/extract'
+import Link from '../link/link'
 import { HTMLAttributes, ReactNode, useEffect, useState } from 'react'
 
-interface NavProps {
+interface NavProps extends HTMLAttributes<HTMLElement> {
   title?: string
   variant?: NavbarVariant
   children?: ReactNode
@@ -14,22 +15,29 @@ export const Navbar = ({ children, variant, title }: NavProps) => {
     const classNames: string[] = []
     if (variant) classNames.push(variant)
     setProps({ ...props, className: classNames.join(' ') })
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [variant])
 
   return (
     <nav role="navigation" {...props}>
-      <a
+      <Link
         aria-label="Open seb.se in new tab"
         href="http://www.seb.se"
         target="_blank"
         className="brand"
-        rel="noreferrer"
-      >
-        {''}
-      </a>
-      <h1 className="mx-4">{title}</h1>
-      {children}
+      />
+      <div className="container-fluid">
+        <div className="row justify-content-between align-items-center">
+          <div className="col-auto">
+            <Link className="mx-4">
+              <h1>{title}</h1>
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="col-auto">
+        <div className="group size-sm">{children}</div>
+      </div>
     </nav>
   )
 }
