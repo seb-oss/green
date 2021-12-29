f2fadb829f6314a1e9b0d3e0d3c13b45014edb2b
Ethan Quek <ethan.qsh@gmail.com>
2021-12-29 10:36:49 +0800

    feat: 🎸 Added table list

--- libs/react/src/lib/list/list.tsx
+++ libs/react/src/lib/list/list.tsx
@@ -5,10 +5,16 @@ import ListItem from './listItem'
 interface ListProps
   extends HTMLAttributes<HTMLOListElement | HTMLUListElement> {
   listType?: ListType
+  tableCaption?: string
   children?: ReactNode[]
 }
 
-export const List = ({ listType, children, ...props }: ListProps) => {
+export const List = ({
+  listType,
+  tableCaption,
+  children,
+  ...props
+}: ListProps) => {
   if (listType === 'ordered') {
     return (
       <ol {...props}>
@@ -17,13 +23,26 @@ export const List = ({ listType, children, ...props }: ListProps) => {
         ))}
       </ol>
     )
-  } else {
+  } else if (listType !== 'table') {
     return (
       <ul {...props} className={listType}>
         {children?.map((child, index) => (
           <ListItem key={index}>{child}</ListItem>
         ))}
       </ul>
+    )
+  } else {
+    return (
+      <figure>
+        <figcaption className="table-list-caption">{tableCaption}</figcaption>
+        <dl>
+          {children?.map((child, index) => (
+            <ListItem listType={listType} key={index}>
+              {child}
+            </ListItem>
+          ))}
+        </dl>
+      </figure>
     )
   }
 }
--- libs/react/src/lib/list/listItem.tsx
+++ libs/react/src/lib/list/listItem.tsx
@@ -1,11 +1,17 @@
+import { ListType } from '@sebgroup/extract'
 import React, { HTMLAttributes, ReactNode } from 'react'
 
 interface ListItemProps extends HTMLAttributes<HTMLLIElement> {
-  children: ReactNode
+  listType?: ListType
+  children?: ReactNode
 }
 
-const ListItem = ({ children, ...props }: ListItemProps) => {
-  return <li {...props}>{children}</li>
+const ListItem = ({ listType, children, ...props }: ListItemProps) => {
+  if (listType === 'table') {
+    return <div>{children}</div>
+  } else {
+    return <li {...props}>{children}</li>
+  }
 }
 
 export default ListItem
