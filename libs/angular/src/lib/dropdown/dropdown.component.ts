6e15c725279dd5c83754028a65393c8b913012be
Maximilian Rågmo <max.ragmo@outlook.com>
2022-01-11 16:28:12 +0100

    refactor(dropdown): extract implementation

--- libs/angular/src/lib/dropdown/dropdown.component.ts
+++ libs/angular/src/lib/dropdown/dropdown.component.ts
@@ -1,24 +1,68 @@
-import { Component, Input } from '@angular/core'
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core'
 import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms'
+import {
+  createDropdown,
+  AbstractDropdown,
+  DropdownHandler,
+  DropdownOption,
+  ExtendedDropdownOption,
+  ElementProps,
+} from '@sebgroup/extract'
 
 @Component({
   selector: 'ngg-dropdown',
   template: `
-    <div nggPopover [config]="config">
-      <button type="button" nggPopoverTrigger>
-        {{ value }}
+    <div>
+      <button
+        #togglerRef
+        [id]="toggler?.attributes?.id"
+        [attr.role]="toggler?.attributes?.role"
+        [attr.aria-haspopup]="toggler?.attributes?.['aria-haspopup']"
+        [attr.aria-expanded]="toggler?.attributes?.['aria-expanded']"
+        [attr.aria-owns]="toggler?.attributes?.id"
+        [tabindex]="toggler?.attributes?.tabIndex"
+        [style]="toggler?.attributes?.style"
+        [class]="toggler?.classes"
+        (click)="handler?.toggle()"
+      >
+        {{ dropdown?.text }}
       </button>
-      <div nggPopoverElement tabindex="-1" autofocus>
-        <button class="close m-4 m-sm-2 d-block d-sm-none ">
+      <div
+        #listboxRef
+        [id]="listbox?.attributes?.id"
+        [attr.role]="listbox?.attributes?.role"
+        [attr.aria-activedescendant]="
+          listbox?.attributes?.['aria-activedescendant']
+        "
+        [tabindex]="listbox?.attributes?.tabIndex"
+        [style]="listbox?.attributes?.style"
+        [class]="listbox?.classes"
+      >
+        <button
+          class="close m-4 m-sm-2 d-block d-sm-none"
+          (click)="handler?.close()"
+        >
           <span class="sr-only">Close</span>
         </button>
         <ul role="listbox">
           <li
-            *ngFor="let option of options; let i = index"
-            [nggPopoverOption]="option.value"
-            [index]="i"
-            [selected]="value === option.value"
-            (selectedChanged)="select($event)"
+            *ngFor="let option of dropdown?.options"
+            [id]="option.attributes.id"
+            [attr.role]="option.attributes.role"
+            [attr.aria-selected]="option.attributes['aria-selected']"
+            [style]="option.attributes.style"
+            [class]="option.classes"
+            (click)="select(option)"
           >
             {{ option.key }}
           </li>
@@ -33,33 +77,80 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms'
       multi: true,
     },
   ],
-  //changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class NggDropdownComponent implements ControlValueAccessor {
-  get options(): Array<any> {
-    return this._options
-  }
+export class NggDropdownComponent
+  implements ControlValueAccessor, AfterViewInit, OnDestroy, OnChanges
+{
+  @Input() id?: string
+  @Input() text?: string
+  @Input() loop?: boolean
+  @Input() options: DropdownOption[] = []
 
-  @Input() set options(value: Array<any>) {
-    this._options = value
-  }
-  get config(): any {
-    return this._config
-  }
-  @Input() set config(value: any) {
-    this._config = value
-  }
+  @ViewChild('togglerRef') public togglerRef:
+    | ElementRef<HTMLElement>
+    | undefined
 
-  value: any
+  @ViewChild('listboxRef') public listboxRef:
+    | ElementRef<HTMLElement>
+    | undefined
+
+  value: any = undefined
   onChangeFn: any
   onTouchedFn: any
 
-  private _options: Array<any> = []
+  dropdown?: AbstractDropdown
+  handler?: DropdownHandler
+  toggler?: Partial<ElementProps>
+  listbox?: Partial<ElementProps>
 
-  private _config?: any = {}
+  constructor(private cd: ChangeDetectorRef) {}
+
+  ngAfterViewInit(): void {
+    if (
+      !this.handler &&
+      this.togglerRef?.nativeElement &&
+      this.listboxRef?.nativeElement
+    ) {
+      this.handler = createDropdown(
+        {
+          id: this.id,
+          text: this.text,
+          options: this.options,
+          loop: this.loop,
+        },
+        this.togglerRef.nativeElement,
+        this.listboxRef.nativeElement,
+        (dropdown) => {
+          this.dropdown = dropdown
+          this.toggler = dropdown.elements.toggler
+          this.listbox = dropdown.elements.listbox
+          this.cd.detectChanges()
+        }
+      )
+
+      this.selectFromValue(this.value)
+    }
+  }
+
+  ngOnDestroy(): void {
+    this.handler?.destroy()
+  }
+
+  ngOnChanges(): void {
+    if (!this.dropdown) return
+    this.handler?.update({
+      id: this.id,
+      text: this.text,
+      options: this.options,
+      loop: this.loop,
+    })
+    this.cd.detectChanges()
+  }
 
   writeValue(obj: any): void {
     this.value = obj
+    this.selectFromValue(this.value)
   }
 
   registerOnChange(fn: any): void {
@@ -70,8 +161,17 @@ export class NggDropdownComponent implements ControlValueAccessor {
     this.onTouchedFn = fn
   }
 
-  select(value: any) {
-    this.onChangeFn(value)
-    this.value = value
+  select(option: ExtendedDropdownOption) {
+    this.handler?.select(option)
+    this.onChangeFn(option.value)
+  }
+
+  private selectFromValue(value: any) {
+    if (this.handler && value !== undefined) {
+      const option = this.handler.dropdown.options.find(
+        (option) => option.value === value
+      )
+      if (option) this.handler?.select(option)
+    }
   }
 }
