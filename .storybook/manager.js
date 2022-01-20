f6905cbcf1bfe05852f0de1ce6636b3f93b6094c
Robert Hjalmers <robert.hjalmers@gmail.com>
2022-01-20 15:41:34 +0100

    docs(storybook): refactor styling and assets loading
    closes #353

---
+++ .storybook/manager.js
@@ -0,0 +1,2 @@
+// import scss using scss-loader file
+import '!style-loader!css-loader!postcss-loader!sass-loader!./manager.scss'
--- libs/angular/.storybook/preview.js
+++ libs/angular/.storybook/preview.js
@@ -5,7 +5,7 @@ export const parameters = {
 }
 
 // workaround for https://github.com/nrwl/nx/issues/7054
-import GlobalStyles from '!postcss-loader!sass-loader!./scss-loader.scss'
+import GlobalStyles from '!postcss-loader!sass-loader!./preview.scss'
 const storybookStyles = document.createElement('style')
 storybookStyles.innerHTML = GlobalStyles //'body {background: red;}'
 document.body.appendChild(storybookStyles)
