1475887dbc5eb058bee5d94b02ebe441eed57baf
Robert Hjalmers <robert.hjalmers@gmail.com>
2022-01-20 22:10:39 +0100

    docs(storybook): add stories for mixing frameworks

--- apps/angular-handle-legacy/src/app/green-charts/green-charts.component.ts
+++ apps/angular-handle-legacy/src/app/green-charts/green-charts.component.ts
@@ -4,7 +4,9 @@ import { ChartSettings } from '@sebgroup/green-charts'
 @Component({
   selector: 'green-green-charts',
   template: `<h3 class="my-3">Green chart</h3>
-    <div class="use-green"><ngg-chart [settings]="settings"></ngg-chart></div>
+    <div class="use-green">
+      <ngg-chart [settings]="settings"></ngg-chart>
+    </div>
     <button type="button" class="btn btn-primary">Bootstrap primary</button>
     <div class="use-green mt-3">
       <button type="button">Green default button</button>
