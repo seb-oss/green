598f749b2ec6ea9a6580bf9351bffd939b2d85c4
Terrance Prasath <92840841+terrance456@users.noreply.github.com>
2022-01-21 21:21:44 +0800

    refactor: 💡 Refactored card component (#351)
    * refactor: 💡 card storybook
    
    * refactor: 💡 Refactored card component
    
    Changed the card props and the markup for the card component, added unit
    test
    
    * refactor: 💡 refactored story
    
    Co-authored-by: Ethan Quek <ethan.qsh@gmail.com>

--- libs/react/src/lib/card/card.spec.tsx
+++ libs/react/src/lib/card/card.spec.tsx
@@ -1,15 +1,29 @@
 import { render, screen } from '@testing-library/react'
 import Card from './card'
-
+import Button from '../form/button/button'
 describe('Card', () => {
+  const Header = () => <h3>Card Headline</h3>
+
+  const Footer = () => (
+    <>
+      <Button>Default button</Button>
+      <Button variant="primary">Primary</Button>
+    </>
+  )
   it('renders', () => {
     render(<Card>Hello</Card>)
-
     expect(screen.getByText('Hello')).toBeVisible()
   })
-  it('renders headline', () => {
-    render(<Card headline="Headline">Hello</Card>)
+  it('renders header, content and footer', () => {
+    render(
+      <Card header={<Header />} footer={<Footer />}>
+        Hello
+      </Card>
+    )
 
-    expect(screen.getByRole('heading')).toHaveTextContent('Headline')
+    expect(screen.getByRole('heading')).toHaveTextContent('Card Headline')
+    expect(screen.getByText('Hello')).toBeInTheDocument()
+    expect(screen.getAllByRole('button')[0]).toHaveTextContent('Default button')
+    expect(screen.getAllByRole('button')[1]).toHaveTextContent('Primary')
   })
 })
