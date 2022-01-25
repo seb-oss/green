6cdee7140c3fbad218e8c60793a03d4d5db90e2e
Ethan Quek <62468447+ethan-qsh@users.noreply.github.com>
2022-01-25 21:42:32 +0800

    refactor: 💡 alert component storybook (#337)
    * refactor: 💡 alert component storybook
    
    * refactor: 💡 refactor alert component to use new markup

--- libs/react/src/lib/alert/alert.spec.tsx
+++ libs/react/src/lib/alert/alert.spec.tsx
@@ -1,7 +1,12 @@
 import { render, screen } from '@testing-library/react'
 import Alert from './alert'
+import Button from '../form/button/button'
 
 describe('Alert', () => {
+  const Header = () => <h3>Card Headline</h3>
+
+  const Footer = () => <Button>Default Button</Button>
+
   it('renders', () => {
     render(<Alert type="info">Alert</Alert>)
 
@@ -19,11 +24,21 @@ describe('Alert', () => {
   })
   it('renders heading', () => {
     render(
-      <Alert type="info" heading="Heading">
+      <Alert type="info" header={<Header />}>
         Alert
       </Alert>
     )
 
-    expect(screen.getByRole('heading')).toHaveTextContent('Heading')
+    expect(screen.getByRole('heading')).toHaveTextContent('Card Headline')
+  })
+
+  it('renders footer', () => {
+    render(
+      <Alert type="info" footer={<Footer />}>
+        Alert
+      </Alert>
+    )
+
+    expect(screen.getByRole('contentinfo')).toHaveTextContent('Default Button')
   })
 })
