d3d7a87309f1b83336f0c1f970481eba2b4a9456
Hjalmers <opensource@rhj.se>
2022-01-27 12:18:33 +0100

    docs(storybook): add global assets folder

--- .storybook/main.js
+++ .storybook/main.js
@@ -22,7 +22,7 @@ module.exports = {
       },
     },
   ],
-  staticDirs: ['../../../dist/fonts'],
+  staticDirs: ['../../../dist/fonts', '../../../.storybook/assets'],
   // uncomment the property below if you want to apply some webpack config globally
   webpackFinal: async (config, { configType }) => {
     // Make whatever fine-grained changes you need that should apply to all storybook configs
