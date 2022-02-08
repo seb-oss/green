a672c4575b08b61dcc811c77087b763b07c5d07d
Terrance Prasath <92840841+terrance456@users.noreply.github.com>
2022-02-08 15:43:13 +0800

    feat: 🎸 Added documentation for react install (#391)

--- libs/react/.storybook/main.js
+++ libs/react/.storybook/main.js
@@ -7,6 +7,7 @@ module.exports = {
   stories: [
     ...rootMain.stories,
     '../src/lib/**/*.stories.mdx',
+    '../src/introduction/**/*.stories.mdx',
     '../src/lib/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   addons: [...rootMain.addons, '@nrwl/react/plugins/storybook'],
