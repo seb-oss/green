173928044eac79493ac2374b7f85d59c210fcde1
Maximilian Rågmo <max.ragmo@outlook.com>
2022-02-09 18:03:28 +0100

    docs(dropdown): update

--- apps/angular-lib-dev/src/app/dropdown/dropdown.component.ts
+++ apps/angular-lib-dev/src/app/dropdown/dropdown.component.ts
@@ -6,8 +6,6 @@ import { FormBuilder } from '@angular/forms'
   templateUrl: './dropdown.component.html',
 })
 export class DropdownComponent {
-  templateDropdown = 'tacos'
-
   options: Array<{ key: string; value: string }> = [
     {
       key: 'Tacos',
@@ -24,9 +22,14 @@ export class DropdownComponent {
   ]
 
   dropdownForm = this._fb.group({
-    select1: 'sushi',
-    select2: undefined,
+    select1: undefined,
+    select2: 'pizza',
   })
+
+  dropdownValue1: any = undefined
+  dropdownValue2 = 'sushi'
+  dropdownValue3: any = undefined
+  dropdownValue4 = 'tacos'
 
   constructor(private _fb: FormBuilder) {}
 }
