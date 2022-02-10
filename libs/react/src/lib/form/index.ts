0a5846d992c2c039d053f3cdee8c9859b0693cb1
Johan Öbrink <johan.obrink@gmail.com>
2022-02-10 17:13:04 +0100

    refactor: 💡 Moved group to form folder and added form-group
    Also: An input without info and description now renders naked

--- libs/react/src/lib/form/index.ts
+++ libs/react/src/lib/form/index.ts
@@ -1,5 +1,6 @@
 export * from './button/button'
 export * from './buttonGroup/buttonGroup'
 export * from './form'
+export * from './group/group'
 export * from './input/input'
 export * from './text/text'
