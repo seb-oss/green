27727c31dcbddcac0ff40469d66e5ebd1c2cfa76
Hjalmers <opensource@rhj.se>
2022-02-15 07:44:01 +0100

    docs(angular-charts): update chart example

--- apps/angular-lib-dev/src/app/green-charts/green-charts.component.ts
+++ apps/angular-lib-dev/src/app/green-charts/green-charts.component.ts
@@ -3,17 +3,26 @@ import { ChartSettings } from '@sebgroup/green-charts'
 
 @Component({
   selector: 'green-green-charts',
-  template: `<ngg-chart [settings]="settings"></ngg-chart>`,
+  template: ` <div class="card d-block" style="height: 270px;">
+    <ngg-chart [settings]="settings"></ngg-chart>
+  </div>`,
 })
 export class GreenChartsComponent {
   settings: ChartSettings = {
+    fitHeightToParent: true,
     data: [
       {
         name: 'Antal',
         type: 'bar',
-        values: [234, 197, 97, 12],
+        values: [234, 197, 97, 142],
+      },
+      {
+        name: 'Total',
+        type: 'bar',
+        values: [24, 17, 7, 14],
       },
     ],
+    legend: 'right',
     categories: [
       'Signerade avtal (SEB)',
       'Påbörjade trials (Oxceed)',
