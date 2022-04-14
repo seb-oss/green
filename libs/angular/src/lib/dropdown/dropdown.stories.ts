10c5a09354f2f0a02a58cf44b66fc339b4535a4a
Hjalmers <opensource@rhj.se>
2022-04-14 18:09:37 +0200

    docs(dropdown): update story

--- libs/angular/src/lib/dropdown/dropdown.stories.ts
+++ libs/angular/src/lib/dropdown/dropdown.stories.ts
@@ -1,9 +1,18 @@
 // Dropdown.stories.ts
 import DropdownDocumentation from './Dropdown-Documentation.mdx'
 import { moduleMetadata, Story, Meta } from '@storybook/angular'
-import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms'
 import { NggDropdownModule } from './dropdown.module'
 import { NggDropdownComponent } from './dropdown.component'
+import { DropdownComponent } from '../../../../../apps/angular-lib-dev/src/app/dropdown/dropdown.component'
+import { of } from 'rxjs'
+import { delay } from 'rxjs/operators'
 
 export default {
   title: 'Components/Dropdown',
@@ -37,3 +46,139 @@ Select.args = {
     { key: 'Sushi', value: 'sushi' },
   ],
 }
+
+const FormControlTemplate: Story<DropdownComponent> = (
+  args: DropdownComponent
+) => {
+  const validationForm = new FormGroup({
+    country: new FormControl(undefined, [Validators.required]),
+  })
+
+  const options$ = of([
+    {
+      key: 'Sweden',
+      value: 'sweden',
+    },
+    {
+      key: 'Denmark',
+      value: 'denmark',
+    },
+    {
+      key: 'Finland',
+      value: 'Finland',
+    },
+    {
+      key: 'Norway',
+      value: 'norway',
+    },
+    {
+      key: 'England',
+      value: 'england',
+    },
+    {
+      key: 'Germany',
+      value: 'germany',
+    },
+    {
+      key: 'Estonia',
+      value: 'estonia',
+    },
+    {
+      key: 'Lithuania',
+      value: 'lithuania',
+    },
+    {
+      key: 'Belarus',
+      value: 'belarus',
+    },
+    {
+      key: 'Latvia',
+      value: 'latvia',
+    },
+    {
+      key: 'Greece',
+      value: 'greece',
+    },
+    {
+      key: 'Italy',
+      value: 'italy',
+    },
+    {
+      key: 'Austria',
+      value: 'austria',
+    },
+    {
+      key: 'Switzerland',
+      value: 'switzerland',
+    },
+    {
+      key: 'Netherlands',
+      value: 'netherlands',
+    },
+    {
+      key: 'Belgium',
+      value: 'belgium',
+    },
+    {
+      key: 'France',
+      value: 'france',
+    },
+    {
+      key: 'Spain',
+      value: 'spain',
+    },
+    {
+      key: 'Portugal',
+      value: 'portugal',
+    },
+    {
+      key: 'Poland',
+      value: 'poland',
+    },
+  ]).pipe(delay(3000))
+
+  const save = (form: any) => {
+    console.log('Saved!', form)
+  }
+
+  return {
+    component: DropdownComponent,
+    template: `
+      <form [formGroup]="validationForm" #ngForm="ngForm" (submit)="save(validationForm.value)">
+  <div class="form-group" *ngIf="validationForm.get('country') as dropdown">
+    <ngg-dropdown
+      label="Country"
+      [options]="options$ | async"
+      formControlName="country"
+      text="Select"
+      [valid]="dropdown.valid && ngForm.submitted"
+      [invalid]="dropdown.invalid && ngForm.submitted"
+    >
+      <!-- Hint text when not submitted -->
+      <ng-container data-form-info *ngIf="!ngForm['submitted']"
+        >Select country</ng-container
+      >
+      <ng-container data-form-info *ngIf="ngForm['submitted']">
+        <!-- Text when form control contains one or more errors -->
+        <ng-container *ngIf="dropdown.errors as errors">
+          <!-- Text for each error (only one will be displayed at a time) -->
+          <ng-container *ngIf="errors['required']">Select country</ng-container>
+        </ng-container>
+      </ng-container>
+    </ngg-dropdown>
+  </div>
+  <button type="submit" [disabled]="ngForm?.submitted && validationForm.invalid">
+    Save
+  </button>
+    `,
+    props: {
+      ...args,
+      validationForm,
+      options$,
+      save,
+    },
+  }
+}
+
+export const Form = FormControlTemplate.bind({})
+Form.args = {}
