663c5cfd57203d9186d6a0dc12cd5bdc5fd17dc3
Hjalmers <opensource@rhj.se>
2022-05-05 12:45:10 +0200

    docs(angular): badge - update story

--- libs/angular/src/lib/badge/badge.stories.ts
+++ libs/angular/src/lib/badge/badge.stories.ts
@@ -12,6 +12,12 @@ export default {
       imports: [NggBadgeModule],
     }),
   ],
+  argTypes: {
+    badgeType: {
+      options: ['info', 'success', 'warning', 'danger', 'light', 'dark', null],
+      control: 'radio',
+    },
+  },
   parameters: {
     docs: {
       page: Documentation,
@@ -21,20 +27,20 @@ export default {
 
 const Template: Story<NggBadgeComponent> = (args: NggBadgeComponent) => ({
   template: `
-    <ngg-badge [isCloseable]="isCloseable">Default</ngg-badge>
-    <ngg-badge badgeType="info" [isCloseable]="isCloseable">Information</ngg-badge>
-    <ngg-badge badgeType="success" [isCloseable]="isCloseable">Success</ngg-badge>
-    <ngg-badge badgeType="warning" [isCloseable]="isCloseable">Warning</ngg-badge>
-    <ngg-badge badgeType="danger" [isCloseable]="isCloseable">Danger</ngg-badge>
-    <ngg-badge badgeType="light" [isCloseable]="isCloseable">Light</ngg-badge>
-    <ngg-badge badgeType="dark" [isCloseable]="isCloseable">Dark</ngg-badge>
+    <span ngg-badge [badgeType]="badgeType" [isCloseable]="isCloseable">{{text}}</span>
     `,
   props: args,
 })
 
-export const Types = Template.bind({})
-
+export const Default = Template.bind({})
+Default.args = {
+  isCloseable: undefined,
+  badgeType: undefined,
+  text: 'Badge',
+}
 export const Dismissible = Template.bind({})
 Dismissible.args = {
   isCloseable: true,
+  badgeType: 'success',
+  text: 'Badge',
 }
