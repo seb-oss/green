f990055afacf6ff22b67daf120ddb5c238ec97d7
sjkayle <kayle.sanjuan@gmail.com>
2022-05-05 17:25:38 +0800

    feat: 🎸 new card component for angular
    ✅ Closes: #490

--- libs/angular/src/lib/green-angular.module.ts
+++ libs/angular/src/lib/green-angular.module.ts
@@ -3,10 +3,16 @@ import { CommonModule } from '@angular/common'
 import { NggSegmentedControlModule } from './segmented-control/segmented-control.module'
 import { NggDropdownModule } from './dropdown/dropdown.module'
 import { NggDatepickerModule } from './datepicker/datepicker.module'
+import { NggCardModule } from './card/card.module'
 
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  exports: [NggSegmentedControlModule, NggDropdownModule, NggDatepickerModule],
+  exports: [
+    NggSegmentedControlModule,
+    NggDropdownModule,
+    NggDatepickerModule,
+    NggCardModule,
+  ],
 })
 export class NggModule {}
