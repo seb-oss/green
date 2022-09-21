480d70f08ee43cc8ccd2897f70276588d563ef78
Daniel Eriksson <daniel.z.eriksson@seb.se>
2022-09-21 16:34:08 +0200

    feat: Button (React) - add disabled property

--- libs/react/src/lib/form/button/button.tsx
+++ libs/react/src/lib/form/button/button.tsx
@@ -8,12 +8,14 @@ export interface ButtonProps {
   variant?: ButtonVariant
   active?: boolean
   onClick?: MouseEventHandler<HTMLButtonElement>
+  disabled?: boolean
 }
 
 export function Button({
   children,
   variant,
   onClick,
+  disabled,
   active = false,
   type = 'button',
 }: ButtonProps) {
@@ -21,9 +23,10 @@ export function Button({
   const classNames: string[] = []
   if (variant) classNames.push(variant)
   if (active) classNames.push('active')
-
+ 
   if (classNames.length) props.className = classNames.join(' ')
   if (onClick) props.onClick = onClick
+  if (disabled) props.disabled = disabled
 
   return <button {...props}>{children}</button>
 }
