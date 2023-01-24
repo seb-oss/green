1f31705235e7edcc63a35cd16018c881d72323a0
Vilhelm Sjölander <vilhelm.sjolander@gmail.com>
2023-01-24 13:50:58 +0100

    refactor: 💡 AlertRibbon
    Refactoring the Alert component so it's named AlertRibbon. Keeping Alert
    as an exported constant to be backwards compatible. Moving to style this
    component based on the class name "alert ribbon" rather than using the
    role attribute due to that we need to have the flexiblity to change the
    role to "status" or "dialog". Also hiding the icon with aria-hidden
    since it's just decorative and the alert message is the main focus.
    Changed header not to fall back to h3 but rather span with header class
    for styling. Added onClose event. Fixed test after component change
    since they broke. Also removing the i element from icons since they
    should only contain the svg icon and not the i element.
    
    BREAKING CHANGE: 🧨 Renaming Alert export to AlertRibbon, changing styling form
    [role="alert"] to .alert-ribbon
    
    ✅ Closes: 659

--- libs/react/src/index.ts
+++ libs/react/src/index.ts
@@ -1,7 +1,7 @@
 export * from './lib/modal/modal'
 export * from './lib/layout'
 export * from './lib/card/card'
-export * from './lib/alert/alert'
+export * from './lib/alert-ribbon/alert-ribbon'
 export * from './lib/form'
 export * from './lib/list/list'
 export * from './lib/navbar/navbar'
@@ -10,3 +10,6 @@ export * from './lib/badge/badge'
 export * from './lib/stepper/stepper'
 export * from './lib/dropdown/dropdown'
 export * from './lib/datepicker/datepicker'
+
+// Backwards compatibility export
+export { AlertRibbon as Alert } from './lib/alert-ribbon/alert-ribbon';
