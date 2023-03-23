3282cb0f883118e2a292695a8030cf5171148617
astrit <am@astrit.co>
2023-03-23 21:58:47 +0100

    feat: 🎸 Remove the duplicate ripple file
    ✅ Closes: #802

--- libs/chlorophyll/scss/components/chips/ripple.js
+++
@@ -1,9 +0,0 @@
-export const handleRippleEffect = (e) => {
-  const rect = e.target.getBoundingClientRect()
-  e.target.style.setProperty('--gds-ripple-top', `${e.clientY - rect.top}px`)
-  e.target.style.setProperty('--gds-ripple-left', `${e.clientX - rect.left}px`)
-  e.target.classList.remove('gds-ripple-effect')
-  setTimeout(function () {
-    e.target.classList.add('gds-ripple-effect')
-  }, 20)
-}
