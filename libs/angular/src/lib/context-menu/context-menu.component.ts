a2d7f51b25a2173a31604e8d2f8ce9d7fd3ecf87
Skalskis <S8502B@corp1.ad1.seb.net>
2023-04-19 16:54:09 +0300

    feat: 🎸 Context menu v0.2
    Adding correct css
    
    BREAKING CHANGE: 🧨 -
    
    ✅ Closes: -

--- libs/angular/src/lib/context-menu/context-menu.component.ts
+++ libs/angular/src/lib/context-menu/context-menu.component.ts
@@ -1,43 +1,88 @@
-import { Component, ElementRef, EventEmitter, Input, Output, TemplateRef } from '@angular/core';
-import { DropdownOption } from '@sebgroup/extract';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Input,
+  Output,
+  TemplateRef,
+  ViewChild,
+} from '@angular/core'
+import { DropdownOption } from '@sebgroup/extract'
 
 @Component({
   selector: 'ngg-context-menu',
-  templateUrl: './context-menu.component.html'
+  templateUrl: './context-menu.component.html',
 })
 export class NggContextMenuComponent {
-  @Input() menuItems: DropdownOption[] = [];
-  @Input() menuItemTemplate: TemplateRef<any> | null = null;
+  @Input() menuItems: DropdownOption[] = []
+  @Input() menuItemTemplate: TemplateRef<any> | null = null
 
-  @Output() contextMenuItemClicked: EventEmitter<DropdownOption> = new EventEmitter<DropdownOption>();
+  @Output() contextMenuItemClicked: EventEmitter<DropdownOption> =
+    new EventEmitter<DropdownOption>()
 
-  isOpen = false;
-  top = '0px';
-  left = '0px';
+  @ViewChild('contextMenuPopover', { static: false }) popover:
+    | ElementRef
+    | undefined
+  isActive = false
+  top = '0px'
+  left = '0px'
 
   constructor(private elementRef: ElementRef) {}
 
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent): void {
+    if (!this.isActive) {
+      return
+    }
+    const targetElement = event.target as HTMLElement
+    const contextMenuElement = this.elementRef.nativeElement as HTMLElement
+    if (!contextMenuElement.contains(targetElement)) {
+      this.close()
+    }
+  }
+
   open(event: MouseEvent): void {
-    event.preventDefault();
-    this.top = `${event.clientY}px`;
-    this.left = `${event.clientX}px`;
-    this.isOpen = true;
+    const button = event.target as HTMLElement
+    const popover = this.popover?.nativeElement as HTMLElement
+
+    const buttonRect = button.getBoundingClientRect()
+    console.log(popover?.offsetWidth)
+    const popoverWidth = popover?.offsetWidth || 0
+    const screenRight = window.innerWidth
+    const buttonRight = buttonRect.left + buttonRect.width
+
+    const left = buttonRight
+    // buttonRight + popoverWidth <= screenRight
+    //   ? buttonRight
+    //   : buttonRect.left - popoverWidth
+    const top = buttonRect.top + window.pageYOffset
+
+    this.left = `${left}px`
+    this.top = `${top}px`
+    this.isActive = true
   }
 
   close(): void {
-    this.isOpen = false;
+    this.isActive = false
   }
 
   onItemClick(item: DropdownOption): void {
-    this.contextMenuItemClicked.emit(item);
-    this.close();
+    this.contextMenuItemClicked.emit(item)
+    this.close()
   }
 
-  onDocumentClick(event: MouseEvent): void {
-    const targetElement = event.target as HTMLElement;
-    const contextMenuElement = this.elementRef.nativeElement as HTMLElement;
-    if (!contextMenuElement.contains(targetElement)) {
-      this.close();
+  onMenuItemKeyDown(event: KeyboardEvent, index: number): void {
+    switch (event.key) {
+      case 'Enter':
+      case ' ':
+        event.preventDefault()
+        if (index >= 0 && index < this.menuItems.length) {
+          this.onItemClick(this.menuItems[index])
+        }
+        break
+      default:
+        break
     }
   }
 }
--- libs/angular/src/lib/context-menu/context-menu.stories.ts
+++ libs/angular/src/lib/context-menu/context-menu.stories.ts
@@ -1,8 +1,8 @@
-import { Story, Meta, moduleMetadata } from '@storybook/angular';
-import { NggContextMenuComponent} from './context-menu.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CommonModule } from '@angular/common';
-import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common'
+import { BrowserModule } from '@angular/platform-browser'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { Meta, Story, moduleMetadata } from '@storybook/angular'
+import { NggContextMenuComponent } from './context-menu.component'
 
 export default {
   title: 'Components/Context Menu',
@@ -13,26 +13,24 @@ export default {
       imports: [CommonModule, BrowserModule, BrowserAnimationsModule],
     }),
   ],
-} as Meta<NggContextMenuComponent>;
+} as Meta<NggContextMenuComponent>
 
 // Template for the component
-const Template: Story<NggContextMenuComponent> = (args: NggContextMenuComponent) => ({
+const Template: Story<NggContextMenuComponent> = (
+  args: NggContextMenuComponent
+) => ({
   component: NggContextMenuComponent,
   props: args,
   template: `
-  <div style="position: relative;">
-    <button (contextmenu)="ctx.open($event)">Right-click me</button>
-    <ngg-context-menu #ctx [menuItems]="menuItems" [menuItemTemplate]="menuItemTemplate" (itemClicked)="onItemClick($event)"></ngg-context-menu>
-  </div>
+  <ngg-context-menu #ctx [menuItems]="menuItems" (itemClicked)="onItemClick($event)"></ngg-context-menu>
   `,
-});
+})
 
-// Define the args for the stories
-export const Default = Template.bind({});
+export const Default = Template.bind({})
 Default.args = {
   menuItems: [
     { label: 'Option 1', value: 'option1' },
     { label: 'Option 2', value: 'option2' },
     { label: 'Option 3', value: 'option3' },
   ],
-};
+}
