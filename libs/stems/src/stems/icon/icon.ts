3e808e23d61df0a705b3cd0af967df728d2a8495
astrit <am@astrit.co>
2023-05-03 15:38:17 +0200

    feat: 🎸 Add x and chevron icon

--- libs/stems/src/stems/icon/icon.ts
+++ libs/stems/src/stems/icon/icon.ts
@@ -26,11 +26,16 @@ export class Icon extends LitElement {
         ${styles}
       </style>
       <svg display="none">
-        <symbol
-          id="plus"
-        >
+        <symbol id="plus">
           <line x1="12" y1="5" x2="12" y2="19" />
           <line x1="5" y1="12" x2="19" y2="12" />
+        </symbol>
+        <symbol id="x">
+          <line x1="18" y1="6" x2="6" y2="18" />
+          <line x1="6" y1="6" x2="18" y2="18" />
+        </symbol>
+        <symbol id="chevron">
+          <polyline points="9 18 15 12 9 6" />
         </symbol>
       </svg>
       <svg viewBox="0 0 24 24">
