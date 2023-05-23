0162d755fe34c25639f789ddedb6467da8be6de2
Joacim Magnusson <joacim@isogram.se>
2023-05-23 22:01:58 +0200

    docs: popover

--- libs/stems/src/stems/popover/popover.ts
+++ libs/stems/src/stems/popover/popover.ts
@@ -7,14 +7,15 @@ import styles from './stem.styles.scss'
 
 /**
  * @element gds-popover
- * @slot - Content of the popover
- * @fires ui-state - Fired when the popover is opened or closed
  *
  * A popover is a transient view that appears above other content. It is used by components such as dropdowns.
  *
  * GdsPopover can be supplied with a trigger through the `trigger` property. If a trigger is specified, the popover will
  * register a keydown listener on the trigger and listen to `ArrowDown` key presses. When the trigger is focused and
  * `ArrowDown` is pressed, the popover will open and focus the first slotted child.
+ *
+ * @slot - Content of the popover
+ * @fires ui-state - Fired when the popover is opened or closed
  */
 @customElement('gds-popover')
 export class GdsPopover extends LitElement {
