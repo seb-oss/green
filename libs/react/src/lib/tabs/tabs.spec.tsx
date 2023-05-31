6cd6d08b63566fe577e08372da00d362e42f0300
comeab <comeab@gmail.com>
2023-05-31 11:36:28 +0200

    feat: 🎸 components can be added as tab content (#903)
    Previously, tabs did not allow any content other than text. Second
    issue, the tab content and tab title were always equal. Now the `Tabs`
    component allows adding `Tab` component children with any type of
    content.
    
    ---------
    
    Co-authored-by: Joacim Magnusson <joacim@isogram.se>

--- libs/react/src/lib/tabs/tabs.spec.tsx
+++ libs/react/src/lib/tabs/tabs.spec.tsx
@@ -1,5 +1,6 @@
 import { render, screen, fireEvent } from '@testing-library/react'
-import Tabs, { IList } from './tabs'
+import Tabs, { IList, Tab } from './tabs'
+
 
 const list: IList[] = [
   { text: 'Page 1', href: '#' },
@@ -10,7 +11,7 @@ const list: IList[] = [
   { text: 'Page 6', disabled: true },
 ]
 
-describe('Tabs', () => {
+describe('Tabs only allow text as content', () => {
   it('should render all anchor elements', () => {
     render(<Tabs list={list}></Tabs>)
     const anchorTag: HTMLAnchorElement[] = screen.getAllByRole('tab')
@@ -48,6 +49,59 @@ describe('Tabs', () => {
 
   it('Should set href to "#" if href is not defined ', () => {
     render(<Tabs list={list} />)
+    const anchorTag: HTMLAnchorElement[] = screen.getAllByRole('tab')
+    expect(anchorTag[4].getAttribute('href')).toBe('#')
+  })
+})
+
+const tabList: TabProps[] = [
+  { title: 'Page 1', children: 'Page 1 Content' },
+  { title: 'Page 2', children: 'Page 2 Content' },
+  { title: 'Page 3', children: 'Page 3 Content' },
+  { title: 'Page 4', children: <div><div>Page 4 Content</div></div> },
+  { title: 'Page 5', children: <>Page 5 is a Component</> },
+  { title: 'Page 6', disabled: true },
+]
+
+describe('Tabs allow components as content', () => {
+  it('should render all anchor elements', () => {
+    render(<Tabs>{ tabList.map(tab => <Tab {...tab}>{tab.children}</Tab>)}</Tabs>)
+    const anchorTag: HTMLAnchorElement[] = screen.getAllByRole('tab')
+    expect(anchorTag).toBeTruthy()
+    anchorTag?.forEach((tag, index) => {
+      expect(tag.textContent).toBe(tabList[index].title);
+    }  
+    )
+  })
+
+  it('onClick changes selectedTab', () => {
+    render(<Tabs>{ tabList.map(tab => <Tab title={tab.title}>{tab.children}</Tab>)}</Tabs>)
+    const anchorTag: HTMLAnchorElement[] = screen.getAllByRole('tab')
+    fireEvent.click(anchorTag[1])
+    expect(screen.getByRole('tabpanel').textContent).toEqual('Page 2 Content')
+  })
+
+  it('OnClick should fire tabOnChange function', () => {
+    const onTabChange: jest.Mock = jest
+      .fn()
+      .mockImplementation((value: number) => value)
+    render(<Tabs onTabChange={onTabChange}>{ tabList.map(tab => <Tab {...tab}></Tab>)}</Tabs>)
+    const anchorTag: HTMLAnchorElement[] = screen.getAllByRole('tab')
+    fireEvent.click(anchorTag[1])
+    expect(screen.getByRole('tabpanel').textContent).toEqual('Page 2 Content')
+    expect(onTabChange).toBeCalledWith(1)
+  })
+
+  it('Should have aria-disabled', () => {
+    render(<Tabs>{ tabList.map(tab => <Tab {...tab}>{tab.children}</Tab>)}</Tabs>)
+    const anchorTag: HTMLAnchorElement[] = screen.getAllByRole('tab')
+    expect(anchorTag[4].getAttribute('aria-disabled')).toBe(null)
+    expect(anchorTag[4].getAttribute('href')).toBe('#')
+    expect(anchorTag[5].getAttribute('aria-disabled')).toBe('true')
+  })
+
+  it('Should set href to "#" if href is not defined ', () => {
+    render(<Tabs>{ tabList.map(tab => <Tab title={tab.title}>{tab.children}</Tab>)}</Tabs>)
     const anchorTag: HTMLAnchorElement[] = screen.getAllByRole('tab')
     expect(anchorTag[4].getAttribute('href')).toBe('#')
   })
