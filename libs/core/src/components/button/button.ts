a1af7e0f798e630474457d10da6a5a4ebfe45bfc
Joacim Magnusson <joacim@isogram.se>
2023-06-08 21:31:07 +0200

    chore(button): remove React export (and autoran prettier :)

--- libs/core/src/components/button/button.ts
+++ libs/core/src/components/button/button.ts
@@ -1,7 +1,5 @@
-import * as React from 'react'
 import { LitElement, html, unsafeCSS } from 'lit'
 import { customElement, property } from 'lit/decorators.js'
-import { createComponent } from '@lit-labs/react'
 import { constrainSlots } from 'utils/helpers'
 import '../icon/icon'
 import '../../primitives/ripple/ripple'
@@ -14,7 +12,7 @@ import styles from './style/button.styles.scss'
  *
  * @status beta
  *
- * @slot lead - This slot is optional and allows gds-icon. 
+ * @slot lead - This slot is optional and allows gds-icon.
  * @slot trail - This slot is optional and allows gds-icon.
  * @slot label - Allows test
  * @slot split - Allows icon and changes the button structure
@@ -30,35 +28,35 @@ export class GdsButton extends LitElement {
     delegatesFocus: true,
   }
 
-  @property({ type: String, reflect: true }) 
-  lead = null;
-  
-  @property({ type: String, reflect: true }) 
-  trail = null;
-  
-  @property({ type: Boolean, reflect: true }) 
-  disabled = false;
-  
-  @property({ type: Boolean, reflect: true, attribute: 'aria-pressed' }) 
-  ariaPressed = null;
-  
-  @property({ type: Boolean, reflect: true, attribute: 'aria-expanded' }) 
-  ariaExpanded = null;
-  
-  @property({ type: String, reflect: true, attribute: 'aria-label' }) 
-  ariaLabel = null;
-  
-  @property({ type: String, reflect: true }) 
-  variant = '';
-  
-  @property({ type: String, reflect: true }) 
-  set = null;
-  
-  @property({ type: String, reflect: true }) 
-  size = null;
-  
-  @property({ type: String, reflect: true }) 
-  effect = null;
+  @property({ type: String, reflect: true })
+  lead = null
+
+  @property({ type: String, reflect: true })
+  trail = null
+
+  @property({ type: Boolean, reflect: true })
+  disabled = false
+
+  @property({ type: Boolean, reflect: true, attribute: 'aria-pressed' })
+  ariaPressed = null
+
+  @property({ type: Boolean, reflect: true, attribute: 'aria-expanded' })
+  ariaExpanded = null
+
+  @property({ type: String, reflect: true, attribute: 'aria-label' })
+  ariaLabel = null
+
+  @property({ type: String, reflect: true })
+  variant = ''
+
+  @property({ type: String, reflect: true })
+  set = null
+
+  @property({ type: String, reflect: true })
+  size = null
+
+  @property({ type: String, reflect: true })
+  effect = null
 
   // Private members
   #internals: ElementInternals
@@ -74,31 +72,37 @@ export class GdsButton extends LitElement {
   }
 
   slotLead() {
-    return html`<slot name="lead" gds-allow="gds-icon"></slot>`;
+    return html`<slot name="lead" gds-allow="gds-icon"></slot>`
   }
 
   slotLabel() {
-    return this.textContent ? html`<slot part="label" gds-allow="#text"></slot>` : '';
+    return this.textContent
+      ? html`<slot part="label" gds-allow="#text"></slot>`
+      : ''
   }
- 
+
   slotTrail() {
     return html`<slot name="trail" gds-allow="gds-icon"></slot>`
   }
- 
+
   slotEffect() {
-    return this.effect ? html`<gds-ripple></gds-ripple>` : '';
+    return this.effect ? html`<gds-ripple></gds-ripple>` : ''
   }
 
   slotSplit() {
-    if (!this.variant || !this.variant.includes("split")) { return '' }
-    const slotElement = html`<slot name="split" gds-allow="gds-icon"></slot>`;
-    const rippleElement = this.effect ? html`<gds-ripple></gds-ripple>` : '';
-    const buttonElement = html`<button @click="${effectRipple}">${slotElement}${rippleElement}</button>`;
-    return buttonElement;
+    if (!this.variant || !this.variant.includes('split')) {
+      return ''
+    }
+    const slotElement = html`<slot name="split" gds-allow="gds-icon"></slot>`
+    const rippleElement = this.effect ? html`<gds-ripple></gds-ripple>` : ''
+    const buttonElement = html`<button @click="${effectRipple}">
+      ${slotElement}${rippleElement}
+    </button>`
+    return buttonElement
   }
 
   render() {
-    const content = html`${this.slotLead()}${this.slotLabel()}${this.slotTrail()}${this.slotEffect()}`;
+    const content = html`${this.slotLead()}${this.slotLabel()}${this.slotTrail()}${this.slotEffect()}`
     return html`
       <button
         ?disabled="${this.disabled}"
@@ -107,13 +111,9 @@ export class GdsButton extends LitElement {
         ?aria-expanded="${this.ariaExpanded}"
         tabindex="0"
         @click="${effectRipple}"
-      >${content}</button>${this.slotSplit()}
-    `;
+      >
+        ${content}</button
+      >${this.slotSplit()}
+    `
   }
 }
-
-export const ButtonReact = createComponent({
-  tagName: 'gds-button',
-  elementClass: GdsButton,
-  react: React,
-})
