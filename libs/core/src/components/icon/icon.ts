15641071a3773ca75db48d6141ac85d256eba128
astrit <am@astrit.co>
2023-06-12 14:27:09 +0200

    feat: 🎸 Add new icons

--- libs/core/src/components/icon/icon.ts
+++ libs/core/src/components/icon/icon.ts
@@ -34,6 +34,20 @@ export class GdsIcon extends LitElement {
         <symbol id="chevron">
           <polyline points="9 18 15 12 9 6" />
         </symbol>
+        <symbol id="arrow">
+          <line x1="5" y1="12" x2="19" y2="12" />
+          <polyline points="12 5 19 12 12 19" />
+        </symbol>
+        <symbol id="bell">
+          <path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9" />
+          <path d="M13.73 21a2 2 0 0 1-3.46 0" />
+        </symbol>
+        <symbol id="calendar">
+          <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
+          <line x1="16" y1="2" x2="16" y2="6"/>
+          <line x1="8" y1="2" x2="8" y2="6"/>
+          <line x1="3" y1="10" x2="21" y2="10" />
+        </symbol>
       </svg>
       <svg viewBox="0 0 24 24">
         <use href="#${name}"></use>
