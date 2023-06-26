7b7e66d50ee364c7f8945c052439e4425d513743
Joacim Magnusson <joacim@isogram.se>
2023-06-26 15:40:39 +0200

    chore(scoping): update core test app

--- apps/core-lib-dev/src/app/app.element.ts
+++ apps/core-lib-dev/src/app/app.element.ts
@@ -1,7 +1,16 @@
 import './app.element.scss'
 
-import { GdsDropdown, GdsOption } from '@sebgroup/green-core'
+import {
+  GdsDropdown,
+  GdsOption,
+  htmlTemplateTagFactory,
+  getScopedTagName,
+} from '@sebgroup/green-core'
 import { registerTransitionalStyles } from '@sebgroup/green-core'
+
+const html = htmlTemplateTagFactory((strs: TemplateStringsArray) =>
+  strs.join('')
+)
 
 export class AppElement extends HTMLElement {
   public static observedAttributes = []
@@ -11,47 +20,38 @@ export class AppElement extends HTMLElement {
 
     registerTransitionalStyles()
 
-    this.innerHTML = `
-    <div class="wrapper">
-      <form id="my-form">
-
-          <input name="my-input">
-
-          <label id="my-label">A dropdown label</label>
-          <gds-dropdown id="dropdown" name="my-dropdown" aria-labelledby="my-label">
-            <gds-option>Select value</gds-option>
-            <gds-option value="v1" aria-label="Custom layout">
-              <div style="display: flex">
-                <div>
-                  <div><strong>Left side</strong></div>
-                  <div>Under</div>
-                </div>
-                <div>
-                  <div><strong>Right side</strong></div>
-                  <div>Under</div>
-                </div>
-              </div>
-            </gds-option>
-            <gds-option value="v2">Value 2</gds-option>
-            <gds-option value="v3">Value 3</gds-option>
-            <p>Hello</p>
-            <div>Some other crap</div>
-            asdasd
+    this.innerHTML = html`
+      <div class="wrapper">
+        <h1 class="mb-5">Green Core test app</h1>
+        <div class="card">
+          <form id="my-form">
+            <gds-dropdown
+              id="dropdown"
+              name="my-dropdown"
+              label="Select a value"
+            >
+              <gds-option>Select value</gds-option>
+              <gds-option value="v1">Value 1</gds-option>
+              <gds-option value="v2">Value 2</gds-option>
+              <gds-option value="v3">Value 3</gds-option>
             </gds-dropdown>
 
             <br />
 
             <button type="submit">Submit</button>
-
-      </form>
-    </div>
-      `
+          </form>
+        </div>
+      </div>
+    `
 
     const dropdown = this.querySelector('#dropdown') as GdsDropdown
+
     dropdown.addEventListener('change', (e: CustomEvent) => {
+      console.log('Is Droopdown? ', e.target instanceof GdsDropdown)
       console.log('Value changed: ', e.detail.value)
       console.log('dropdown', dropdown.options)
     })
+
     dropdown.addEventListener('gds-ui-state', (e: CustomEvent) =>
       console.log('UI state changed: ', e.detail)
     )
@@ -59,7 +59,9 @@ export class AppElement extends HTMLElement {
     dropdown.value = 'v2'
     console.log('dropdown value', dropdown.value)
 
-    const newOption = document.createElement('gds-option') as GdsOption
+    const newOption = document.createElement(
+      getScopedTagName('gds-option')
+    ) as GdsOption
     newOption.value = { test: 'test' }
     newOption.innerHTML = 'New option'
     dropdown.appendChild(newOption)
--- libs/core/src/components/dropdown/dropdown.ts
+++ libs/core/src/components/dropdown/dropdown.ts
@@ -370,9 +370,3 @@ export class GdsDropdown<ValueType = any>
     if (isClickOutside || isFocusOutside) this.open = false
   }
 }
-
-declare global {
-  interface HTMLElementTagNameMap {
-    'gds-dropdown': GdsDropdown
-  }
-}
