e8df742c6d18acb4cdc70f24dc31c5ad53519958
Joacim Magnusson <joacim@isogram.se>
2023-06-26 16:02:06 +0200

    docs: update custom element scoping documentation

--- libs/core/src/utils/helpers/custom-element-scoping.ts
+++ libs/core/src/utils/helpers/custom-element-scoping.ts
@@ -1,8 +1,8 @@
 /**
  * # Custom element scoping
  *
- * One problem with the current state of the Web Component standard is that there is
- * only a single, global, CustomElementRegistry. This means that if you have two different
+ * One problem with the current state of the Custom Elements standard is that there is
+ * only a single, global, `CustomElementRegistry`. This means that if you have two different
  * libraries, or two different versions of the same library, that both define a custom
  * element with the same name, only one of them will be registered, and the other will
  * throw an error.
@@ -17,45 +17,43 @@
  *
  * ## Solution
  *
- * To get around the problem we will instead define our own scoping mechanism. It will
- * work something like this:
+ * To get around the problem we define our own scoping mechanism. It will works like this:
  *
- * * Green Core will have its own lookup table of custom elements, and there will be a
- *   suffix added to the name of each custom element. For example, `gds-popover` will become
- *  `gds-popover-<versionstring>`. The version string will be deterministically generated
- *  based on the version of the Green Core package.
+ * * Green Core has its own lookup table of custom elements, and a suffix is added to the
+ *   name of each custom element. For example, `gds-popover` becomes `gds-popover-<versionstring>`.
+ *   The version string is deterministically generated based on the version of the Green
+ *   Core package.
  *
- * * We'll also define a new `customElement` decorator that will be used to register custom
- *   elements. This decorator will automatically add the version suffix to the name of
- *   the custom element. If the custom element is already registered, it will abort silently,
- *   with the assumption that the custom element was already registered by the same version
- *   of Green Core and is therefor compatible.
+ * * We also define a new `customElement` decorator that will be used to register custom
+ *   elements. `@gdsCustomElement` will automatically add the version suffix to the name of
+ *   the custom element, and register it in the lookup table. If the custom element is already
+ *   registered, it will abort silently, with the assumption that the custom element was
+ *   already registered by the same version of Green Core and is therefor compatible.
  *
- * * There will be a custom `html` template tag that will automatically rewrite all custom
+ * * There is also a custom `html` template tag that will automatically rewrite all custom
  *   element names from the lookup table to include the version suffix, before passing the
- *   template on to the Lit `html` tag.
- *
- * This solution will take care of automatically scoping all of the primitives used
- * internally by Green Core.
+ *   template on to the Lit `html` tag. Alternatively, a templte tag factory can be used
+ *   to create a custom `html` tag, if Lit is not used by the consumer
  *
  * ## Caveats
  *
- * Consumers using the top-level components from Green Core will have to add the version suffix
- * in some way. One way could be to use the `html` template tag in consuming apps too, but this
- * will only work if the consumer is using Lit.
- * A more general workaround could be to expose the generated version string as a constant from
- * Green Core, so that it can be added to the tag names in the consuming app, but this will be
- * cumbersome to work with.
+ * Consumers using top-level components from Green Core will have to add the version suffix
+ * in some way. The React and Angular wrappers takes care of this automatically. Another way
+ * is to use the `html` template tag or template tag factory from Green Core, but it will only
+ * work if the consumer is using javascript template literals.
  *
- * Therefor the recommendation will be to only consume Green Core though the React or Angular
- * wrappers, or to use Lit, when building a MFE application, as the wrappers will take care
- * of the scoping automatically and Lit can use the custom `html` template tag from Green Core.
+ * If the consumer is using hard coded HTML, or a templating language that does not support
+ * template tags, the consumer will have to manually add the version suffix to the custom
+ * element names, which would be cumbersome. In this edge-case, the scoping feature can be disabled,
+ * and the consumer will have to make sure that only a single version of Green Core is used
+ * on the page.
+ *
+ * The recommendation is to only consume Green Core though the React or Angular wrappers, or to
+ * use the `html` template tag or template tag factory from Green Core.
  *
  * At some point in the future, when the scoped custom element registry proposal has been
- * implemented in all major browsers, we can remove this custom scoping mechanism and
- * switch to using native scoped registries instead. The recommendation of using only React
- * or Angular can then be lifted and replaced with a recommendation to render each application
- * in a shadow root with a scoped registry instead.
+ * implemented in all major browsers, we might remove this custom scoping mechanism and
+ * switch to using native scoped registries instead.
  */
 
 import { html as litHtml } from 'lit'
