c6bfb2608605650817cf76bc23cce43ec45b0f51
Joacim Magnusson <joacim@isogram.se>
2023-07-07 17:11:41 +0200

    feat(dropdown-react): adopt Core dropdown for React

--- libs/core/src/components/dropdown/dropdown.react.ts
+++
@@ -1,9 +0,0 @@
-import { createComponent } from '@lit-labs/react'
-import * as React from 'react'
-import { GdsDropdown } from './dropdown'
-
-export const GdsDropdownReact = createComponent({
-  tagName: 'gds-dropdown',
-  elementClass: GdsDropdown,
-  react: React,
-})
--- libs/core/src/primitives/listbox/option.react.ts
+++
@@ -1,9 +0,0 @@
-import { createComponent } from '@lit-labs/react'
-import * as React from 'react'
-import { GdsOption } from './option'
-
-export const GdsOptionReact = createComponent({
-  tagName: 'gds-option',
-  elementClass: GdsOption,
-  react: React,
-})
