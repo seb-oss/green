a4a181b18f4baca128a95a034a75185b16adc3cc
Joacim Magnusson <joacim@isogram.se>
2023-08-23 15:40:45 +0200

    chore: rename a few remaining instances of `stems` to `core`
    `stems` was the inital name of the `core` package

--- apps/core-lib-dev/jest.config.ts
+++ apps/core-lib-dev/jest.config.ts
@@ -1,13 +1,13 @@
 /* eslint-disable */
 export default {
-  displayName: 'stems-lib-dev',
+  displayName: 'core-lib-dev',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   transform: {
     '^.+\\.[tj]s$': 'babel-jest',
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/stems-lib-dev',
+  coverageDirectory: '../../coverage/apps/core-lib-dev',
   transformIgnorePatterns: [
     'node_modules/(?!.*.mjs$|lit-element|lit-html|lit|@lit|@lit-labs|dist/)',
   ],
