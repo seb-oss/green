011c5c2d7b4ae7e47d2319890e3cd5a6bf3abc33
Joacim Magnusson <joacim@isogram.se>
2023-08-23 11:02:46 +0200

    test: config updates for dev apps

--- apps/core-lib-dev/jest.config.ts
+++ apps/core-lib-dev/jest.config.ts
@@ -8,4 +8,10 @@ export default {
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/apps/stems-lib-dev',
+  transformIgnorePatterns: [
+    'node_modules/(?!.*.mjs$|lit-element|lit-html|lit|@lit|@lit-labs|dist/)',
+  ],
+  moduleNameMapper: {
+    '@sebgroup/green-core': '<rootDir>/../../dist/libs/core/src',
+  },
 }
