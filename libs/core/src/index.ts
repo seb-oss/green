9aa0d7add944a6b20aeb38ae912777e8014baef5
Joacim Magnusson <joacim@isogram.se>
2023-09-12 16:02:08 +0200

    ci: break out Transitional Styles and Localization to optional exports

--- libs/core/src/index.ts
+++ libs/core/src/index.ts
@@ -1,5 +1,2 @@
 export * from './components'
-
-export * from './utils/helpers/transitional-styles'
 export * from './utils/helpers/custom-element-scoping'
-export * from './utils/localization'
--- libs/core/src/utils/helpers/transitional-styles.ts
+++ libs/core/src/utils/helpers/transitional-styles.ts
@@ -8,13 +8,15 @@ export const registerTransitionalStyles = () => {
   Popover.register()
 }
 
-export class TransitionalStyles {
-  private static singleton: TransitionalStyles
+declare global {
+  var __gdsTransitionalStyles: TransitionalStyles
+}
 
+export class TransitionalStyles {
   static get instance() {
-    if (!TransitionalStyles.singleton)
-      TransitionalStyles.singleton = new TransitionalStyles()
-    return TransitionalStyles.singleton
+    if (!globalThis.__gdsTransitionalStyles)
+      globalThis.__gdsTransitionalStyles = new TransitionalStyles()
+    return globalThis.__gdsTransitionalStyles
   }
 
   private sheets = new Map<string, CSSStyleSheet>()
