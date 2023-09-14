5358c2db0d76ff44e87596b3b88efd7ddbec1fb8
Joacim Magnusson <joacim@isogram.se>
2023-09-14 10:24:52 +0200

    chore: revert React dropdown

--- libs/react/src/lib/dropdown/dropdown.tsx
+++ libs/react/src/lib/dropdown/dropdown.tsx
@@ -1,25 +1,7 @@
-import { DropdownArgs, DropdownOption, OnChange } from '@sebgroup/extract'
-import React from 'react'
-import { createComponent } from '@lit-labs/react'
-
-import { GdsDropdown, GdsOption, getScopedTagName } from '@sebgroup/green-core'
-
-import { registerTransitionalStyles } from '@sebgroup/green-core/transitional-styles'
-
-registerTransitionalStyles()
-
-export const CoreDropdown = createComponent({
-  tagName: getScopedTagName('gds-dropdown'),
-  elementClass: GdsDropdown,
-  events: { onchange: 'change' },
-  react: React,
-})
-
-export const CoreOption = createComponent({
-  tagName: getScopedTagName('gds-option'),
-  elementClass: GdsOption,
-  react: React,
-})
+import { DropdownArgs, OnChange } from '@sebgroup/extract'
+import { HTMLAttributes, useRef } from 'react'
+import { dropdownValues } from '@sebgroup/extract'
+import { useDropdown } from './hooks'
 
 export interface DropdownProps extends DropdownArgs {
   onChange?: OnChange
@@ -27,7 +9,7 @@ export interface DropdownProps extends DropdownArgs {
 
 export const Dropdown = ({
   compareWith,
-  display = 'label',
+  display,
   id,
   informationLabel,
   label,
@@ -38,43 +20,86 @@ export const Dropdown = ({
   searchFilter,
   searchable,
   texts,
-  useValue = 'value',
+  useValue,
   validator,
   value,
 }: DropdownProps) => {
-  const [selectedOption, setSelectedOption] = React.useState<
-    DropdownOption | undefined
-  >(options.find((o) => o.value === value))
+  const togglerRef = useRef<HTMLButtonElement>(null)
+  const listboxRef = useRef<HTMLDivElement>(null)
 
-  const handleOnChange = (e: any) => {
-    if (e.detail?.value) {
-      setSelectedOption(options.find((o) => o.value === e.detail.value))
-      onChange?.(e)
-    }
+  const { dropdown, listboxProps, togglerProps, listItems, multiSelectProps } =
+    useDropdown({
+      id,
+      value,
+      options,
+      loop,
+      multiSelect,
+      searchable,
+      searchFilter,
+      compareWith,
+      useValue,
+      display,
+      togglerRef,
+      listboxRef,
+      texts,
+      onChange,
+      validator,
+    })
+
+  const getListBoxProps = (props: HTMLAttributes<HTMLElement>) => {
+    if (Object.keys(props).length === 0)
+      return {
+        role: 'listbox',
+        tabIndex: -1,
+        className: dropdownValues().elements?.listbox?.classes?.join(' '),
+      }
+    return props
   }
 
   return (
     <div className="form-group">
-      <CoreDropdown
-        id={id}
-        label={label}
-        searchable={searchable}
-        multiple={multiSelect}
-        value={selectedOption?.value}
-        onchange={handleOnChange}
-        invalid={validator?.indicator === 'error'}
-      >
-        {informationLabel && <span slot="sub-label">{informationLabel}</span>}
-        {validator && <span slot="message">{validator.message}</span>}
-        <CoreOption isPlaceholder aria-hidden>
-          {texts?.placeholder || 'Select'}
-        </CoreOption>
-        {options.map((option) => (
-          <CoreOption key={option[useValue]} value={option[useValue]}>
-            {option[display]}
-          </CoreOption>
-        ))}
-      </CoreDropdown>
+      {label && <label htmlFor={togglerProps.id}>{label}</label>}
+      {informationLabel && <div className="form-info">{informationLabel}</div>}
+      <button type="button" {...togglerProps} ref={togglerRef}>
+        <span>{togglerProps.children}</span>
+      </button>
+      <div {...getListBoxProps(listboxProps)} ref={listboxRef}>
+        <div className="d-flex d-sm-none align-items-center">
+          <span className="flex-grow-1 ps-4 fs-2 fw-bolder">{label}</span>
+          <button
+            type="button"
+            className="close m-4 m-sm-2 d-block d-sm-none"
+            onClick={dropdown?.close}
+            aria-label={dropdown?.dropdown.texts.close}
+          >
+            <i></i>
+          </button>
+        </div>
+        {dropdown?.dropdown.isMultiSelect ? (
+          <div className="sg-fieldset-container">
+            <fieldset {...multiSelectProps.fieldsetProps}>
+              <legend {...multiSelectProps.legendProps} />
+              {multiSelectProps.checkboxes?.map((checkboxItem) => (
+                <label
+                  key={checkboxItem.labelProps.id}
+                  {...checkboxItem.labelProps}
+                >
+                  <input {...checkboxItem.inputProps} />
+                  <span {...checkboxItem.spanProps} />
+                  <i></i>
+                </label>
+              ))}
+            </fieldset>
+          </div>
+        ) : (
+          <ul role="listbox">
+            {listItems.map((liProps) => (
+              <li key={liProps.id} {...liProps} />
+            ))}
+          </ul>
+        )}
+      </div>
+      {validator && <span className="form-info">{validator?.message}</span>}
     </div>
   )
 }
