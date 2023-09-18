7c1f0f323de2d11cb5ec7482cb358dde182b02ef
Joacim Magnusson <joacim@isogram.se>
2023-09-18 18:45:05 +0200

    chore: [nx migration] update-16-0-0-add-nx-packages

--- apps/react-lib-dev/webpack.config.js
+++ apps/react-lib-dev/webpack.config.js
@@ -1,5 +1,5 @@
 const { composePlugins, withNx } = require('@nx/webpack')
-const { withReact } = require('@nrwl/react')
+const { withReact } = require('@nx/react')
 
 // Nx plugins for webpack.
 module.exports = composePlugins(
--- libs/react/jest.config.ts
+++ libs/react/jest.config.ts
@@ -6,7 +6,7 @@ export default {
   displayName: 'react',
   preset: '../../jest.preset.js',
   transform: {
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/libs/react',
