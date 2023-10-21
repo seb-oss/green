a043ae2fb70b42ca50f73092f80719459d5e386b
Joacim Magnusson <joacim@isogram.se>
2023-10-21 10:31:44 +0200

    chore(button): refactor (WIP)

--- libs/core/src/components/button/button.ts
+++ libs/core/src/components/button/button.ts
@@ -1,51 +1,24 @@
 import { LitElement, unsafeCSS } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
+import { property, query } from 'lit/decorators.js'
 import { constrainSlots } from 'utils/helpers'
 import '../icon/icon'
 import '../../primitives/ripple/ripple'
-import { effectRipple } from '../../utils/ripple/ripple'
 import styles from './style/button.styles.scss'
 
 import { gdsCustomElement, html } from 'utils/helpers/custom-element-scoping'
+import { classMap } from 'lit/directives/class-map.js'
 
 /**
  * @element gds-button
  * @summary A custom button element that can display a label, lead and trail icons, and a ripple effect on click.
- * @documentation https://seb.io/docs/component/button
  * @status beta
- * @since 1.0.0
  *
+ * @slot - Content to be displayed as the button label.
  * @slot lead - An optional slot that allows a `gds-icon` element to be placed before the label.
  * @slot trail - An optional slot that allows a `gds-icon` element to be placed after the label.
- * @slot label - A slot that allows text to be displayed as the button label.
- * @slot split - An optional slot that allows an icon to be placed in the middle of the button, splitting the label into two parts.
- * @slot circle - An optional slot that allows a `gds-icon` element to be placed in the center of the button, creating a circular button.
  *
- * @cssprop --gds-button-background-color - The background color of the button.
- * @cssprop --gds-button-color - The text color of the button.
- * @cssprop --gds-button-font-size - The font size of the button.
- * @cssprop --gds-button-height - The height of the button.
- * @cssprop --gds-button-width - The width of the button.
- *
- * @property {string} lead - The name of the lead icon to display.
- * @property {string} trail - The name of the trail icon to display.
- * @property {boolean} disabled - Whether the button is disabled.
- * @property {boolean} ariaPressed - Whether the button is pressed.
- * @property {boolean} ariaExpanded - Whether the button is expanded.
- * @property {string} ariaLabel - The label for the button, used for accessibility.
- * @property {string} variant - The variant of the button, which can include "split" or "circle".
- * @property {string} set - The set of icons to use for the lead and trail icons.
- * @property {string} size - The size of the button, which can be "small", "medium", or "large".
- * @property {string} effect - The type of ripple effect to use, which can be "center" or "full".
- *
- * @event click - Fired when the button is clicked, initiating the ripple effect.
- *
- * @example
- * <gds-button>Click me</gds-button>
- * <gds-button variant="split">Click me</gds-button>
- * <gds-button variant="circle">Click me</gds-button>
+ * @event click - Fired when the button is clicked.
  */
-
 @gdsCustomElement('gds-button')
 export class GdsButton extends LitElement {
   static styles = unsafeCSS(styles)
@@ -55,117 +28,62 @@ export class GdsButton extends LitElement {
     delegatesFocus: true,
   }
 
-  @property({ type: String, reflect: true })
-  lead = null
-
-  @property({ type: String, reflect: true })
-  trail = null
-
+  /**
+   * Whether the button is disabled.
+   */
   @property({ type: Boolean, reflect: true })
   disabled = false
 
-  @property({ type: Boolean, reflect: true, attribute: 'aria-pressed' })
-  ariaPressed = null
-
-  @property({ type: Boolean, reflect: true, attribute: 'aria-expanded' })
-  ariaExpanded = null
-
-  @property({ type: String, reflect: true, attribute: 'aria-label' })
-  ariaLabel = null
-
+  /**
+   * The variant of the button. Defaults to "primary".
+   */
   @property({ type: String, reflect: true })
-  variant = ''
+  variant: 'primary' | 'secondary' | 'tertiary' = 'primary'
 
+  /**
+   * Defines which set the button belongs to. Defaults to "neutral".
+   */
   @property({ type: String, reflect: true })
-  set = null
+  set: 'neutral' | 'positive' | 'negative' = 'neutral'
 
+  /**
+   * Sets the size of the button. Defaults to "small".
+   */
   @property({ type: String, reflect: true })
-  size = null
+  size: 'small' | 'medium' | 'large' = 'small'
 
-  @property({ type: String, reflect: true })
-  effect = null
+  @query('slot:not([name])') private _mainSlot?: HTMLSlotElement
 
-  // Private members
-  #internals: ElementInternals
+  #isIconButton = false
 
   constructor() {
     super()
-    this.#internals = this.attachInternals()
     constrainSlots(this)
   }
 
-  connectedCallback() {
-    super.connectedCallback()
-  }
+  #mainSlotChange = () => {
+    const assignedNodes = this._mainSlot?.assignedNodes() ?? []
 
-  slotLead() {
-    return html`<slot name="lead" gds-allow="gds-icon"></slot>`
-  }
+    this.#isIconButton =
+      assignedNodes.length === 1 &&
+      assignedNodes.some((node) => node.nodeName === 'GDS-ICON')
 
-  slotLabel() {
-    return this.textContent
-      ? html`<slot part="label" gds-allow="#text"></slot>`
-      : ''
-  }
+    console.log(this.#isIconButton, assignedNodes)
 
-  slotTrail() {
-    return html`<slot name="trail" gds-allow="gds-icon"></slot>`
-  }
-
-  slotEffect() {
-    return this.effect ? html`<gds-ripple></gds-ripple>` : ''
-  }
-
-  slotSplit() {
-    if (!this.variant || !this.variant.includes('split')) {
-      return ''
-    }
-    const slotElement = html`<slot name="split" gds-allow="gds-icon"></slot>`
-    const rippleElement = this.effect ? html`<gds-ripple></gds-ripple>` : ''
-    const buttonElement = html` <button
-      ?disabled="${this.disabled}"
-      aria-label="${this.textContent}"
-      ?aria-pressed="${this.ariaPressed}"
-      ?aria-expanded="${this.ariaExpanded}"
-      tabindex="0"
-      @click="${effectRipple}"
-    >
-      ${slotElement}${rippleElement}
-    </button>`
-    return buttonElement
-  }
-
-  slotCircle() {
-    return html`<slot name="circle" gds-allow="gds-icon"></slot>`
+    this.requestUpdate()
   }
 
   render() {
-    if (this.variant.includes('circle')) {
-      const content = html`${this.slotCircle()}${this.slotEffect()}`
-      return html` <button
-        ?disabled="${this.disabled}"
-        aria-label="${this.textContent}"
-        ?aria-pressed="${this.ariaPressed}"
-        ?aria-expanded="${this.ariaExpanded}"
-        tabindex="0"
-        @click="${effectRipple}"
-      >
-        ${content}
-      </button>`
-    } else {
-      const content = html`${this.slotLead()}${this.slotLabel()}${this.slotTrail()}${this.slotEffect()}`
-      return html`
-        <button
-          ?disabled="${this.disabled}"
-          aria-label="${this.textContent}"
-          ?aria-pressed="${this.ariaPressed}"
-          ?aria-expanded="${this.ariaExpanded}"
-          tabindex="0"
-          @click="${effectRipple}"
-        >
-          ${content}</button
-        >${this.slotSplit()}
-      `
-    }
+    return html`
+      <button class="${classMap({ circle: this.#isIconButton })}">
+        <slot name="lead" gds-allow="gds-icon"></slot>
+        <slot
+          @slotchange=${this.#mainSlotChange}
+          gds-allow="#text gds-icon"
+        ></slot>
+        <slot name="trail" gds-allow="gds-icon"></slot>
+        <gds-ripple></gds-ripple>
+      </button>
+    `
   }
 }
