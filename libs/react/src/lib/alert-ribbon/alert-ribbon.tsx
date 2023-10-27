d02e8bebecc46d626531b47e0c2d80b56c8489ed
Joacim Magnusson <joacim@isogram.se>
2023-10-27 12:18:27 +0200

    fix(alert-ribbon): fix header render logic

--- libs/react/src/lib/alert-ribbon/alert-ribbon.tsx
+++ libs/react/src/lib/alert-ribbon/alert-ribbon.tsx
@@ -61,14 +61,23 @@ export function AlertRibbon({
     }
   }
 
-  let renderedHeader: ReactNode = React.isValidElement(header) ? header : null;
-  renderedHeader = renderedHeader ?? <span className="header">{header}</span>;
+  const renderHeader = () => {
+    if (!header) {
+      return null
+    }
+
+    return React.isValidElement(header) ? (
+      header
+    ) : (
+      <span className="header">{header}</span>
+    )
+  }
 
   return (
     <div className={`alert-ribbon ${type}`} role={role} aria-live={ariaLive}>
       <i aria-hidden="true">{renderIcon()}</i>
       <div className="alert-ribbon__content">
-        {renderedHeader}
+        {renderHeader()}
         <p>{children}</p>
       </div>
       {closeButton && (
