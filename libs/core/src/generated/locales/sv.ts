d261f41a108af16e4dbc1a18307c4831805d0942
Joacim Magnusson <joacim@isogram.se>
2023-11-03 13:06:10 +0100

    fix(dropdown): add missing label to close button
    Affects mobile viewport

--- libs/core/src/generated/locales/sv.ts
+++ libs/core/src/generated/locales/sv.ts
@@ -11,6 +11,7 @@
     export const templates = {
       's58bfb494feb8eb02': str`${0} valda`,
 's5d929ff1619ac0c9': `Sök`,
+'s5e8250fb85d64c23': `Stäng`,
 'sd898d99fd9c13de6': `Sök i listan av val`,
     };
   
