d788ae12cfaa166b4d1f3f97790b952e8133ed87
Joacim Magnusson <joacim@isogram.se>
2023-11-10 15:36:00 +0100

    chore: import tokens from `internal` set

--- libs/core/src/tokens.style.ts
+++ libs/core/src/tokens.style.ts
@@ -1,8 +1,8 @@
 import { unsafeCSS } from 'lit'
 
-import palletCss from '../../../dist/libs/tokens/css/pallet.css'
-import lightThemeCss from '../../../dist/libs/tokens/css/theme/light.css'
-import darkThemeCss from '../../../dist/libs/tokens/css/theme/dark.css'
+import palletCss from '../../../dist/libs/tokens/internal/pallet.css'
+import lightThemeCss from '../../../dist/libs/tokens/internal/theme/light.css'
+import darkThemeCss from '../../../dist/libs/tokens/internal/theme/dark.css'
 
 // These are here temporarliy until we can get them from the tokens package
 import localTokens from './tokens/tokens.css'
@@ -12,7 +12,7 @@ import localA11yTokens from './tokens/a11y.css'
 const tokens = [
   unsafeCSS(palletCss),
   unsafeCSS(lightThemeCss),
-  unsafeCSS(darkThemeCss),
+  //unsafeCSS(darkThemeCss),
   unsafeCSS(localTokens),
   unsafeCSS(localA11yTokens),
 ]
