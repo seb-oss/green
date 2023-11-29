31e2d01efffd183f1080a7991ed6f78da5dce71d
Gauri Kudtarkar <gauri.kudtarkar@seb.se>
2023-11-29 10:04:04 +0100

    fix: missed 'close' button variant during merging of code

--- libs/extract/src/lib/button.ts
+++ libs/extract/src/lib/button.ts
@@ -1,3 +1,8 @@
 export type ButtonType = 'button' | 'submit' | 'reset'
-export type ButtonVariant = 'primary' | 'secondary' | 'ghost' | 'tertiary' | 'link'
+export type ButtonVariant =
+  | 'primary'
+  | 'secondary'
+  | 'ghost'
+  | 'tertiary'
+  | 'close'
 export type ButtonSize = 'small' | 'large'
