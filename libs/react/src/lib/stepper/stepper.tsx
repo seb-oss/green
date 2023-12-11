0cbbe100707ee742d23b81bc1b4410f2bcc62a6c
Joacim Magnusson <joacim.magnusson@seb.se>
2023-12-11 15:35:31 +0100

    fix(react/stepper): refactor internal state handling (#1147)
    * fix(react/stepper): refactor internal state handling
    Closes #1062

--- libs/react/src/lib/stepper/stepper.tsx
+++ libs/react/src/lib/stepper/stepper.tsx
@@ -1,6 +1,11 @@
-import {ChangeEvent} from 'react'
-import { StepperArgs, useStepper } from './hook'
-import {IValidator, validateClassName, IndicatorType} from "@sebgroup/extract";
+import { ChangeEvent, useCallback, useEffect, useState } from 'react'
+import {
+  IValidator,
+  validateClassName,
+  IndicatorType,
+  StepperArgs,
+} from '@sebgroup/extract'
+import { on } from 'events'
 
 export interface StepperProps extends StepperArgs {
   label?: string
@@ -9,53 +14,98 @@ export interface StepperProps extends StepperArgs {
   validator?: IValidator
 }
 
-
 // TODO: Should be named "Numeric input" instead of stepper?
 
 export function Stepper({
+  id,
   label,
   description,
   statusMessage,
   validator,
-  ...stepperArgs
+  value = 0,
+  onChange = () => undefined,
+  min = Number.MIN_SAFE_INTEGER,
+  max = Number.MAX_SAFE_INTEGER,
+  step = 1,
 }: StepperProps) {
+  const [localValue, setLocalValue] = useState<number>(value)
 
-  const [stepper, data] = useStepper(stepperArgs)
+  const clamp = (v: number) => Math.max(min, Math.min(v, max))
 
-  const onChangeEvent = (e: ChangeEvent<HTMLInputElement>) => {
-    stepper.setValue(e.target.valueAsNumber)
+  const onChangeEvent = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      if (isNaN(e.target.valueAsNumber)) return
+      const value = clamp(e.target.valueAsNumber)
+      setLocalValue(value)
+      onChange(value)
+    },
+    [onChange]
+  )
+
+  useEffect(() => {
+    setLocalValue(value)
+  }, [value])
+
+  const down = () => {
+    if (localValue > min) {
+      const newValue = clamp(localValue - step)
+      setLocalValue(newValue)
+      onChange(newValue)
+    }
+  }
+
+  const up = () => {
+    if (localValue < max) {
+      const newValue = clamp(localValue + step)
+      setLocalValue(newValue)
+      onChange(newValue)
+    }
+  }
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'ArrowUp') {
+      e.preventDefault()
+      up()
+    } else if (e.key === 'ArrowDown') {
+      e.preventDefault()
+      down()
+    }
   }
 
   const PrimitiveStepper = (
-    <div className={`group group-border group-stepper ${validator && validateClassName(validator?.indicator as IndicatorType)}`}>
-      <button onClick={() => stepper.down()}>-</button>
+    <div
+      className={`group group-border group-stepper ${
+        validator && validateClassName(validator?.indicator as IndicatorType)
+      }`}
+    >
+      <button type="button" onClick={down}>
+        -
+      </button>
       <input
-        id={data.id}
+        id={id}
         type="number"
         onChange={onChangeEvent}
-        onFocus={({target}) => target.select()}
+        onFocus={({ target }) => target.select()}
+        onKeyDown={handleKeyDown}
         placeholder="0"
-        value={data.value}
+        value={localValue}
       />
-      <button onClick={() => stepper.up()}>+</button>
+      <button type="button" onClick={up}>
+        +
+      </button>
     </div>
   )
 
-  if (!label && !description && !statusMessage && !validator) return PrimitiveStepper;
+  if (!label && !description && !statusMessage && !validator)
+    return PrimitiveStepper
 
   return (
     <div className="form-group">
-      { label && (
-        <label htmlFor={data.id}>{ label }</label>
-      )}
-      { description && (
-        <span className="form-info">{ description }</span>
-      )}
+      {label && <label htmlFor={id}>{label}</label>}
+      {description && <span className="form-info">{description}</span>}
       <div className="stepper-wrapper">
-        { PrimitiveStepper }
-        { validator && (
-          <span className="form-info">{ validator.message }</span>
-        )}
+        {PrimitiveStepper}
+        {validator && <span className="form-info">{validator.message}</span>}
       </div>
     </div>
   )
