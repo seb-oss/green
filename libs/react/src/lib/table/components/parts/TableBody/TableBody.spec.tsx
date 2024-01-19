bbdad0fdf480257e17ad2935b0bdc1039cfc53b7
terrance456 <terranceprasath97@gmail.com>
2024-01-19 10:58:11 +0800

    feat: 🎸 table body component

---
+++ libs/react/src/lib/table/components/parts/TableBody/TableBody.spec.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { TableBody } from './TableBody'
+import { TableRow } from '../TableRow'
+import { render } from '@testing-library/react'
+
+describe('Component: Table body', () => {
+  it('Should render correctly', () => {
+    render(
+      <table>
+        <TableBody />
+      </table>
+    )
+    expect(document.body.querySelector('tbody')).toBeInTheDocument()
+  })
+
+  it('Should render children correctly', () => {
+    render(
+      <table>
+        <TableBody>
+          <TableRow />
+        </TableBody>
+      </table>
+    )
+    expect(document.body.querySelector('tr')).toBeInTheDocument()
+  })
+
+  it('Should render append parent key to sub row correctly', () => {
+    const uniqueKey: string = 'test'
+    render(
+      <table>
+        <TableBody>
+          <TableRow uniqueKey={uniqueKey}></TableRow>
+          <TableRow isSubRow></TableRow>
+        </TableBody>
+      </table>
+    )
+    expect(document.body.querySelector('.sub-row')).toBeInTheDocument()
+  })
+
+  it('Should render append parent key to sub row correctly within fragment', () => {
+    const uniqueKey: string = 'test'
+    render(
+      <table>
+        <TableBody>
+          <React.Fragment>
+            <TableRow uniqueKey={uniqueKey}></TableRow>
+            <TableRow isSubRow></TableRow>
+            <tr className="custom" />
+          </React.Fragment>
+        </TableBody>
+      </table>
+    )
+    expect(document.body.querySelector('.sub-row')).toBeInTheDocument()
+    expect(document.body.querySelector('.custom')).toBeInTheDocument()
+  })
+})
