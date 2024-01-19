6439fb9f23bebec48405513799bf1c79045f5a8c
Terrance Prasath <92840841+terrance456@users.noreply.github.com>
2024-01-20 00:11:36 +0800

    feat(react/table): add react table component (#1162)

--- libs/react/src/index.ts
+++ libs/react/src/index.ts
@@ -18,6 +18,7 @@ export * from './lib/tabs/tabs'
 export * from './lib/modal/modal'
 export * from './lib/context-menu/context-menu'
 export * from './types'
+export * from './lib/table/components'
 
 // Backwards compatibility export
 export { AlertRibbon as Alert } from './lib/alert-ribbon/alert-ribbon'
---
+++ libs/react/src/lib/table/components/Table.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from '@testing-library/react'
+import { Table } from './Table'
+
+describe('Component: Table', () => {
+  it('Should render simple table', async () => {
+    render(<Table />)
+    expect(screen.getByRole('table')).not.toBeNull()
+  })
+})
