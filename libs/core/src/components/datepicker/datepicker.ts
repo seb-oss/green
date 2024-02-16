df677d7b201efa50c0b16edf0dd67004b06258a6
Joacim Magnusson <joacim@isogram.se>
2024-02-16 14:05:46 +0100

    Datepicker: update calendar button + icon

--- libs/core/src/components/datepicker/datepicker.ts
+++ libs/core/src/components/datepicker/datepicker.ts
@@ -227,14 +227,11 @@ export class GdsDatepicker extends GdsFormControlElement<Date> {
           aria-describedby="label"
           @click=${() => (this.open = !this.open)}
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 448 512"
-            style="width:100%;height:100%"
-          >
-            <path
-              d="M152 64h144V24c0-13.25 10.7-24 24-24s24 10.75 24 24v40h40c35.3 0 64 28.65 64 64v320c0 35.3-28.7 64-64 64H64c-35.35 0-64-28.7-64-64V128c0-35.35 28.65-64 64-64h40V24c0-13.25 10.7-24 24-24s24 10.75 24 24v40zM48 248h80v-56H48v56zm0 48v64h80v-64H48zm128 0v64h96v-64h-96zm144 0v64h80v-64h-80zm80-104h-80v56h80v-56zm0 216h-80v56h64c8.8 0 16-7.2 16-16v-40zm-128 0h-96v56h96v-56zm-144 0H48v40c0 8.8 7.16 16 16 16h64v-56zm144-216h-96v56h96v-56z "
-            />
+          <svg viewBox="0 0 24 24" inert>
+            <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
+            <line x1="16" y1="2" x2="16" y2="6" />
+            <line x1="8" y1="2" x2="8" y2="6" />
+            <line x1="3" y1="10" x2="21" y2="10" />
           </svg>
         </button>
       </div>
