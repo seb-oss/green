acd36a1810bacc80e384336c121e9e4f6f96ede5
astrit <am@astrit.co>
2024-03-04 16:14:32 +0100

    New method simplified and initiate gap

--- libs/core/src/components/grid/grid.ts
+++ libs/core/src/components/grid/grid.ts
@@ -29,18 +29,54 @@ export class GdsGrid extends LitElement {
   @property({ attribute: 'columns-desktop', type: Number })
   columnsDesktop?: number
 
+  // updated(changedProperties: Map<string | number | symbol, unknown>) {
+  //   super.updated(changedProperties)
+
+  //   if (
+  //     changedProperties.has('columnsMobile') ||
+  //     changedProperties.has('columnsTablet') ||
+  //     changedProperties.has('columnsDesktop')
+  //   ) {
+  //     const columnProperties = [
+  //       { name: 'gds-columns-mobile', value: this.columnsMobile },
+  //       { name: 'gds-columns-tablet', value: this.columnsTablet },
+  //       { name: 'gds-columns-desktop', value: this.columnsDesktop },
+  //     ]
+
+  //     const cssVariables = columnProperties
+  //       .filter(({ value }) => value !== undefined)
+  //       .map(({ name, value }) => `--${name}: ${value};`)
+  //       .join(' ')
+
+  //     const sheet = new CSSStyleSheet()
+  //     sheet.replaceSync(`:host {${cssVariables}}`)
+
+  //     if (this.shadowRoot) {
+  //       this.shadowRoot.adoptedStyleSheets = [
+  //         ...this.shadowRoot.adoptedStyleSheets,
+  //         sheet,
+  //       ]
+  //     }
+  //   }
+  // }
+
+  @property({ attribute: 'columns', type: String })
+  columns?: string
+
+  @property({ attribute: 'gap', type: String })
+  gap?: string
+
   updated(changedProperties: Map<string | number | symbol, unknown>) {
     super.updated(changedProperties)
 
-    if (
-      changedProperties.has('columnsMobile') ||
-      changedProperties.has('columnsTablet') ||
-      changedProperties.has('columnsDesktop')
-    ) {
+    if (changedProperties.has('columns')) {
+      const [columnsDesktop, columnsTablet, columnsMobile] =
+        this.columns?.split(' ').map(Number) || []
+
       const columnProperties = [
-        { name: 'gds-columns-mobile', value: this.columnsMobile },
-        { name: 'gds-columns-tablet', value: this.columnsTablet },
-        { name: 'gds-columns-desktop', value: this.columnsDesktop },
+        { name: 'gds-columns-mobile', value: columnsMobile },
+        { name: 'gds-columns-tablet', value: columnsTablet },
+        { name: 'gds-columns-desktop', value: columnsDesktop },
       ]
 
       const cssVariables = columnProperties
