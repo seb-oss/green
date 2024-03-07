6f69b67c3c6553aab69cc7dd8053b82502aa72fd
Joacim Magnusson <joacim.magnusson@seb.se>
2024-03-07 14:40:19 +0100

    Merge `.css` support from `vision`

--- libs/core/src/declarations.d.ts
+++ libs/core/src/declarations.d.ts
@@ -1,3 +1,8 @@
+declare module '*.css' {
+  const content: Record<string, string>
+  export default content
+}
+
 declare module '*.scss' {
   const content: Record<string, string>
   export default content
