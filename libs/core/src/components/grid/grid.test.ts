9df22a4438f7ee284f17fca7ade77bf7d49131e9
astrit <am@astrit.co>
2024-03-14 15:18:53 +0100

    Add grid to branch grid

---
+++ libs/core/src/components/grid/grid.test.ts
@@ -0,0 +1,46 @@
+import { expect, fixture, html } from '@open-wc/testing'
+import './grid.ts'
+import { GdsGrid } from './grid.js'
+
+describe('GdsGrid', () => {
+  let element: GdsGrid
+  beforeEach(async () => {
+    element = await fixture(html`<gds-grid></gds-grid>`)
+  })
+
+  it('renders a slot', () => {
+    const slot = element.shadowRoot?.querySelector('slot')
+    expect(slot).to.exist
+  })
+
+  it('sets default properties', () => {
+    const element: GdsGrid = document.createElement('gds-grid') as GdsGrid
+    expect(element.columns).to.be.undefined
+    expect(element.gap).to.be.undefined
+    expect(element.padding).to.be.undefined
+    expect(element.fluid).to.be.undefined
+  })
+
+  it('accepts only valid GridSizes for columns, gap, and padding', () => {
+    const validSizes = ['none', 'xs', 's', 'm', 'l', 'xl', 'xxl', 'xxxl']
+    const invalidSize = 'invalid'
+
+    validSizes.forEach((size) => {
+      element.columns = size as any
+      element.gap = size as any
+      element.padding = size as any
+
+      expect(element.columns).to.equal(size)
+      expect(element.gap).to.equal(size)
+      expect(element.padding).to.equal(size)
+    })
+
+    element.columns = invalidSize as any
+    element.gap = invalidSize as any
+    element.padding = invalidSize as any
+
+    expect(element.columns).to.not.equal(invalidSize)
+    expect(element.gap).to.not.equal(invalidSize)
+    expect(element.padding).to.not.equal(invalidSize)
+  })
+})
---
+++ libs/core/src/components/grid/grid.ts
@@ -0,0 +1,129 @@
+import { LitElement, unsafeCSS } from 'lit'
+import { property } from 'lit/decorators.js'
+import {
+  gdsCustomElement,
+  html as customElementHtml,
+} from '../../utils/helpers/custom-element-scoping'
+import { stripWhitespace } from '../../utils/helpers/strip-white-space'
+
+import { tokens } from '../../tokens.style'
+import grid from './grid.style.css'
+const gridCSS = unsafeCSS(grid)
+
+type GridSizes = 'none' | 'xs' | 's' | 'm' | 'l' | 'xl' | 'xxl' | 'xxxl'
+type GridGap = GridSizes
+type GridPadding = GridSizes
+
+const html = stripWhitespace(customElementHtml)
+
+@gdsCustomElement('gds-grid')
+export class GdsGrid extends LitElement {
+  static styles = [tokens, gridCSS]
+
+  static shadowRootOptions: ShadowRootInit = {
+    mode: 'open',
+    delegatesFocus: true,
+  }
+
+  @property({ attribute: 'columns', type: String })
+  columns?: string
+
+  @property({ attribute: 'gap', type: String })
+  gap?: GridGap
+
+  @property({ attribute: 'padding', type: String })
+  padding?: GridPadding
+
+  @property({ attribute: 'fluid', type: String })
+  fluid?: boolean
+
+  updated(changedProperties: Map<string | number | symbol, unknown>) {
+    super.updated(changedProperties)
+
+    let cssVariables = ''
+
+    if (changedProperties.has('columns')) {
+      const [columnsDesktop, columnsTablet, columnsMobile] =
+        this.columns?.split(' ').map(Number) || []
+
+      const columnProperties = [
+        { name: 'gds-columns-mobile', value: columnsMobile },
+        { name: 'gds-columns-tablet', value: columnsTablet },
+        { name: 'gds-columns-desktop', value: columnsDesktop },
+      ]
+
+      cssVariables += columnProperties
+        .filter(({ value }) => value !== undefined)
+        .map(({ name, value }) => `--${name}: ${value};`)
+        .join(' ')
+    }
+
+    if (changedProperties.has('gap')) {
+      const [gapDesktop, gapTablet, gapMobile] = this.gap?.split(' ') || []
+
+      const gapProperties = [
+        { name: 'gds-gap-desktop', value: gapDesktop },
+        { name: 'gds-gap-tablet', value: gapTablet },
+        { name: 'gds-gap-mobile', value: gapMobile },
+      ]
+
+      cssVariables += gapProperties
+        .filter(({ value }) => value !== undefined)
+        .map(
+          ({ name, value }) => `--${name}: var(--gds-sys-grid-gap-${value});`
+        )
+        .join(' ')
+    }
+
+    if (changedProperties.has('padding')) {
+      const [paddingDesktop, paddingTablet, paddingMobile] =
+        this.padding?.split(' ') || []
+
+      const paddingProperties = [
+        { name: 'gds-padding-desktop', value: paddingDesktop },
+        { name: 'gds-padding-tablet', value: paddingTablet },
+        { name: 'gds-padding-mobile', value: paddingMobile },
+      ]
+
+      cssVariables += paddingProperties
+        .filter(({ value }) => value !== undefined)
+        .map(
+          ({ name, value }) =>
+            `--${name}: var(--gds-sys-grid-padding-${value});`
+        )
+        .join(' ')
+    }
+
+    if (changedProperties.has('fluid') && this.fluid) {
+      cssVariables += `
+        --gds-grid-col-count: var(--gds-c);
+        --gds-grid-col-width: 200px;
+        --gap-count: calc(var(--gds-grid-col-count) - 1);
+        --total-gap-width: calc(var(--gap-count) * var(--gds-gap-column));
+        --gds-grid-col-width-max: calc(
+          (100% - var(--total-gap-width)) / var(--gds-grid-col-count)
+        );
+  
+        grid-template-columns: repeat(
+          auto-fill,
+          minmax(max(var(--gds-grid-col-width), var(--gds-grid-col-width-max)), 1fr)
+        );
+        text-wrap: balance;
+      `
+    }
+
+    const sheet = new CSSStyleSheet()
+    sheet.replaceSync(`:host {${cssVariables}}`)
+
+    if (this.shadowRoot) {
+      this.shadowRoot.adoptedStyleSheets = [
+        ...this.shadowRoot.adoptedStyleSheets,
+        sheet,
+      ]
+    }
+  }
+
+  render() {
+    return html` <slot></slot> `
+  }
+}
