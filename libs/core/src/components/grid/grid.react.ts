530520d56a8065711cd96c9ec08a4516dcb09719
astrit <am@astrit.co>
2024-03-15 22:53:23 +0100

    #1258 - Remove grid.react.ts

--- libs/core/src/components/grid/grid.react.ts
+++
@@ -1,9 +0,0 @@
-import { createComponent } from '@lit-labs/react'
-import * as React from 'react'
-import { GdsGrid } from './grid'
-
-export const GridReact = createComponent({
-  tagName: 'gds-grid',
-  elementClass: GdsGrid,
-  react: React,
-})
