4a74b909f8aa4c5b54101ab8f9fdf4398490fa6b
astrit <am@astrit.co>
2024-03-15 23:00:30 +0100

    #1258 - Add JSDocs for comp and all props

--- libs/core/src/components/grid/grid.ts
+++ libs/core/src/components/grid/grid.ts
@@ -13,6 +13,14 @@ type GridSizes = 'none' | 'xs' | 's' | 'm' | 'l' | 'xl' | '2xl' | '3xl'
 type GridGap = GridSizes
 type GridPadding = GridSizes
 
+/**
+ * @element gds-grid
+ * The `gds-grid` is a custom element that provides a flexible grid system. It uses CSS grid layout to arrange its child elements into columns. This component is highly customizable and responsive, allowing you to specify the number of `columns`, `gap`, `padding`, and fluidity for different devices with automated column size based on the content using the `fluid` attribute.
+ *
+ * @status beta
+ *
+ */
+
 @gdsCustomElement('gds-grid')
 export class GdsGrid extends LitElement {
   static styles = [tokens, gridCSS]
@@ -22,15 +30,43 @@ export class GdsGrid extends LitElement {
     delegatesFocus: true,
   }
 
+  /**
+   * @property {string} columns - The number of columns for the grid. This can be a space-separated string of three numbers, specifying the number of columns for desktop, tablet, and mobile devices, respectively.
+   * @example
+   * ```html
+   * <gds-grid columns="3 2 1"></gds-grid>
+   * ```
+   */
   @property({ attribute: 'columns', type: String })
   columns?: string
 
+  /**
+   * @property {string} gap - The size of the gap between grid items. This can be a space-separated string of three sizes ('none', 'xs', 's', 'm', 'l', 'xl', '2xl', '3xl'), specifying the gap size for desktop, tablet, and mobile devices, respectively.
+   * @example
+   * ```html
+   * <gds-grid gap="m s xs"></gds-grid>
+   * ```
+   */
   @property({ attribute: 'gap', type: String })
   gap?: GridGap
 
+  /**
+   * @property {string} padding - The size of the padding around the grid. This can be a space-separated string of three sizes ('none', 'xs', 's', 'm', 'l', 'xl', '2xl', '3xl'), specifying the padding size for desktop, tablet, and mobile devices, respectively.
+   * @example
+   * ```html
+   * <gds-grid padding="l m s"></gds-grid>
+   * ```
+   */
   @property({ attribute: 'padding', type: String })
   padding?: GridPadding
 
+  /**
+   * @property {boolean} fluid - A boolean indicating whether the grid should be fluid. If true, the grid will automatically adjust the column size based on the content.
+   * @example
+   * ```html
+   * <gds-grid fluid></gds-grid>
+   * ```
+   */
   @property({ attribute: 'fluid', type: String })
   fluid?: boolean
 
