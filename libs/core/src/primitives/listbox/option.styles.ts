7532a30132cd15767bb9892f0babeea69dfb8880
Joacim Magnusson <joacim.magnusson@seb.se>
2024-04-02 13:25:07 +0200

    Add base 2023 style for the popover primitive (#1285)
    * Add basic default styles for context-menu, option, and popover
    
    * Update 2023 popover style
    
    * Add changeset

--- libs/core/src/primitives/listbox/option.styles.ts
+++ libs/core/src/primitives/listbox/option.styles.ts
@@ -4,12 +4,23 @@ const style = css`
   @layer base, reset, transitional-styles;
   @layer base {
     :host {
-      padding: 0.5 1rem;
+      display: block;
+      padding: 1rem 1.5rem;
       cursor: pointer;
     }
 
+    :host(:not(:last-child)) {
+      border-bottom: 1px solid #e0e0e0;
+    }
+
     :host(:hover) {
-      background-color: grey;
+      background-color: #ededed;
+    }
+
+    :host(:focus-visible) {
+      outline: auto;
+      outline-offset: -6px;
+      outline-color: #666;
     }
   }
 `
