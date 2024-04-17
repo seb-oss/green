631ca398dd96db4c67632c382e3437b9c1a91b4b
Joacim Magnusson <joacim.magnusson@seb.se>
2024-04-17 17:44:41 +0200

    Add doc comments

--- libs/core/src/components/theme/theme.ts
+++ libs/core/src/components/theme/theme.ts
@@ -6,9 +6,23 @@ import {
 } from '../../utils/helpers/custom-element-scoping'
 import { TransitionalStyles } from '../../transitional-styles'
 
+/**
+ * @element gds-theme
+ *
+ * A component that provides CSS variables for a part of the DOM tree.
+ * Every descendant of this component will inherit the CSS variables
+ * provided by the theme set on this component.
+ *
+ * @slot - The content to apply the theme to.
+ *
+ * @status beta
+ */
 @gdsCustomElement('gds-theme')
 export class GdsTheme extends GdsElement {
 
+  /**
+   * The theme mode. Can be `light`, `dark`, or `auto`.
+   */
   @property({ reflect: true })
   mode: 'light' | 'dark' | 'auto' = 'light'
 
