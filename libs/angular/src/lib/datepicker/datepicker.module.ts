c1473004f1faab601f1b23432936b15aaf4abe04
Joacim Magnusson <joacim.magnusson@seb.se>
2024-04-30 12:34:46 +0200

    Remove dropdown dep in datepicker
    This dependency is resolved in the web component

--- libs/angular/src/lib/datepicker/datepicker.module.ts
+++ libs/angular/src/lib/datepicker/datepicker.module.ts
@@ -1,12 +1,11 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { NggDatepickerComponent } from './datepicker.component'
-import { NggDropdownModule } from '@sebgroup/green-angular/src/lib/dropdown'
 import { NggCoreWrapperModule } from '@sebgroup/green-angular/src/lib/shared'
 
 @NgModule({
   declarations: [NggDatepickerComponent],
-  imports: [CommonModule, NggDropdownModule, NggCoreWrapperModule],
+  imports: [CommonModule, NggCoreWrapperModule],
   exports: [NggDatepickerComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
