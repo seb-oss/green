618c5c7e18dd826dd1fc5f5ba7f9a3d8436d9ed0
Vilhelm Sjölander <vilhelm.sjolander@seb.se>
2024-04-30 16:03:24 +0200

    Update
    - Better token filters
    - New custom formats for ios & android to support light/dark values
    - Updated config to output tokens

--- libs/tokens/src/filters.ts
+++ libs/tokens/src/filters.ts
@@ -4,10 +4,10 @@ function getLastWorkInName(token) {
 }
 
 export default {
-  'light-mode': {
-    name: 'light-mode',
+  'color-scheme': {
+    name: 'color-scheme',
     matcher: function (token) {
-      return getLastWorkInName(token) === 'light'
+      return 'darkValue' in token
     },
   },
   'dark-mode': {
@@ -16,10 +16,95 @@ export default {
       return getLastWorkInName(token) === 'dark'
     },
   },
-  'color-scheme': {
-    name: 'color-scheme',
-    matcher: function (token) {
+  is2016Color: {
+    name: 'is2016Color',
+    matcher: (token) => {
+      if (token.version !== '2016') {
+        return false
+      }
       return 'darkValue' in token
+    },
+  },
+  is2016Pallet: {
+    name: 'is2016Pallet',
+    matcher: (token) => {
+      if (token.darkValue) return false
+      return token.version === '2016'
+    },
+  },
+  is2016Ref: {
+    name: 'is2016Ref',
+    matcher: (token) => {
+      if (token.version !== '2016') {
+        return false
+      }
+      return token.path.includes('ref')
+    },
+  },
+  is2023MotionColor: {
+    name: 'is2023MotionColor',
+    matcher: (token) => {
+      if (token.version === '2016') return false
+
+      if (token.path.includes('motion')) {
+        return true
+      }
+
+      return token.type === 'color' && !!token.darkValue
+    },
+  },
+  is2023Color: {
+    name: 'is2023Color',
+    matcher: (token) => {
+      if (token.version === '2016') {
+        return false
+      }
+      return 'darkValue' in token
+    },
+  },
+  is2023Radii: {
+    name: 'is2023Radii',
+    matcher: (token) => {
+      if (token.version === '2016') return false
+      return token.path.includes('radii')
+    },
+  },
+  is2023Ref: {
+    name: 'is2023Ref',
+    matcher: (token) => {
+      if (token.version === '2016') {
+        return false
+      }
+      return token.path.includes('ref')
+    },
+  },
+  is2023Size: {
+    name: 'is2023Size',
+    matcher: (token) => {
+      if (token.version === '2016') return false
+      if (token.path.includes('ref')) return false
+      return token.path.includes('size')
+    },
+  },
+  is2023Sys: {
+    name: 'is2023Sys',
+    matcher: (token) => {
+      if (token.version === '2016') {
+        return false
+      }
+      return token.path.includes('sys')
+    },
+  },
+  isMotion: {
+    name: 'isMotion',
+    matcher: function (token) {
+      return token.path?.includes('motion')
+    },
+  },
+  'light-mode': {
+    name: 'light-mode',
+    matcher: function (token) {
+      return getLastWorkInName(token) === 'light'
     },
   },
 }
--- libs/tokens/src/formats.ts
+++ libs/tokens/src/formats.ts
@@ -1,28 +1,52 @@
-import { Format, formatHelpers } from 'style-dictionary'
+import * as fs from 'fs'
+import * as _template from 'lodash.template'
+import {
+  Format,
+  Options,
+  TransformedToken,
+  formatHelpers,
+} from 'style-dictionary'
+
+console.log(_template)
+
+const {
+  fileHeader,
+  sortByReference,
+  createPropertyFormatter,
+  sortByName,
+  setSwiftFileProperties,
+} = formatHelpers
+
+const useColorScheme = (
+  allTokens: TransformedToken[],
+  options: Options,
+): TransformedToken[] => {
+  return allTokens.map((token) => {
+    const { darkValue } = token
+    // Only override if the token has a darkValue and that you"ve passed in colorScheme: "dark" in the file options.
+    if (darkValue && options.colorScheme === 'dark') {
+      return Object.assign({}, token, {
+        value: token.darkValue,
+        //Also need to override the original value for outputReferences: false to work
+        original: { value: token.original.darkValue },
+      })
+    } else {
+      return token
+    }
+  })
+}
 
 const formats: Record<string, Format> = {
   'css/theme': {
     name: 'css/theme',
     formatter: function (args) {
       const dictionary = Object.assign({}, args.dictionary)
-      const options = Object.assign(
+      const options: Options = Object.assign(
         { colorScheme: 'light', selector: ':root' },
         args.options,
       )
       // Override each token"s `value` with `darkValue`
-      dictionary.allTokens = dictionary.allTokens.map((token) => {
-        const { darkValue } = token
-        // Only override if the token has a darkValue and that you"ve passed in colorScheme: "dark" in the file options.
-        if (darkValue && options.colorScheme === 'dark') {
-          return Object.assign({}, token, {
-            value: token['darkValue'],
-            //Also need to override the original value for outputReferences: false to work
-            original: { value: token.original['darkValue'] },
-          })
-        } else {
-          return token
-        }
-      })
+      dictionary.allTokens = useColorScheme(dictionary.allTokens, options)
 
       // Use the built-in format but with our customized dictionary object
       // so it will output the darkValue instead of the value
@@ -142,6 +166,69 @@ const formats: Record<string, Format> = {
         }) +
         `\n}\n`
       )
+    },
+  },
+  'green/ios-swift-class': {
+    name: 'green/ios-swift-class',
+    formatter: function ({ dictionary, options, file, platform }) {
+      const template = _template(
+        fs.readFileSync(
+          process.cwd() +
+            '/node_modules/style-dictionary/lib/common/templates/ios-swift/any.swift.template',
+        ),
+      )
+
+      /**
+       * Setting fallback for colorScheme to light
+       */
+      options = Object.assign({ colorScheme: 'light' }, options)
+
+      let allTokens
+      const { outputReferences } = options
+      options = setSwiftFileProperties(
+        options,
+        'class',
+        platform.transformGroup,
+      )
+      const formatProperty = createPropertyFormatter({
+        outputReferences,
+        dictionary,
+        formatting: {
+          suffix: '',
+        },
+      })
+
+      if (outputReferences) {
+        allTokens = [...dictionary.allTokens].sort(sortByReference(dictionary))
+      } else {
+        allTokens = [...dictionary.allTokens].sort(sortByName)
+      }
+
+      allTokens = useColorScheme(allTokens, options)
+
+      return template({ allTokens, file, options, formatProperty, fileHeader })
+    },
+  },
+  'green/android-resources': {
+    name: 'green/android-resources',
+    formatter: function ({ dictionary, options, file }) {
+      const template = _template(
+        fs.readFileSync(
+          process.cwd() +
+            '/node_modules/style-dictionary/lib/common/templates/android/resources.template',
+        ),
+      )
+
+      dictionary.allTokens = dictionary.allTokens.map((token) => {
+        token.attributes.category = options.resourceType || 'string'
+        console.log(options.resourceType)
+
+        return token
+      })
+
+      dictionary.allTokens = useColorScheme(dictionary.allTokens, options)
+
+      return template({ dictionary, file, options, fileHeader })
     },
   },
 }
