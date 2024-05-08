9bbd62c3240f24c4a8eae84449464b39ed16aa28
Vilhelm Sjölander <vilhelm.sjolander@seb.se>
2024-05-08 11:16:58 +0200

    Create a common storybook config and migrate angular charts

---
+++ .storybook/main.ts
@@ -0,0 +1,46 @@
+import type { StorybookConfig } from '@storybook/angular'
+const EventHooksPlugin = require('event-hooks-webpack-plugin')
+const copyfiles = require('copyfiles')
+import * as fs from 'fs'
+
+const config: Omit<StorybookConfig, 'framework'> = {
+  stories: ['../src/**/*.mdx', '../**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: ['@storybook/addon-essentials', '@storybook/addon-docs'],
+  webpackFinal: async (config, { configType }) => {
+    config.plugins?.push(
+      new EventHooksPlugin({
+        compile: async () => {
+          fs.access('dist/fonts', async (error) => {
+            if (error) {
+              console.log('Font directory does not exist. Copying fonts...')
+              await new Promise((resolve) =>
+                copyfiles(
+                  ['node_modules/@sebgroup/fonts/fonts/**/*', 'dist/fonts'],
+                  { up: true },
+                  resolve,
+                ),
+              )
+                .catch((_) => [{ success: false }])
+                .then((_) => [{ success: true }])
+            } else {
+              console.log("Font directory already exist. Won't copy fonts...")
+            }
+          })
+        },
+      }),
+    )
+
+    config.module?.rules?.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/i,
+      type: 'asset/resource',
+    })
+
+    return config
+  },
+}
+
+export default config
+
+// To customize your webpack configuration you can use the webpackFinal field.
+// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
+// and https://nx.dev/recipes/storybook/custom-builder-configs
---
+++ .storybook/preview.ts
@@ -0,0 +1,1 @@
+import '!style-loader!css-loader!postcss-loader!sass-loader!./preview.scss'
