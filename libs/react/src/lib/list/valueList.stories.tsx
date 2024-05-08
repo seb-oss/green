f7cb757fbcad4eda15e101fcdc6fe5c85add3b9c
Vilhelm Sjölander <vilhelm.sjolander@seb.se>
2024-05-08 11:33:54 +0200

    migrated react storybook v7

---
+++ libs/react/src/lib/list/valueList.stories.tsx
@@ -0,0 +1,24 @@
+import ValueList from './valueList'
+
+export default {
+  component: ValueList,
+}
+
+export const Primary = {
+  render: (props) => (
+    <ValueList.List {...props}>
+      <ValueList.Label>Ni vill sälja</ValueList.Label>
+      <ValueList.Value>Fritidshus</ValueList.Value>
+      <ValueList.Label>Lån på bostaden</ValueList.Label>
+      <ValueList.Value>1 000 000 kr</ValueList.Value>
+      <ValueList.Label>Ni köpte bostaden för</ValueList.Label>
+      <ValueList.Value>5 000 000 kr</ValueList.Value>
+      <ValueList.Label>Lägsta försäljningsbelopp</ValueList.Label>
+      <ValueList.Value>2 500 000 kr</ValueList.Value>
+      <ValueList.Label>Mäklararvode</ValueList.Label>
+      <ValueList.Value>50 000 kr</ValueList.Value>
+      <ValueList.Label>Kontantinsats från överskottet</ValueList.Label>
+      <ValueList.Value>375 000 kr</ValueList.Value>
+    </ValueList.List>
+  ),
+}
