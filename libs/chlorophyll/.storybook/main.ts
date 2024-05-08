9c7af45a0da6282523bdbcc69b003b049e942156
Vilhelm Sjölander <vilhelm.sjolander@seb.se>
2024-05-08 11:41:53 +0200

    migrated chlorophyll storybook v7

---
+++ libs/chlorophyll/.storybook/main.ts
@@ -0,0 +1,16 @@
+import defaultConfig from '../../../.storybook/main'
+import type { StorybookConfig } from '@storybook/html-webpack5'
+
+const config: StorybookConfig = {
+  ...defaultConfig,
+  framework: {
+    name: '@storybook/html-webpack5',
+    options: {},
+  },
+}
+
+export default config
+
+// To customize your webpack configuration you can use the webpackFinal field.
+// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
+// and https://nx.dev/recipes/storybook/custom-builder-configs
