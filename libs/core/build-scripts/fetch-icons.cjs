f32ee9b442338248778ce2235c3798129012dbd6
astrit <am@astrit.co>
2024-05-21 14:39:44 +0200

    Fetch icons

--- libs/core/build-scripts/fetch-icons.cjs
+++ libs/core/build-scripts/fetch-icons.cjs
@@ -1,18 +1,26 @@
 const axios = require('axios')
-const fs = require('fs')
+const fs = require('fs').promises
 const path = require('path')
 
 const figmaAccessKey = process.env.FIGMA_ACCESS_KEY
 const figmaProjectId = process.env.FIGMA_PROJECT_ID
 const figmaRegularNodeID = process.env.FIGMA_REGULAR_NODE_ID
 const figmaSolidNodeID = process.env.FIGMA_SOLID_NODE_ID
-const figmaSaveTo = path.resolve(__dirname, '../src/components/icon/assets')
+const figmaSaveToRegular = path.resolve(
+  __dirname,
+  '../src/components/icon/assets/regular',
+)
+const figmaSaveToSolid = path.resolve(
+  __dirname,
+  '../src/components/icon/assets/solid',
+)
 
-console.log('Figma access key:', figmaRegularNodeID)
+console.log('Starting script...')
 
-async function fetchAndSaveFigmaIcons() {
+async function fetchAndSaveFigmaIcons(nodeId, savePath) {
+  console.log(`Fetching and saving Figma icons for node ${nodeId}...`)
   try {
-    // Fetch the document
+    console.log('Sending request to Figma API...')
     const docResponse = await axios.get(
       `https://api.figma.com/v1/files/${figmaProjectId}`,
       {
@@ -21,77 +29,65 @@ async function fetchAndSaveFigmaIcons() {
         },
       },
     )
+    console.log('Received response from Figma API.')
 
-    console.log('Figma document:', docResponse.data.document.name)
-    console.log('Figma document children:', docResponse.data.document.children)
-
-    // Traverse the document to find the specific node and its children
     const nodes = []
     const traverse = (node) => {
-      if (node.id === figmaRegularNodeID) {
+      if (node.id === nodeId) {
         console.log('Found node:', node.name)
         if (node.children) {
           node.children.forEach((child) =>
             nodes.push({ id: child.id, name: child.name }),
-          )
-          console.log(
-            'Children:',
-            node.children.map((child) => child.name),
           )
         }
       } else if (node.children) {
         node.children.forEach(traverse)
       }
     }
+    console.log('Traversing document...')
     traverse(docResponse.data.document)
 
-    // Split nodes into chunks of 60
-    const chunks = []
-    while (nodes.length) {
-      chunks.push(nodes.splice(0, 60))
-    }
-
-    // Fetch the images for each node
-    for (const chunk of chunks) {
-      const fetchPromises = chunk.map(async (node) => {
-        const imagesResponse = await axios.get(
-          `https://api.figma.com/v1/images/${figmaProjectId}/?ids=${node.id}&format=svg`,
-          {
-            headers: {
-              'X-Figma-Token': figmaAccessKey,
-            },
+    const fetchPromises = nodes.map(async (node) => {
+      console.log(`Fetching image for node ${node.id}...`)
+      const imagesResponse = await axios.get(
+        `https://api.figma.com/v1/images/${figmaProjectId}/?ids=${node.id}&format=svg`,
+        {
+          headers: {
+            'X-Figma-Token': figmaAccessKey,
           },
-        )
+        },
+      )
 
-        const images = imagesResponse.data.images
-        const imageUrl = images[node.id]
-        if (imageUrl) {
-          axios
-            .get(imageUrl)
-            .then((response) => {
-              let nodeName = node.name.split(',')[0]
-              let fileName = `${nodeName}.svg`
-              let counter = 1
-              while (fs.existsSync(path.join(figmaSaveTo, fileName))) {
-                // If a file with the same name exists, append a number
-                fileName = `${nodeName}-${counter}.svg`
-                counter++
-              }
-              const svgData = response.data
-              fs.writeFileSync(path.join(figmaSaveTo, fileName), svgData)
-              console.log(`Fetched Figma SVG for ${nodeName}`)
-            })
-            .catch((error) => {
-              console.error(`Error fetching Figma SVG`, error)
-            })
-        }
-      })
+      const images = imagesResponse.data.images
+      const imageUrl = images[node.id]
+      if (imageUrl) {
+        console.log(`Fetching SVG from ${imageUrl}...`)
+        const response = await axios.get(imageUrl)
+        let nodeName = node.name.split(',')[0]
+        let fileName = `${nodeName}.svg`
+        const svg = response.data
+        console.log(`Writing SVG to ${path.join(savePath, fileName)}...`)
+        await fs.writeFile(path.join(savePath, fileName), svg)
+        console.log(`Fetched Figma SVG for ${nodeName}`)
+      }
+    })
 
-      await Promise.all(fetchPromises)
-    }
+    console.log('Waiting for all fetch promises to resolve...')
+    await Promise.all(fetchPromises)
+    console.log('All fetch promises resolved.')
   } catch (error) {
     console.error('Error processing Figma SVGS:', error)
   }
 }
 
-fetchAndSaveFigmaIcons()
+async function main() {
+  console.log('Fetching regular icons...')
+  await fetchAndSaveFigmaIcons(figmaRegularNodeID, figmaSaveToRegular)
+  console.log('Fetching solid icons...')
+  await fetchAndSaveFigmaIcons(figmaSolidNodeID, figmaSaveToSolid)
+  console.log('Script finished.')
+}
+
+main().catch((error) => {
+  console.error('Error in main:', error)
+})
--- libs/core/src/components/icon/icon.template.ts
+++ libs/core/src/components/icon/icon.template.ts
@@ -1,8 +1,8 @@
 import { gdsCustomElement } from '../../utils/helpers/custom-element-scoping'
-import { GdsIcon } from './icon'
+import { GdsIcon } from '../icon'
 
-@gdsCustomElement('gds-icon-arrow')
-export class IconArrow extends GdsIcon {
-  static RegularSVG = ``
-  static SolidSVG = ``
+@gdsCustomElement('gds-icon-[NAME]')
+export class IconName extends GdsIcon {
+  static _regularSVG = `[SVG_CONTENT]`
+  static _solidSVG = `[SVG_CONTENT]`
 }
