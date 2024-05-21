334e79092a0af530bb11ceab6812a64f6d3cbe2e
astrit <am@astrit.co>
2024-05-21 13:48:42 +0200

    Convert Icon to abstract class and udpate import method

--- libs/core/src/components/icon/icon.ts
+++ libs/core/src/components/icon/icon.ts
@@ -1,15 +1,14 @@
 import { html, LitElement } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
+import { property } from 'lit/decorators.js'
 import { unsafeHTML } from 'lit/directives/unsafe-html.js'
 
-@customElement('gds-icon')
-export class GdsIcon extends LitElement {
+export abstract class GdsIcon extends LitElement {
   @property({ type: String }) width = 24
   @property({ type: String }) height = 24
   @property({ type: Boolean }) solid = false
 
-  protected _regularSVG?: string
-  protected _solidSVG?: string
+  protected static _regularSVG?: string
+  protected static _solidSVG?: string
 
   render() {
     const svgContent = `<svg
@@ -19,7 +18,7 @@ export class GdsIcon extends LitElement {
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      ${this.solid ? this._solidSVG : this._regularSVG}
+      ${this.solid ? (this.constructor as typeof GdsIcon)._solidSVG : (this.constructor as typeof GdsIcon)._regularSVG}
     </svg>`
 
     return html`${unsafeHTML(svgContent)}`
