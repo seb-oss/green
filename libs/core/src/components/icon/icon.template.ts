3c08c4f0c6a6de39a7a457fc9d8a104113a4761c
astrit <am@astrit.co>
2024-05-22 10:58:14 +0200

    Add icon tests and update some icons

--- libs/core/src/components/icon/icon.template.ts
+++
@@ -1,8 +0,0 @@
-import { gdsCustomElement } from '../../utils/helpers/custom-element-scoping'
-import { GdsIcon } from '../icon'
-
-@gdsCustomElement('gds-icon-[NAME]')
-export class IconName extends GdsIcon {
-  static _regularSVG = `[SVG_CONTENT]`
-  static _solidSVG = `[SVG_CONTENT]`
-}
