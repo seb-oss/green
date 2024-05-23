9e5b3964e0886f76dbd69f1c3af9a53019c78ee2
astrit <am@astrit.co>
2024-05-23 22:09:31 +0200

    Update and format icons

--- libs/core/build-scripts/icons-generate.cjs
+++ libs/core/build-scripts/icons-generate.cjs
@@ -8,6 +8,10 @@ const regularDir = path.resolve(
 const solidDir = path.resolve(__dirname, '../src/components/icon/assets/solid')
 const outputDir = path.resolve(__dirname, '../src/components/icon/icons')
 
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
 function toKebabCase(str) {
   return str.replace(/([a-z0-9]|(?=[A-Z]))([A-Z])/g, '$1-$2').toLowerCase()
 }
@@ -20,9 +24,10 @@ function clearAndUpper(text) {
   return text.replace(/-/, '').toUpperCase()
 }
 
-async function readSvgContent(filePath) {
+async function replaceInFile(filePath) {
   let content = await fs.readFile(filePath, 'utf-8')
   content = content.replace(/#353531/g, 'currentColor')
+  await fs.writeFile(filePath, content, 'utf-8')
   return content.replace(/<\/?svg[^>]*>/g, '')
 }
 
@@ -30,64 +35,67 @@ function generateTsContent(name, regularSvg, solidSvg) {
   const className = `Icon${toPascalCase(name)}`
   const tagName = `gds-icon-${toKebabCase(name)}`
 
-  return `
-    @gdsCustomElement('${tagName}')
-    export class ${className} extends GdsIcon {
-      static _regularSVG = \`${regularSvg}\`
-      static _solidSVG = \`${solidSvg}\`
-    }
-  `.trim()
-}
+  // Remove line breaks and leading/trailing spaces
+  regularSvg = regularSvg.replace(/\s*\n\s*/g, '')
+  solidSvg = solidSvg.replace(/\s*\n\s*/g, '')
 
-const mdxFile = path.resolve(__dirname, '../src/components/icon/icon.list.mdx')
-const tsFile = path.resolve(__dirname, '../src/components/icon/icons/index.ts')
+  return `import { gdsCustomElement } from '../../../utils/helpers/custom-element-scoping'
+import { GdsIcon } from '../icon'
+
+@gdsCustomElement('${tagName}')
+export class ${className} extends GdsIcon {
+  static _regularSVG = \`${regularSvg}\`
+  static _solidSVG = \`${solidSvg}\`
+}`.trim()
+}
 
 async function renameFiles() {
   const regularFiles = await fs.readdir(regularDir)
+  let tsIndexContent = ''
+  let mdxContent = ''
+
   for (const file of regularFiles) {
     if (path.extname(file) === '.svg') {
       let oldName = path.basename(file, '.svg')
       let newName = oldName.split(',')[0] // get the part before the first comma
 
       // Rename the files in the regular and solid directories
-      await fs.rename(
-        path.join(regularDir, file),
-        path.join(regularDir, `${newName}.svg`),
-      )
-      console.log(`Renamed ${oldName} to ${newName} in regular directory`)
-
       try {
         await fs.rename(
-          path.join(solidDir, file),
-          path.join(solidDir, `${newName}.svg`),
-        )
-        console.log(`Renamed ${oldName} to ${newName} in solid directory`)
-      } catch (error) {
-        console.warn(`Failed to rename solid SVG for ${oldName}.`)
-      }
-    }
-  }
-}
-
-async function generateIcons() {
-  try {
-    const regularFiles = await fs.readdir(regularDir)
-    let importContent = ''
-    let elementContent = '<Canvas>\n'
-    for (const file of regularFiles) {
-      if (path.extname(file) === '.svg') {
-        let oldName = path.basename(file, '.svg')
-        let newName = oldName.split(',')[0] // get the part before the first comma
-
-        const regularSvg = await readSvgContent(
+          path.join(regularDir, file),
           path.join(regularDir, `${newName}.svg`),
         )
+        console.log(
+          `%cRenamed ${oldName} to ${newName} in regular directory`,
+          'color: green',
+        )
+        const regularSvg = await replaceInFile(
+          path.join(regularDir, `${newName}.svg`),
+        )
+        console.log(
+          `%cReplaced #353531 with currentColor in ${newName}.svg in regular directory`,
+          'color: blue',
+        )
+
         let solidSvg = ''
         try {
-          solidSvg = await readSvgContent(path.join(solidDir, `${newName}.svg`))
+          await fs.rename(
+            path.join(solidDir, file),
+            path.join(solidDir, `${newName}.svg`),
+          )
+          console.log(
+            `%cRenamed ${oldName} to ${newName} in solid directory`,
+            'color: green',
+          )
+          solidSvg = await replaceInFile(path.join(solidDir, `${newName}.svg`))
+          console.log(
+            `%cReplaced #353531 with currentColor in ${newName}.svg in solid directory`,
+            'color: blue',
+          )
         } catch (error) {
           console.warn(
-            `Failed to read solid SVG for ${newName}, keeping it empty.`,
+            `%cFailed to rename ${oldName} in solid directory. Error: ${error.message}`,
+            'color: orange',
           )
         }
 
@@ -95,26 +103,34 @@ async function generateIcons() {
         await fs.writeFile(path.join(outputDir, `${newName}.ts`), tsContent)
         console.log(`Generated TypeScript file for ${newName}`)
 
-        // Add to import and element content
-        importContent += `export * from './${newName}'\n`
-        elementContent += `<gds-icon-${toKebabCase(newName)}></gds-icon-${toKebabCase(newName)}>\n`
+        tsIndexContent += `export * from './${newName}'\n`
+        mdxContent += `<gds-icon-${toKebabCase(newName)}></gds-icon-${toKebabCase(newName)}>\n`
+
+        // Delay before moving to the next file
+        await delay(1000) // 1 second delay
+      } catch (error) {
+        console.error(
+          `%cFailed to rename ${oldName} in regular directory. Error: ${error.message}`,
+          'color: red',
+        )
+        continue // Skip to the next file
       }
     }
-    elementContent += '</Canvas>\n'
-
-    // Write MDX and TS files
-    await fs.writeFile(mdxFile, elementContent)
-    console.log(`Generated MDX file at ${mdxFile}`)
-    await fs.writeFile(tsFile, importContent)
-    console.log(`Generated TS file at ${tsFile}`)
-  } catch (error) {
-    console.error(error)
   }
+
+  await fs.writeFile(path.join(outputDir, 'index.ts'), tsIndexContent)
+  console.log('Generated index.ts file')
+
+  const mdxFile = path.resolve(
+    __dirname,
+    '../src/components/icon/icon.list.mdx',
+  )
+  await fs.writeFile(mdxFile, mdxContent)
+  console.log('Generated icon.list.mdx file')
 }
 
 async function main() {
   await renameFiles()
-  await generateIcons()
 }
 
 main()
--- libs/core/src/components/icon/icons/minus-large.ts
+++ libs/core/src/components/icon/icons/minus-large.ts
@@ -1,11 +1,8 @@
+import { gdsCustomElement } from '../../../utils/helpers/custom-element-scoping'
+import { GdsIcon } from '../icon'
+
 @gdsCustomElement('gds-icon-minus-large')
-    export class IconMinusLarge extends GdsIcon {
-      static _regularSVG = `
-<path d="M3.75 12H20.25" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>
-
-`
-      static _solidSVG = `
-<path fill-rule="evenodd" clip-rule="evenodd" d="M3 12C3 11.5858 3.33579 11.25 3.75 11.25H20.25C20.6642 11.25 21 11.5858 21 12C21 12.4142 20.6642 12.75 20.25 12.75H3.75C3.33579 12.75 3 12.4142 3 12Z" fill="currentColor"/>
-
-`
-    }
+export class IconMinusLarge extends GdsIcon {
+  static _regularSVG = `<path d="M3.75 12H20.25" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"/>`
+  static _solidSVG = `<path fill-rule="evenodd" clip-rule="evenodd" d="M3 12C3 11.5858 3.33579 11.25 3.75 11.25H20.25C20.6642 11.25 21 11.5858 21 12C21 12.4142 20.6642 12.75 20.25 12.75H3.75C3.33579 12.75 3 12.4142 3 12Z" fill="currentColor"/>`
+}
