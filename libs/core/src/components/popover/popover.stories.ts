60f615e36a60b3f06cec6b1963456bcd41559216
Joacim Magnusson <joacim.magnusson@seb.se>
2024-05-28 10:32:10 +0200

    Update story

--- libs/core/src/components/popover/popover.stories.ts
+++ libs/core/src/components/popover/popover.stories.ts
@@ -1,4 +1,3 @@
-import { html } from 'lit'
 import type { Meta, StoryObj } from '@storybook/web-components'
 
 import '../../../../../dist/libs/core/src/components/popover/index.js'
@@ -44,6 +43,12 @@ const DefaultParams: Story = {
   },
 }
 
-export const Basic: Story = {
+/**
+ * The popover can be used declaratively by adding a trigger button and content inside the popover element, like in the example below.
+ *
+ * It is also possible to set the trigger and anchor by supplying DOM references (wrapped in promises) to the `triggerRef` and `anchorRef`
+ * properties. This is useful when the trigger and anchor needs to be different elements.
+ */
+export const Usage: Story = {
   ...DefaultParams,
 }
