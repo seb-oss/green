d1ddce8c48d0690680c689301308d7004ec81923
Joacim Magnusson <joacim.magnusson@seb.se>
2024-06-10 15:09:39 +0200

    Implement `@styleExpressionProperty` for `gds-grid`

--- libs/core/src/components/grid/grid.style.css.ts
+++ libs/core/src/components/grid/grid.style.css.ts
@@ -1,75 +1,27 @@
 import { css } from 'lit'
 
 const style = css`
-  @layer grid, grid.desktop, grid.tablet, grid.mobile, grid.span, grid.space, grid.align, grid.debug;
-
-  @layer grid {
-    :host {
-      --_c: var(--gds-sys-grid-columns-12);
-      --_grid-col: repeat(var(--_c), 1fr);
-      --_grid-col-start: 1;
-      --_grid-col-end: -1;
-      --_gap-column: 0;
-      --_gap-row: 0;
-      display: grid;
-      width: 100%;
-      grid-template-columns: var(--_grid-col);
-      grid-column-gap: var(--_gap-column);
-      grid-row-gap: var(--_gap-row);
-      padding: var(--_grid-padding);
-    }
-
-    :host([auto-columns]) {
-      --_col-count: var(--_c, 0);
-      --_gap-count: calc(var(--_col-count) - 1);
-      --_total-gap-width: calc(var(--_gap-count) * var(--_gap-column, 0px));
-      --_col-width-max: calc(
-        (100% - var(--_total-gap-width)) / var(--_col-count)
-      );
-      grid-template-columns: repeat(
-        auto-fill,
-        minmax(max(var(--_col-width), var(--_col-width-max)), 1fr)
-      );
-    }
-  }
-
-  /* Responsive */
-
-  :host([columns]) {
-    --_c: var(--_columns-desktop);
-  }
-
   :host {
-    --_gap-column: var(--_gap-desktop);
-    --_gap-row: var(--_row-gap-desktop);
-    --_grid-padding: var(--_padding-desktop);
-    --_col-width: var(--_col-width-desktop);
+    --_c: 12;
+    --_grid-col: repeat(var(--_c), 1fr);
+    display: grid;
+    width: 100%;
+    grid-template-columns: var(--_grid-col);
+    grid-column-gap: var(--_gap-column, 0px);
+    grid-row-gap: var(--_gap-row, 0px);
   }
 
-  @media only screen and (max-width: 768px) {
-    :host([columns]) {
-      --_c: var(--_columns-tablet);
-    }
-
-    :host {
-      --_gap-column: var(--_gap-tablet);
-      --_gap-row: var(--_row-gap-tablet);
-      --_grid-padding: var(--_padding-tablet);
-      --_col-width: var(--_col-width-tablet);
-    }
-  }
-
-  @media only screen and (max-width: 425px) {
-    :host([columns]) {
-      --_c: var(--_columns-mobile);
-    }
-
-    :host {
-      --_gap-column: var(--_gap-mobile);
-      --_gap-row: var(--_row-gap-mobile);
-      --_grid-padding: var(--_padding-mobile);
-      --_col-width: var(--_col-width-mobile);
-    }
+  :host([auto-columns]) {
+    --_col-count: var(--_c, 0);
+    --_gap-count: calc(var(--_col-count) - 1);
+    --_total-gap-width: calc(var(--_gap-count) * var(--_gap-column, 0px));
+    --_col-width-max: calc(
+      (100% - var(--_total-gap-width)) / var(--_col-count)
+    );
+    grid-template-columns: repeat(
+      auto-fill,
+      minmax(max(var(--_col-width), var(--_col-width-max)), 1fr)
+    );
   }
 `
 export default style
