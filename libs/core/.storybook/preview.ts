29f5366d052b77c5429c62d475e653c9b1f15983
Joacim Magnusson <joacim.magnusson@seb.se>
2024-06-12 13:49:06 +0200

    Re-enable transitional styles switcher

--- libs/core/.storybook/preview.ts
+++ libs/core/.storybook/preview.ts
@@ -36,35 +36,35 @@ export default {
   decorators: [(storyFn: any) => html`<gds-theme>${storyFn()}</gds-theme>`],
 }
 
-// class TransitionalStylesToggle extends HTMLElement {
-//   connectedCallback() {
-//     const grid = document.createElement('gds-grid')
-//     grid.setAttribute('gap', 's')
-//     grid.style.marginBottom = '1rem'
-//     this.appendChild(grid)
+class TransitionalStylesToggle extends HTMLElement {
+  connectedCallback() {
+    const grid = document.createElement('gds-grid')
+    grid.setAttribute('gap', 's')
+    grid.style.marginBottom = '1rem'
+    this.appendChild(grid)
 
-//     const show2016Button = document.createElement('gds-button')
-//     show2016Button.setAttribute('rank', 'secondary')
-//     show2016Button.textContent = 'Show 2016 styles'
-//     show2016Button.onclick = () => this.#showTransitionalStyles()
-//     grid.appendChild(show2016Button)
+    const show2016Button = document.createElement('gds-button')
+    show2016Button.setAttribute('rank', 'secondary')
+    show2016Button.textContent = 'Show 2016 styles'
+    show2016Button.onclick = () => this.#showTransitionalStyles()
+    grid.appendChild(show2016Button)
 
-//     const show2023Button = document.createElement('gds-button')
-//     show2023Button.setAttribute('rank', 'secondary')
-//     show2023Button.textContent = 'Show 2023 styles'
-//     show2023Button.onclick = () => location.reload()
-//     grid.appendChild(show2023Button)
-//   }
+    const show2023Button = document.createElement('gds-button')
+    show2023Button.setAttribute('rank', 'secondary')
+    show2023Button.textContent = 'Show 2023 styles'
+    show2023Button.onclick = () => location.reload()
+    grid.appendChild(show2023Button)
+  }
 
-//   #showTransitionalStyles() {
-//     registerTransitionalStyles()
-//     document.querySelectorAll('[gds-element]').forEach((el: any) => {
-//       el.connectedCallback()
-//     })
-//   }
-// }
+  #showTransitionalStyles() {
+    registerTransitionalStyles()
+    document.querySelectorAll('[gds-element]').forEach((el: any) => {
+      el.connectedCallback()
+    })
+  }
+}
 
-// window.customElements.define(
-//   'transitional-styles-toggle',
-//   TransitionalStylesToggle,
-// )
+window.customElements.define(
+  'transitional-styles-toggle',
+  TransitionalStylesToggle,
+)
