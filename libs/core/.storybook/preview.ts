08b0fb362211d605459c41ebe86f6b604eb780bd
Joacim Magnusson <joacim.magnusson@seb.se>
2024-06-12 16:24:16 +0200

    Improve performance when developing in Storybook (#1431)
    * Update core Storybook to use Vite and build components directly
    
    * Revert to pointing default path alias for core to dist
    
    * Remove reference to old `tsconfig.storybook.json`
    
    * Fix an outdated import
    
    * Update tokens imports
    
    * Re-enable transitional styles switcher
    
    * Some cleanup
    
    * Try adding `.nojekyll` file
    
    * Revert adding `.nojekyll` in build step, as it is now added in root
    
    * Add empty changeset
    
    * Rewrite transitional styles toggle as a Lit element
    
    * Fix transitional styles in SB

--- libs/core/.storybook/preview.ts
+++ libs/core/.storybook/preview.ts
@@ -1,13 +1,17 @@
 import { setCustomElementsManifest } from '@storybook/web-components'
+import { LitElement, html } from 'lit'
+import { customElement } from 'lit/decorators.js'
+
 import DocTemplate from './DocTemplate.mdx'
 import customElements from '../custom-elements.json'
-import { registerTransitionalStyles } from '../../../dist/libs/core/src/transitional-styles.js'
 
-import '!style-loader!css-loader!postcss-loader!sass-loader!./fonts.scss'
+import { registerTransitionalStyles } from '../src/transitional-styles.ts'
 
-import '../../../dist/libs/core/src/components/button/index.js'
-import '../../../dist/libs/core/src/components/grid/index.js'
-import { html } from 'lit'
+import './fonts.scss'
+
+import '../src/components/theme/index.js'
+import '../src/components/button/index.js'
+import '../src/components/grid/index.js'
 
 setCustomElementsManifest(customElements)
 
@@ -36,24 +40,10 @@ export default {
   decorators: [(storyFn: any) => html`<gds-theme>${storyFn()}</gds-theme>`],
 }
 
-class TransitionalStylesToggle extends HTMLElement {
-  connectedCallback() {
-    const grid = document.createElement('gds-grid')
-    grid.setAttribute('gap', 's')
-    grid.style.marginBottom = '1rem'
-    this.appendChild(grid)
-
-    const show2016Button = document.createElement('gds-button')
-    show2016Button.setAttribute('rank', 'secondary')
-    show2016Button.textContent = 'Show 2016 styles'
-    show2016Button.onclick = () => this.#showTransitionalStyles()
-    grid.appendChild(show2016Button)
-
-    const show2023Button = document.createElement('gds-button')
-    show2023Button.setAttribute('rank', 'secondary')
-    show2023Button.textContent = 'Show 2023 styles'
-    show2023Button.onclick = () => location.reload()
-    grid.appendChild(show2023Button)
+@customElement('transitional-styles-toggle')
+class TransitionalStylesToggle extends LitElement {
+  protected createRenderRoot() {
+    return this
   }
 
   #showTransitionalStyles() {
@@ -62,9 +52,20 @@ class TransitionalStylesToggle extends HTMLElement {
       el.connectedCallback()
     })
   }
-}
 
-window.customElements.define(
-  'transitional-styles-toggle',
-  TransitionalStylesToggle,
-)
+  render() {
+    return html`<gds-theme>
+      <gds-grid gap="s" style="margin-bottom: 1rem">
+        <gds-button
+          rank="secondary"
+          @click=${() => this.#showTransitionalStyles()}
+        >
+          Show 2016 styles
+        </gds-button>
+        <gds-button rank="secondary" @click=${() => location.reload()}>
+          Show 2023 styles
+        </gds-button>
+      </gds-grid>
+    </gds-theme>`
+  }
+}
