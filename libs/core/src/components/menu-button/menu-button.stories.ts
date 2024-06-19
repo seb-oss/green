4c88816ac4e291a5f74cd0ddcef13b52053c293e
Joacim Magnusson <joacim.magnusson@seb.se>
2024-06-19 17:33:51 +0200

    Fix import and remove disabled code

--- libs/core/src/components/menu-button/menu-button.stories.ts
+++ libs/core/src/components/menu-button/menu-button.stories.ts
@@ -1,101 +1,118 @@
-import type { Meta, StoryObj } from '@storybook/web-components';
+import type { Meta, StoryObj } from '@storybook/web-components'
 
 // import { html } from 'lit';
-import { html } from '@sebgroup/green-core/scoping'
-import './menu-button';
+import { html } from '../../scoping'
+import './menu-button'
 
-import '@sebgroup/green-core/components/icon/icons/magnifying-glass.js';
-import '@sebgroup/green-core/components/icon/icons/bell.js';
-import '@sebgroup/green-core/components/icon/icons/star.js';
-import '@sebgroup/green-core/components/icon/icons/people.js';
-import '@sebgroup/green-core/components/icon/icons/globus.js';
-import '../berry/berry';
+import '../icon/icons/magnifying-glass.js'
+import '../icon/icons/bell.js'
+import '../icon/icons/star.js'
+import '../icon/icons/people.js'
+import '../icon/icons/globus.js'
+//import '../berry/berry'
 
 const meta: Meta = {
-  title: 'Intranet/Menu button',
-  component: 'menu-button',
+  title: 'Components/Menu button',
+  component: 'gds-menu-button',
   tags: ['autodocs'],
-};
+}
 
-export default meta;
-type Story = StoryObj;
+export default meta
+type Story = StoryObj
 
 export const Default: Story = {
-  render: () => html`
-    <menu-button slot="lead">
-      <gds-icon-magnifying-glass width="24" height="24" slot="lead"></gds-icon-magnifying-glass>
-        Search
-    </menu-button>`
-};
+  render: () =>
+    html` <gds-menu-button>
+      <gds-icon-magnifying-glass
+        width="24"
+        height="24"
+        slot="lead"
+      ></gds-icon-magnifying-glass>
+      Search
+    </gds-menu-button>`,
+}
 export const Example_selected_state: Story = {
-  render: () => html`
-    <menu-button slot="trail" selected>
-      <gds-icon-magnifying-glass width="24" height="24" solid slot="lead"></gds-icon-magnifying-glass>
-        Search
-    </menu-button>`
-};
+  render: () =>
+    html` <gds-menu-button selected>
+      <gds-icon-magnifying-glass
+        width="24"
+        height="24"
+        solid
+        slot="lead"
+      ></gds-icon-magnifying-glass>
+      Search
+    </gds-menu-button>`,
+}
 export const Notification: Story = {
-  render: () => html`
-    <menu-button slot="trail">
-        <gds-icon-bell width="24" height="24" slot="trail"></gds-icon-bell>
-        Notification
-    </menu-button>`
-};
+  render: () =>
+    html` <gds-menu-button>
+      <gds-icon-bell width="24" height="24" slot="trail"></gds-icon-bell>
+      Notification
+    </gds-menu-button>`,
+}
 export const Notification_unread: Story = {
-  render: () => html`
-    <menu-button slot="trail">
+  render: () =>
+    html` <gds-menu-button>
         <berry-component slot="trail">3</berry-component>
         <gds-icon-bell width="24" height="24" slot="trail"></gds-icon-bell>
         Notification
-    </menu-button>
-    <menu-button slot="trail">
+      </gds-menu-button>
+      <gds-menu-button>
         <gds-icon-bell width="24" height="24" slot="trail"></gds-icon-bell>
         Notification
-    </menu-button>
-    <menu-button slot="trail">
-        <gds-icon-bell width="24" height="24"  slot="trail"></gds-icon-bell>
+      </gds-menu-button>
+      <gds-menu-button>
+        <gds-icon-bell width="24" height="24" slot="trail"></gds-icon-bell>
         Notification
-    </menu-button>`
-};
+      </gds-menu-button>`,
+}
 export const My_shortcuts: Story = {
-  render: () => html`
-    <menu-button slot="trail">
-        <gds-icon-star width="24" height="24" slot="trail"></gds-icon-star>
-        My shortcuts
-    </menu-button>`
-};
+  render: () =>
+    html` <gds-menu-button>
+      <gds-icon-star width="24" height="24" slot="trail"></gds-icon-star>
+      My shortcuts
+    </gds-menu-button>`,
+}
 export const Profile: Story = {
-  render: () => html`
-    <menu-button slot="trail">
-        <gds-icon-people width="24" height="24" slot="trail"></gds-icon-people>
-        Profile
-    </menu-button>`
-};
-export const Search: Story = {
-  render: () => html`
-    <menu-button slot="trail">
-      <gds-icon-magnifying-glass width="24" height="24" slot="lead"></gds-icon-magnifying-glass>
-        Search
-    </menu-button>`
-};
-export const Settings: Story = {
-  render: () => html`
-    <menu-button slot="trail">
-    <gds-icon-globus width="24" height="24" slot="trail"></gds-icon-globus>
-      Other countries
-    </menu-button>`
-};
-export const Dubble_icon: Story = {
-  render: () => html`
-    <menu-button slot="trail">
-      <gds-icon-magnifying-glass width="24" height="24" slot="lead"></gds-icon-magnifying-glass>
+  render: () =>
+    html` <gds-menu-button>
       <gds-icon-people width="24" height="24" slot="trail"></gds-icon-people>
-        Search a user
-    </menu-button>`
-};
+      Profile
+    </gds-menu-button>`,
+}
+export const Search: Story = {
+  render: () =>
+    html` <gds-menu-button>
+      <gds-icon-magnifying-glass
+        width="24"
+        height="24"
+        slot="lead"
+      ></gds-icon-magnifying-glass>
+      Search
+    </gds-menu-button>`,
+}
+export const Settings: Story = {
+  render: () =>
+    html` <gds-menu-button>
+      <gds-icon-globus width="24" height="24" slot="trail"></gds-icon-globus>
+      Other countries
+    </gds-menu-button>`,
+}
+export const Dubble_icon: Story = {
+  render: () =>
+    html` <gds-menu-button>
+      <gds-icon-magnifying-glass
+        width="24"
+        height="24"
+        slot="lead"
+      ></gds-icon-magnifying-glass>
+      <gds-icon-people width="24" height="24" slot="trail"></gds-icon-people>
+      Search a user
+    </gds-menu-button>`,
+}
 export const About_SEB: Story = {
-  render: () => html`
-    <menu-button type="button" label="button">
-        About SEB
-    </menu-button>`
-};
+  render: () =>
+    html` <gds-menu-button type="button" label="button">
+      About SEB
+    </gds-menu-button>`,
+}
--- libs/core/src/components/menu-button/menu-button.ts
+++ libs/core/src/components/menu-button/menu-button.ts
@@ -1,14 +1,13 @@
 import { unsafeCSS } from 'lit'
-import { property, customElement } from 'lit/decorators.js'
-import { constrainSlots } from '../green-temp/helpers'
+import { property } from 'lit/decorators.js'
+import { classMap } from 'lit/directives/class-map.js'
+import { GdsElement } from '../../gds-element'
+import { gdsCustomElement, html } from '../../scoping'
+import { constrainSlots } from '../../utils/helpers'
+import { tokens } from '../../tokens.style'
 
-import { tokens } from '../green-temp/tokens.style'
 import style from './menu-button.css?inline'
 
-import { classMap } from 'lit/directives/class-map.js'
-import { GdsFormControlElement } from '../green-temp/form-control'
-
-import { html } from '@sebgroup/green-core/scoping'
 //import { GdsIcon } from '@sebgroup/green-core/components/icon/icon.js'
 
 // Create a customized `html` template tag that strips whitespace and applies custom element scoping.
@@ -25,8 +24,8 @@ import { html } from '@sebgroup/green-core/scoping'
  *
  * @event click - Fired when the button is clicked.
  */
-@customElement('menu-button')
-export class MenuButton<ValueT = any> extends GdsFormControlElement<ValueT> {
+@gdsCustomElement('gds-menu-button')
+export class MenuButton extends GdsElement {
   static styles = [tokens, unsafeCSS(style)]
 
   static shadowRootOptions: ShadowRootInit = {
@@ -41,27 +40,6 @@ export class MenuButton<ValueT = any> extends GdsFormControlElement<ValueT> {
   disabled = false
 
   /**
-   * The type of the button. Defaults to "button".
-   */
-  @property({ reflect: true })
-  type: "button" | "submit" | "reset" | "menu" = "button"
-    /**
-     * The variant of the button. Defaults to "primary".
-     */
-
-  /**
-   * The variant of the button. Defaults to "primary".
-   */
-  // @property({ reflect: true })
-  // variant: 'primary' | 'secondary' | 'riffle' | 'tertiary' = 'primary'
-
-  /**
-   * Defines which set the button belongs to. Defaults to "neutral".
-   */
-  // @property({ reflect: true })
-  // set: 'neutral' | 'positive' | 'negative' = 'neutral'
-
-  /**
    * Sets the size of the menu-button. Defaults to "medium".
    */
   @property({ reflect: true })
@@ -71,32 +49,19 @@ export class MenuButton<ValueT = any> extends GdsFormControlElement<ValueT> {
    * The label of the menu-button. Use this to add an accessible label to the button when no text is provided in the default slot.
    */
   @property()
-  label = '';
+  label = ''
 
   /**
    * The label of the menu-button. Use this to add an accessible label to the button when no text is provided in the default slot.
    */
   @property({ type: Boolean })
-  compactview: boolean = false;
+  compactview: boolean = false
 
   /**
-  * Whether the menu button is selected
-  */
+   * Whether the menu button is selected
+   */
   @property({ type: Boolean })
-  selected: boolean = false;
-
-  // handleMenuButtonSelection = () => {
-  //   this.selected = !this.selected;
-  //   const leadElement = this.querySelector('[slot=lead]') as GdsIcon;
-  //   if(leadElement) {
-  //     leadElement.solid = !leadElement.solid;
-  //   } 
-    
-  //   const tailElement = this.querySelector('[slot=trail]') as GdsIcon;
-  //   if(tailElement) {
-  //     tailElement.solid = !tailElement.solid;
-  //   }
-  // }
+  selected: boolean = false
 
   constructor() {
     super()
@@ -107,11 +72,10 @@ export class MenuButton<ValueT = any> extends GdsFormControlElement<ValueT> {
     const classes = {
       selected: this.selected,
       compactview: this.compactview,
-    };
+    }
     return html`
       <button
-        class="intra-menu-button${classMap(classes)}"
-        type="${this.type}"
+        class="${classMap(classes)}"
         ?disabled="${this.disabled}"
         aria-label="${this.label}"
       >
@@ -120,6 +84,5 @@ export class MenuButton<ValueT = any> extends GdsFormControlElement<ValueT> {
         <slot name="trail"></slot>
       </button>
     `
-    //@click=${this.handleMenuButtonSelection}
   }
 }
