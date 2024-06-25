4175a91dda1225855172901c8f2a890ad602c293
Joacim Magnusson <joacim.magnusson@seb.se>
2024-06-25 15:53:10 +0200

    Remove icon sizing from story

--- libs/core/src/components/menu-button/menu-button.stories.ts
+++ libs/core/src/components/menu-button/menu-button.stories.ts
@@ -9,7 +9,6 @@ import '../icon/icons/bell.js'
 import '../icon/icons/star.js'
 import '../icon/icons/people.js'
 import '../icon/icons/globus.js'
-//import '../berry/berry'
 
 const meta: Meta = {
   title: 'Components/Menu button',
@@ -22,97 +21,78 @@ type Story = StoryObj
 
 export const Default: Story = {
   render: () =>
-    html` <gds-menu-button>
-      <gds-icon-magnifying-glass
-        width="24"
-        height="24"
-        slot="lead"
-      ></gds-icon-magnifying-glass>
+    html`<gds-menu-button compactview>
+      <gds-icon-magnifying-glass slot="lead"></gds-icon-magnifying-glass>
       Search
     </gds-menu-button>`,
 }
 export const Example_selected_state: Story = {
   render: () =>
-    html` <gds-menu-button selected>
-      <gds-icon-magnifying-glass
-        width="24"
-        height="24"
-        solid
-        slot="lead"
-      ></gds-icon-magnifying-glass>
+    html`<gds-menu-button selected>
+      <gds-icon-magnifying-glass solid slot="lead"></gds-icon-magnifying-glass>
       Search
     </gds-menu-button>`,
 }
 export const Notification: Story = {
   render: () =>
-    html` <gds-menu-button>
-      <gds-icon-bell width="24" height="24" slot="trail"></gds-icon-bell>
+    html`<gds-menu-button>
+      <gds-icon-bell slot="trail"></gds-icon-bell>
       Notification
     </gds-menu-button>`,
 }
 export const Notification_unread: Story = {
   render: () =>
-    html` <gds-menu-button>
+    html`<gds-menu-button>
         <berry-component slot="trail">3</berry-component>
-        <gds-icon-bell width="24" height="24" slot="trail"></gds-icon-bell>
+        <gds-icon-bell slot="trail"></gds-icon-bell>
         Notification
       </gds-menu-button>
       <gds-menu-button>
-        <gds-icon-bell width="24" height="24" slot="trail"></gds-icon-bell>
+        <gds-icon-bell slot="trail"></gds-icon-bell>
         Notification
       </gds-menu-button>
       <gds-menu-button>
-        <gds-icon-bell width="24" height="24" slot="trail"></gds-icon-bell>
+        <gds-icon-bell slot="trail"></gds-icon-bell>
         Notification
       </gds-menu-button>`,
 }
 export const My_shortcuts: Story = {
   render: () =>
-    html` <gds-menu-button>
-      <gds-icon-star width="24" height="24" slot="trail"></gds-icon-star>
+    html`<gds-menu-button>
+      <gds-icon-star slot="trail"></gds-icon-star>
       My shortcuts
     </gds-menu-button>`,
 }
 export const Profile: Story = {
   render: () =>
     html` <gds-menu-button>
-      <gds-icon-people width="24" height="24" slot="trail"></gds-icon-people>
+      <gds-icon-people slot="trail"></gds-icon-people>
       Profile
     </gds-menu-button>`,
 }
 export const Search: Story = {
   render: () =>
     html` <gds-menu-button>
-      <gds-icon-magnifying-glass
-        width="24"
-        height="24"
-        slot="lead"
-      ></gds-icon-magnifying-glass>
+      <gds-icon-magnifying-glass slot="lead"></gds-icon-magnifying-glass>
       Search
     </gds-menu-button>`,
 }
 export const Settings: Story = {
   render: () =>
     html` <gds-menu-button>
-      <gds-icon-globus width="24" height="24" slot="trail"></gds-icon-globus>
+      <gds-icon-globus slot="trail"></gds-icon-globus>
       Other countries
     </gds-menu-button>`,
 }
 export const Dubble_icon: Story = {
   render: () =>
     html` <gds-menu-button>
-      <gds-icon-magnifying-glass
-        width="24"
-        height="24"
-        slot="lead"
-      ></gds-icon-magnifying-glass>
-      <gds-icon-people width="24" height="24" slot="trail"></gds-icon-people>
+      <gds-icon-magnifying-glass slot="lead"></gds-icon-magnifying-glass>
+      <gds-icon-people slot="trail"></gds-icon-people>
       Search a user
     </gds-menu-button>`,
 }
 export const About_SEB: Story = {
   render: () =>
-    html` <gds-menu-button type="button" label="button">
-      About SEB
-    </gds-menu-button>`,
+    html`<gds-menu-button label="button"> About SEB </gds-menu-button>`,
 }
