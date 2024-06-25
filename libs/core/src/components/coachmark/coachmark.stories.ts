2f70b55d187ae6f0f317270ebf5c2eea6d1a396e
Daniel <osama.al-midani@outlook.com>
2024-06-26 00:10:54 +0200

    render basic story

--- libs/core/src/components/coachmark/coachmark.stories.ts
+++ libs/core/src/components/coachmark/coachmark.stories.ts
@@ -33,91 +33,25 @@ let dummy = {
     title: 'This is a coachmark title',
     content: ['This is the coachmark content.'],
     closeButton: { text: 'Close', ariaLabel: 'Close tooltip' },
-    preferredPlacement: 'bottom',
+    preferredPlacement: 'top',
     target: ['#targetElement'],
   },
 }
 
 export const Basic: Story = {
   ...DefaultParams,
-  args: {
-    coachmark: {
-      id: 'test-coachmark',
-      variant: 'dark',
-      tooltip: {
-        title: 'This is a coachmark title',
-        content: ['This is the coachmark content.'],
-        closeButton: { text: 'Close', ariaLabel: 'Close tooltip' },
-        preferredPlacement: 'bottom',
-        target: ['#targetElement'],
-      },
-    },
-  },
-  render:  () => {
+  args: {},
+  render: () => {
+    return html`
+      <h2 id="targetElement">target Element</h2>
 
-   return html`
-      <style>
-        .gds-coachmark {
-          visibility: hidden;
-          position: absolute;
-          z-index: 1060;
-          box-sizing: border-box;
-          padding: 1rem;
-          width: 20rem;
-          border-radius: 0.25rem;
-          background-color: #222;
-          color: #fff;
-          opacity: 0;
-          transition: opacity 0.3s;
-        }
-
-        #gds-arrow {
-          box-sizing: border-box;
-          z-index: -1;
-          position: absolute;
-          height: 1rem;
-          width: 1rem;
-          background-color: #222;
-          transform: rotate(45deg);
-        }
-
-        .arrow-top,
-        .arrow-top-start,
-        .arrow-top-end {
-          bottom: -0.5rem;
-        }
-
-        .arrow-bottom,
-        .arrow-bottom-start,
-        .arrow-bottom-end {
-          top: -0.5rem;
-        }
-
-        .arrow-left,
-        .arrow-left-start,
-        .arrow-left-end {
-          right: -0.5rem;
-        }
-
-        .arrow-right,
-        .arrow-right-start,
-        .arrow-right-end {
-          left: -0.5rem;
-        }
-      </style>
-      <div id="targetElement">target Element</div>
       ${document.getElementById('targetElement')
         ? html`
-            <gds-coachmark _isVisible="${true}" .coachmark=${dummy}>
-              <div
-                class="gds-coachmark"
-                aria-modal="false"
-                style="visibility:visible;opacity:1;left:0;right:0"
-              >
-                <div slot="body">'This is the coachmark content.</div>
-                <div slot="footer">Button</div>
-                <div id="gds-arrow"></div>
+            <gds-coachmark .coachmark=${dummy}>
+              <div class="gds-coachmark" slot="body" aria-modal="false">
+                <div>${dummy.tooltip.content}</div>
               </div>
+              <div id="gds-arrow"></div>
             </gds-coachmark>
           `
         : html``}
