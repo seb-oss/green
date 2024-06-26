597a206fa28d3b1eac6f4cc991482788403a85dd
Joacim Magnusson <joacim.magnusson@seb.se>
2024-06-26 11:41:48 +0200

    Update controls in story

--- libs/core/src/components/menu-button/menu-button.stories.ts
+++ libs/core/src/components/menu-button/menu-button.stories.ts
@@ -13,34 +13,52 @@ import '../icon/icons/globus.js'
 const meta: Meta = {
   title: 'Components/Menu button',
   component: 'gds-menu-button',
+  parameters: {
+    layout: 'centered',
+  },
   tags: ['autodocs'],
 }
 
 export default meta
 type Story = StoryObj
 
-export const Default: Story = {
-  render: () =>
-    html`<gds-menu-button compact>
-      <gds-icon-magnifying-glass slot="lead"></gds-icon-magnifying-glass>
-      Search
-    </gds-menu-button>`,
+const DefaultParams: Story = {
+  parameters: {
+    docs: {
+      source: { format: true, type: 'dynamic' },
+    },
+    controls: { expanded: true },
+  },
+  args: {
+    innerHTML: `<gds-icon-magnifying-glass slot="lead"></gds-icon-magnifying-glass>
+    Search`,
+  },
 }
+
+export const Basic: Story = {
+  ...DefaultParams,
+}
+
 export const Example_selected_state: Story = {
+  ...DefaultParams,
   render: () =>
     html`<gds-menu-button selected>
       <gds-icon-magnifying-glass solid slot="lead"></gds-icon-magnifying-glass>
       Search
     </gds-menu-button>`,
 }
+
 export const Notification: Story = {
+  ...DefaultParams,
   render: () =>
     html`<gds-menu-button>
       <gds-icon-bell slot="trail"></gds-icon-bell>
       Notification
     </gds-menu-button>`,
 }
+
 export const Notification_unread: Story = {
+  ...DefaultParams,
   render: () =>
     html`<gds-menu-button>
         <gds-icon-bell slot="trail"></gds-icon-bell>
@@ -55,43 +73,39 @@ export const Notification_unread: Story = {
         Notification
       </gds-menu-button>`,
 }
+
 export const My_shortcuts: Story = {
+  ...DefaultParams,
   render: () =>
     html`<gds-menu-button>
       <gds-icon-star slot="trail"></gds-icon-star>
       My shortcuts
     </gds-menu-button>`,
 }
+
 export const Profile: Story = {
+  ...DefaultParams,
   render: () =>
     html` <gds-menu-button>
       <gds-icon-people slot="trail"></gds-icon-people>
       Profile
     </gds-menu-button>`,
 }
+
 export const Search: Story = {
+  ...DefaultParams,
   render: () =>
     html` <gds-menu-button>
       <gds-icon-magnifying-glass slot="lead"></gds-icon-magnifying-glass>
       Search
     </gds-menu-button>`,
 }
+
 export const Settings: Story = {
+  ...DefaultParams,
   render: () =>
     html` <gds-menu-button>
       <gds-icon-globus slot="trail"></gds-icon-globus>
       Other countries
     </gds-menu-button>`,
-}
-export const Dubble_icon: Story = {
-  render: () =>
-    html` <gds-menu-button>
-      <gds-icon-magnifying-glass slot="lead"></gds-icon-magnifying-glass>
-      <gds-icon-people slot="trail"></gds-icon-people>
-      Search a user
-    </gds-menu-button>`,
-}
-export const About_SEB: Story = {
-  render: () =>
-    html`<gds-menu-button label="button"> About SEB </gds-menu-button>`,
 }
