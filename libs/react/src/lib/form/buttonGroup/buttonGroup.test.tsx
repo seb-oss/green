5a44e0bc15d7317b740b73a456d2d0d0be6a6060
terrance456 <terranceprasath97@gmail.com>
2024-07-03 16:21:09 +0800

    feat: 🎸 enable scrolling when group-btn overflow

--- libs/react/src/lib/form/buttonGroup/buttonGroup.test.tsx
+++ libs/react/src/lib/form/buttonGroup/buttonGroup.test.tsx
@@ -1,5 +1,6 @@
-import { fireEvent, render, screen } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import { Button, ButtonGroup } from '..'
+import userEvent, { UserEvent } from '@testing-library/user-event'
 
 describe('ButtonGroup', () => {
   const buttonClick = jest.fn()
@@ -12,25 +13,68 @@ describe('ButtonGroup', () => {
     )
   }
 
+  window.HTMLElement.prototype.scrollIntoView = jest.fn()
+
+  const enableScrolling = async (container: HTMLElement) => {
+    const element = container.querySelector('.btn-group') as HTMLElement
+    const scrollEvent: Event = new Event('scroll')
+    Object.defineProperties(element, {
+      scrollWidth: { value: 500, writable: true, configurable: true },
+      clientWidth: { value: 400, writable: true, configurable: true },
+      scrollLeft: { value: 50, writable: true, configurable: true },
+    })
+    Object.defineProperty(scrollEvent, 'currentTarget', { value: element })
+    await waitFor(() => {
+      element.dispatchEvent(scrollEvent)
+    })
+  }
+
   it('renders single button', () => {
     render(
       <ButtonGroup>
         <Button>Hello</Button>
       </ButtonGroup>,
     )
-
     expect(screen.getAllByRole('button')).toHaveLength(1)
   })
   it('renders buttons', () => {
     render(<MockButtonGroup />)
-
     expect(screen.getAllByRole('button')).toHaveLength(2)
   })
 
-  it('should click button', () => {
-    render(<MockButtonGroup />)
-
-    fireEvent.click(screen.getAllByRole('button')[0])
+  it('should click button', async () => {
+    const { container } = render(<MockButtonGroup />)
+    await enableScrolling(container)
+    await waitFor(() => {
+      window.dispatchEvent(new Event('resize'))
+    })
+    fireEvent.click(screen.getAllByText('Hello')[0])
+    await waitFor(() => {
+      expect(screen.getAllByText('Hello')).toHaveLength(2)
+    })
     expect(buttonClick).toBeCalled()
+  })
+
+  it('Should enable left right control button for scrolling', async () => {
+    const { container } = render(<MockButtonGroup />)
+    await enableScrolling(container)
+    expect(container.querySelector('.control-btn-right')).toBeInTheDocument()
+    expect(container.querySelector('.control-btn-left')).toBeInTheDocument()
+  })
+
+  it('Should scroll left and right', async () => {
+    const user: UserEvent = userEvent.setup()
+    const { container } = render(<MockButtonGroup />)
+    const element = container.querySelector('.btn-group') as HTMLElement
+    await enableScrolling(container)
+    await user.click(
+      container.querySelector('.control-btn-right') as HTMLElement,
+    )
+    await user.click(
+      container.querySelector('.control-btn-left') as HTMLElement,
+    )
+    await waitFor(() => {
+      expect(element.scrollLeft).toBe(50)
+    })
   })
 })
--- libs/react/src/lib/form/buttonGroup/buttonGroup.tsx
+++ libs/react/src/lib/form/buttonGroup/buttonGroup.tsx
@@ -1,7 +1,13 @@
-import { ReactElement, useEffect, useState } from 'react'
-import { ButtonVariant } from '@sebgroup/extract'
+import React, {
+  ReactElement,
+  RefObject,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
+import { ButtonSize, ButtonVariant } from '@sebgroup/extract'
 import { Button, ButtonProps } from '../button/button'
-import { Group } from '../'
 import classNames from 'classnames'
 
 interface ButtonGroupProps {
@@ -10,30 +16,51 @@ interface ButtonGroupProps {
   className?: string
   selectedIndex?: number
   variant?: ButtonVariant
-  id?: string
+  size?: ButtonSize
 }
 type Props = ButtonProps & JSX.IntrinsicAttributes
+
+const chevronRightIcon = (
+  <svg
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+  >
+    <path d="M8.772 5.19066L8.15329 5.80935C8.00685 5.95579 8.00685 6.19322 8.15329 6.33969L13.8001 12L8.15329 17.6603C8.00685 17.8068 8.00685 18.0442 8.15329 18.1907L8.772 18.8094C8.91844 18.9558 9.15588 18.9558 9.30235 18.8094L15.8465 12.2652C15.993 12.1188 15.993 11.8813 15.8465 11.7348L9.30235 5.19066C9.15588 5.04419 8.91844 5.04419 8.772 5.19066Z" />
+  </svg>
+)
+
+const chevronLeftIcon = (
+  <svg
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+  >
+    <path d="M15.2278 18.8094L15.8465 18.1906C15.993 18.0442 15.993 17.8068 15.8465 17.6603L10.1997 12L15.8465 6.3397C15.993 6.19326 15.993 5.95583 15.8465 5.80936L15.2278 5.19064C15.0814 5.0442 14.8439 5.0442 14.6975 5.19064L8.15329 11.7349C8.00685 11.8813 8.00685 12.1187 8.15329 12.2652L14.6975 18.8094C14.8439 18.9558 15.0814 18.9558 15.2278 18.8094Z" />
+  </svg>
+)
+
 export const ButtonGroup = ({
   children,
   className,
   selectedIndex,
   variant,
-  id,
+  size,
 }: ButtonGroupProps) => {
   const [selected, setSelected] = useState(selectedIndex)
-  const [buttons, setButtons] = useState<Props[]>([])
+  const buttonGroupRef: RefObject<HTMLDivElement> = useRef<HTMLDivElement>(null)
+  const [showLeftArrow, setShowLeftArrow] = useState<boolean>(false)
+  const [showRightArrow, setShowRightArrow] = useState<boolean>(false)
+  const [isOverflowing, setIsOverflowing] = React.useState<boolean>(false)
 
-  useEffect(() => {
-    const buttonProps: Props[] = (
-      children instanceof Array ? children : [children]
-    )
+  const buttons: Props[] = useMemo(() => {
+    return (children instanceof Array ? children : [children])
       .filter((b) => b && (b as ReactElement).props)
       .map((b, ix) => {
         const bp = (b as ReactElement).props
         const props: Props = {
           ...bp,
           variant,
-          key: bp.key || `btn_${ix}`,
           active: ix === selected,
           onClick: (e) => {
             setSelected(ix)
@@ -43,15 +70,75 @@ export const ButtonGroup = ({
         }
         return props
       })
-    setButtons(buttonProps)
   }, [children, selected, variant, className])
 
+  const onClickLeftArrow = () => {
+    const currentRef = buttonGroupRef.current as HTMLDivElement
+    currentRef.scrollLeft -= Math.round(currentRef.scrollWidth / buttons.length)
+  }
+
+  const onClickRightArrow = () => {
+    const currentRef = buttonGroupRef.current as HTMLDivElement
+    currentRef.scrollLeft += Math.round(currentRef.scrollWidth / buttons.length)
+  }
+
+  const onScroll = (event: React.UIEvent<HTMLDivElement>) => {
+    const currentTarget: HTMLDivElement = event.currentTarget
+    const maxScroll: number =
+      currentTarget.scrollWidth - currentTarget.clientWidth
+    setShowRightArrow(maxScroll - Math.round(currentTarget.scrollLeft) > 2)
+    setShowLeftArrow(currentTarget.scrollLeft > 0)
+  }
+
+  useEffect(() => {
+    const checkOverflow = () => {
+      const currentRef = buttonGroupRef.current as HTMLDivElement
+      setShowRightArrow(currentRef.scrollWidth > currentRef.clientWidth)
+      setIsOverflowing(currentRef.scrollWidth > currentRef.clientWidth)
+    }
+    checkOverflow()
+    window.addEventListener('resize', checkOverflow)
+    return () => {
+      window.removeEventListener('resize', checkOverflow)
+    }
+  }, [])
+
+  useEffect(() => {
+    if (selected !== undefined && isOverflowing) {
+      buttonGroupRef.current
+        ?.querySelectorAll('.btn-group button')
+        [selected]?.scrollIntoView({ behavior: 'smooth', inline: 'center' })
+    }
+  }, [selected, isOverflowing])
+
   return (
-    <Group data-label="button-group" id={id}>
-      {buttons.map((props) => (
-        <Button key={props.key} {...props} />
-      ))}
-    </Group>
+    <div className="button-group-wrapper">
+      {showLeftArrow && (
+        <Button
+          onClick={onClickLeftArrow}
+          size={size}
+          className="control-btn-left"
+          variant={variant}
+        >
+          {chevronLeftIcon}
+        </Button>
+      )}
+      <div className="group btn-group" ref={buttonGroupRef} onScroll={onScroll}>
+        {buttons.map((props: Props, index: number) => (
+          <Button {...props} key={index} size={size} />
+        ))}
+      </div>
+      {showRightArrow && (
+        <Button
+          onClick={onClickRightArrow}
+          size={size}
+          className="control-btn-right"
+          variant={variant}
+        >
+          {chevronRightIcon}
+        </Button>
+      )}
+    </div>
   )
 }
 
