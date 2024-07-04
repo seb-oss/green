a1f64597eab0546de02f5987dbd42bb87d64aa61
terrance456 <terranceprasath97@gmail.com>
2024-07-05 00:01:05 +0800

    feat: 🎸 Gds-Segemented component react wrapper

---
+++ libs/react/src/lib/segmented-control/segmented-control.spec.tsx
@@ -0,0 +1,28 @@
+import { render, screen } from '@testing-library/react'
+import { SegmentedControl, Segment } from './segmented-control'
+
+class ResizeObserver {
+  observe() {
+    return
+  }
+  unobserve() {
+    return
+  }
+  disconnect() {
+    return {}
+  }
+}
+
+describe('SegmentedControl', () => {
+  ;(window as any).ResizeObserver = ResizeObserver
+  it('Should render', () => {
+    render(
+      <SegmentedControl>
+        <Segment>Button 1</Segment>
+        <Segment>Button 2</Segment>
+        <Segment>Button 3</Segment>
+      </SegmentedControl>,
+    )
+    expect(screen.getAllByText(/Button/)).toHaveLength(3)
+  })
+})
---
+++ libs/react/src/lib/segmented-control/segmented-control.tsx
@@ -0,0 +1,65 @@
+import React, { ReactNode } from 'react'
+import { createComponent } from '@lit/react'
+import { GdsSegmentedControl } from '@sebgroup/green-core/components/segmented-control/index.js'
+import { GdsSegment } from '@sebgroup/green-core/components/segmented-control/segment/index.js'
+import { getScopedTagName } from '@sebgroup/green-core/scoping'
+import { registerTransitionalStyles } from '@sebgroup/green-core/transitional-styles'
+import classNames from 'classnames'
+
+registerTransitionalStyles()
+
+export const CoreSegementedControl = createComponent({
+  tagName: getScopedTagName('gds-segmented-control'),
+  elementClass: GdsSegmentedControl,
+  events: { onchange: 'change' },
+  react: React,
+})
+
+export const CoreSegment = createComponent({
+  tagName: getScopedTagName('gds-segment'),
+  elementClass: GdsSegment,
+  react: React,
+})
+
+export interface SegmentedControlProps {
+  value?: string
+  segMinWidth?: number
+  size?: 'small' | 'medium'
+  onChange?: (value: string) => void
+  className?: string
+  children?: ReactNode | ReactNode[]
+}
+
+export interface SegmentProps {
+  value?: string
+  className?: string
+  disabled?: boolean
+  children?: ReactNode | ReactNode[]
+}
+
+const SegmentedControl: React.FC<SegmentedControlProps> = ({
+  onChange,
+  className,
+  ...props
+}: SegmentedControlProps) => {
+  const onControlChange = (event: Event) => {
+    onChange && onChange((event?.target as HTMLInputElement).value)
+  }
+
+  return (
+    <CoreSegementedControl
+      onchange={onControlChange}
+      className={classNames(className)}
+      {...props}
+    />
+  )
+}
+
+const Segment: React.FC<SegmentProps> = ({
+  className,
+  ...props
+}: SegmentProps) => {
+  return <CoreSegment className={classNames(className)} {...props} />
+}
+
+export { SegmentedControl, Segment }
