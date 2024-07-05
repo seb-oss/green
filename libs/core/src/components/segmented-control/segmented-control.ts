7f9dbd5239677a46e38683b060183875a5c7a9ea
Joacim Magnusson <joacim.magnusson@seb.se>
2024-07-05 10:38:42 +0200

    Add disabled style to 2023 version

--- libs/core/src/components/segmented-control/segmented-control.ts
+++ libs/core/src/components/segmented-control/segmented-control.ts
@@ -13,7 +13,7 @@ import '../../components/icon/icons/chevron-left'
 import '../../components/icon/icons/chevron-right'
 
 import { tokens } from '../../tokens.style'
-import style from './segmented-control.style.css'
+import style from './segmented-control.style.css?inline'
 
 const BTN_SIZE = {
   small: 36,
