e80a426ee8d40b52b0d500786a27dc326411bc76
astrit <am@astrit.co>
2024-08-12 08:56:02 +0200

    Fix the flex issue

--- libs/core/src/components/dropdown/dropdown.styles.ts
+++ libs/core/src/components/dropdown/dropdown.styles.ts
@@ -5,37 +5,28 @@ const style = css`
 
   @layer base {
     button {
-      transition:
-        0.3s cubic-bezier(0.23, 1, 0.32, 1),
-        outline-offset,
-        outline-width;
-      cursor: pointer;
-      font-family: inherit;
-      // padding: 0.75rem 1rem;
-      border: solid 1px #000;
-      background: #fff;
-      color: #000;
-      min-height: 2.75rem;
       display: flex;
       flex-wrap: nowrap;
       justify-content: space-between;
       align-items: center;
-      max-width: 100%;
-      font-size: inherit;
-      font-weight: 400;
-      line-height: 1.125;
-      // margin-bottom: 0.5rem;
-      // margin-top: 0.5rem;
-      width: 100%;
-      // New
-      display: flex;
       gap: 1ch;
       border-radius: var(--gds-space-2xs);
       margin-block: var(--gds-space-2xs);
       font-size: var(--gds-text-size-body-large);
+      font-weight: var(--gds-text-weight-regular);
       padding-inline: var(--gds-space-s);
       padding-block: var(--gds-space-xs);
+      background-color: var(--gds-sys-color-container-container-bright);
       border: 1px solid var(--gds-sys-color-stroke-stroke);
+      width: 100%;
+      max-width: 100%;
+      cursor: pointer;
+
+      // Motion
+      transition:
+        0.3s cubic-bezier(0.23, 1, 0.32, 1),
+        outline-offset,
+        outline-width;
 
       &:hover {
         background-color: var(--gds-sys-color-container-container-dim1);
