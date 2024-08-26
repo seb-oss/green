0fab969e758fdaf7e9d91cee824a3cea4f7f414f
Joacim Magnusson <joacim.magnusson@seb.se>
2024-08-26 16:13:13 +0200

    Dialogs: correct a11y label and modality state (#1527)
    * Add accessible label for modals
    
    * Add changeset
    
    * Only make modal when trapFocus is active
    
    * Add changset
    
    * Fully specify crypto lib
    
    * Make crypto lib available in jest setup

---
+++ .changeset/empty-dryers-remain.md
@@ -0,0 +1,5 @@
+---
+"@sebgroup/green-angular": patch
+---
+
+**Modals:** Fix accessible label
--- libs/angular/src/test-setup.ts
+++ libs/angular/src/test-setup.ts
@@ -1,1 +1,4 @@
 import 'jest-preset-angular/setup-jest'
+
+import { randomUUID } from 'node:crypto'
+globalThis.crypto.randomUUID = randomUUID
