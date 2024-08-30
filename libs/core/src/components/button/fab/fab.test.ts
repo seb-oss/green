b542092ff0d971c369460b9be0b0fb289e52f887
Joacim Magnusson <joacim.magnusson@seb.se>
2024-08-30 14:58:33 +0200

    Update tests

--- libs/core/src/components/button/fab/fab.test.ts
+++ libs/core/src/components/button/fab/fab.test.ts
@@ -4,8 +4,8 @@ import { sendKeys, sendMouse } from '@web/test-runner-commands'
 import { clickOnElement, conditionToBeTrue, timeout } from '../../utils/testing'
 import sinon from 'sinon'
 
-import '@sebgroup/green-core/components/fab'
-import type { GdsFab } from '@sebgroup/green-core/components/fab'
+import '@sebgroup/green-core/components/button/fab'
+import type { GdsFab } from '@sebgroup/green-core/components/button/fab'
 
 import {
   htmlTemplateTagFactory,
@@ -27,7 +27,26 @@ describe('<gds-fab>', () => {
     })
   })
 
-  describe('API', () => {})
+  describe('API', () => {
+    it('has the `position` property', async () => {
+      const el = await fixture<GdsFab>(html`<gds-fab></gds-fab>`)
+      expect(el.position).to.be.undefined
+      el.position = 'fixed'
+      expect(el.position).to.equal('fixed')
+    })
 
-  describe('Interactions', () => {})
+    it('has the `transform` property', async () => {
+      const el = await fixture<GdsFab>(html`<gds-fab></gds-fab>`)
+      expect(el.transform).to.be.undefined
+      el.transform = 'translate(10px, 10px)'
+      expect(el.transform).to.equal('translate(10px, 10px)')
+    })
+
+    it('has the `inset` property', async () => {
+      const el = await fixture<GdsFab>(html`<gds-fab></gds-fab>`)
+      expect(el.inset).to.be.undefined
+      el.inset = 'auto 5rem 5rem auto'
+      expect(el.inset).to.equal('auto 5rem 5rem auto')
+    })
+  })
 })
