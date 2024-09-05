9bd175ed89a41db761cda20ff916ea1114d48f9f
astrit <astrit@users.noreply.github.com>
2024-09-05 09:39:15 +0000

    Deploying to gh-pages from @ seb-oss/green@71980abbd5356171a0232ab9e7e331f8dd28595b 🚀

---
+++ pr-1560/core/assets/DocsRenderer-NNNQARDV-0e83269e.js
@@ -0,0 +1,1 @@
+import{D as a,d}from"./chunk-HLWAVYOI-7aae1f81.js";import"./iframe-df9a9dba.js";import"../sb-preview/runtime.js";import"./index-1ac06a16.js";import"./_commonjsHelpers-725317a4.js";import"./index-d38538b0.js";import"./index-356e4a49.js";export{a as DocsRenderer,d as defaultComponents};
--- pr-1560/core/assets/theme-938ad005.js
+++
@@ -1,5 +0,0 @@
-import{n as m,g as p,G as h,h as i}from"./gds-element-54cd6e2a.js";import{T as u}from"./transitional-styles-1a2d368e.js";import{i as f}from"./lit-element-71e04f06.js";var d=Object.defineProperty,g=Object.getOwnPropertyDescriptor,a=(n,t,o,s)=>{for(var e=s>1?void 0:s?g(t,o):t,l=n.length-1,c;l>=0;l--)(c=n[l])&&(e=(s?c(t,o,e):c(e))||e);return s&&e&&d(t,o,e),e};let r=class extends h{constructor(){super(...arguments),this.colorScheme="light"}connectedCallback(){super.connectedCallback(),u.instance.apply(this,"gds-theme")}render(){return i`<slot></slot>`}};r.styles=f`
-    :host {
-      display: contents;
-    }
-  `;a([m({reflect:!0,attribute:"color-scheme"})],r.prototype,"colorScheme",2);r=a([p("gds-theme")],r);
