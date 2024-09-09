dd96744640666a3f4b9677344116b4fc15dc8455
astrit <astrit@users.noreply.github.com>
2024-09-09 08:14:32 +0000

    Deploying to gh-pages from @ seb-oss/green@fb15c740fd30e5633544969dd90be8d599d01c93 🚀

---
+++ pr-1560/core/assets/syntaxhighlighter-B5GMVT5T-64e81ae8.js
@@ -0,0 +1,1 @@
+import{S as e,c as s,s as c}from"./index-ce1ffa64.js";import"./iframe-b67a0839.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./index-d38538b0.js";import"./index-356e4a49.js";export{e as SyntaxHighlighter,s as createCopyToClipboardFunction,c as default};
