bceebb1065b44ecfbd771b0cf7fe581c8772abd2
splashdust <splashdust@users.noreply.github.com>
2024-09-09 08:38:46 +0000

    Deploying to gh-pages from @ seb-oss/green@fb15c740fd30e5633544969dd90be8d599d01c93 🚀

--- pr-1569/core/assets/input.stories-a5857192.js
+++
@@ -1,9 +0,0 @@
-import"./input-bec70a75.js";import"./lit-element-da1d3957.js";import"./gds-element-7aeb5dea.js";import"./_commonjsHelpers-725317a4.js";import"./query-b9d3c2af.js";import"./query-async-1ff18261.js";import"./until-b31a68a9.js";import"./directive-helpers-a760bbfe.js";import"./async-directive-d03ddf37.js";import"./directive-12249aa5.js";import"./when-cf7256a5.js";import"./lit-localize-87611c26.js";import"./constrain-slots-08d8606c.js";import"./watch-c4961afe.js";import"./form-control-86c8becc.js";import"./tokens.style-8a0fa4c3.js";const I={title:"Docs/Components/Input",component:"gds-input",parameters:{layout:"centered",docs:{description:{component:`[Source code](https://github.com/seb-oss/green/tree/main/libs/core/src/components/input)
-&nbsp;|&nbsp;
-[Usage guidelines](https://seb.io/component/input)
-
-@status beta
-
-Input fields allow users to enter text into a UI. They typically appear in forms and dialogs.`}}},tags:["autodocs"]},s={parameters:{docs:{source:{format:!0,type:"dynamic"}},controls:{expanded:!0}},args:{label:"Label text"}},t={...s};var e,o,r;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
-  ...DefaultParams
-}`,...(r=(o=t.parameters)==null?void 0:o.docs)==null?void 0:r.source}}};const B=["Basic"];export{t as Basic,B as __namedExportsOrder,I as default};
