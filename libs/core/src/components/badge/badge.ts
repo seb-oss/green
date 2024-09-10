303e402c7d63a19cdd6375fe871d8741cd2595e4
astrit <am@astrit.co>
2024-09-10 22:23:02 +0200

    Update badge

---
+++ libs/core/src/components/badge/badge.ts
@@ -0,0 +1,69 @@
+import { html } from 'lit/static-html.js'
+import { unsafeCSS } from 'lit'
+import { gdsCustomElement } from '../../utils/helpers/custom-element-scoping'
+import { GdsElement } from '../../gds-element'
+import { tokens } from '../../tokens.style'
+import { styleExpressionProperty } from '../../utils/decorators/style-expression-property'
+
+import BadgeCSS from './badge.style.css?inline'
+
+/**
+ * `gds-badge`
+ *
+ * @element gds-badge
+ * @status beta
+ *
+ */
+
+@gdsCustomElement('gds-badge')
+export class GdsBadge extends GdsElement {
+  static styles = [tokens, unsafeCSS(BadgeCSS)]
+
+  @styleExpressionProperty({
+    valueTemplate: (v) => `${v}`,
+    selector: '.badge',
+    styleTemplate: (prop, values) => {
+      const variant = values[0]
+      const variantMapping: { [key: string]: { bg: string; color: string } } = {
+        information: {
+          bg: 'information',
+          color: 'information',
+        },
+        notice: {
+          bg: 'notice',
+          color: 'notice',
+        },
+        success: {
+          bg: 'positive',
+          color: 'positive',
+        },
+        warning: {
+          bg: 'warning',
+          color: 'warning',
+        },
+        error: {
+          bg: 'negative',
+          color: 'negative',
+        },
+      }
+
+      const tokens = variantMapping[variant] || {
+        bg: 'default-bg',
+        color: 'default-content',
+      }
+      const BG = `background-color: var(--gds-color-l3c-background-${tokens.bg}-secondary);`
+      const CL = `color: var(--gds-color-l3c-content-${tokens.color});`
+      const style = `${BG} ${CL}`
+      return style
+    },
+  })
+  variant?: string
+
+  render() {
+    return html`<div class="badge">
+      <slot name="lead"></slot>
+      <slot></slot>
+      <slot name="trail"></slot>
+    </div>`
+  }
+}
