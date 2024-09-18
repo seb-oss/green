62b7df9ffc2d718eb3d0b7c23917d082f68b1dd8
Joacim Magnusson <joacim.magnusson@seb.se>
2024-09-18 16:41:47 +0200

    Add support for pseudo-selectors in styleExpressionProperties

---
+++ libs/core/src/utils/helpers/style-expression-parser-tester.ts
@@ -0,0 +1,16 @@
+// This file is here to help with development of the style expression parser, since the regular
+// tests are a bit clunky for this kind of work.
+//
+// Run it like this: `npx tsx libs/core/src/utils/helpers/style-expression-parser-tester.ts`
+
+import { inspect } from 'util'
+import { tokenize, parse, toCss } from './style-expression-parser'
+
+const expression = 'block; hover:flex; xl { focus:block; flex }'
+const ast = parse(tokenize(expression))
+const css = toCss('div', 'padding', ast)
+console.log(
+  'AST',
+  inspect(ast, { showHidden: false, depth: null, colors: true }),
+)
+console.log(inspect(css, { showHidden: false, depth: null, colors: true }))
