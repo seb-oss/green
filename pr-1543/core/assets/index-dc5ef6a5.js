bd27db756440a3e63a2779511d9baf4dccc16fca
astrit <astrit@users.noreply.github.com>
2024-09-18 15:19:57 +0000

    Deploying to gh-pages from @ seb-oss/green@336531c5f5204a3cf1f7e6774d9c5e5aeaddda05 🚀

---
+++ pr-1543/core/assets/index-dc5ef6a5.js
@@ -0,0 +1,1 @@
+import{M,u as a}from"./index-55a1c9f0.js";import"./index-3226b986.js";import"./iframe-a7c01e1a.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./index-65ec3394.js";import"./index-356e4a49.js";export{M as MDXProvider,a as useMDXComponents};
---
+++ pr-1543/core/assets/input.stories-dd14d6b6.js
@@ -0,0 +1,477 @@
+import{k as s}from"./lit-element-1d72f0ce.js";import"./input-85ff2b8f.js";import"./badge-14ea8264.js";import"./flex-d3b80d4d.js";import"./divider-c2c10b07.js";import"./credit-card-aed5fef2.js";import"./magnifying-glass-0fce063b.js";import"./gds-element-86064462.js";import"./_commonjsHelpers-725317a4.js";import"./query-b9d3c2af.js";import"./query-async-1ff18261.js";import"./until-27bb1490.js";import"./directive-helpers-8f06cd33.js";import"./async-directive-1808f33b.js";import"./directive-dd518ee3.js";import"./when-748fcf30.js";import"./runtime-c06dc943.js";import"./constrain-slots-08d8606c.js";import"./watch-c4961afe.js";import"./button-c89fd615.js";import"./static-c0094392.js";import"./if-defined-5f576255.js";import"./class-map-f04c1558.js";import"./transitional-styles-8ea9588a.js";import"./tokens.style-b5b8efd2.js";import"./cross-small-7e719cb3.js";import"./icon-a640baec.js";import"./triangle-exclamation-da39395f.js";import"./container-9164f049.js";import"./style-expression-property-0e3d4d40.js";import"./text-4793c6cc.js";const P={title:"Docs/Components/Form/Input",component:"gds-input",parameters:{layout:"centered",docs:{description:{component:`[Source code](https://github.com/seb-oss/green/tree/main/libs/core/src/components/input)
+&nbsp;|&nbsp;
+[Usage guidelines](https://seb.io/component/input)
+
+@status beta
+
+Input fields allow users to enter text into a UI. They typically appear in forms and dialogs.`}}},tags:["autodocs"]},i={parameters:{docs:{source:{format:!0,type:"dynamic"}},controls:{expanded:!0}},args:{label:"Label text",multiline:!1}},t={...i,name:"All",render:()=>s`
+    <gds-flex flex-direction="column" gap="2xl">
+      <gds-flex gap="xl">
+        <gds-input label="Label" supporting-text="Label support text">
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+        <gds-input
+          label="Label"
+          supporting-text="Label support text"
+          value=" "
+          clearable
+        >
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+      </gds-flex>
+      <gds-flex padding="xl 0 0 0" flex-direction="column">
+        <gds-text>Validation</gds-text>
+        <gds-divider></gds-divider>
+      </gds-flex>
+      <gds-flex gap="xl">
+        <gds-input
+          label="Label"
+          supporting-text="Label support text"
+          value=" "
+          .invalid=${!0}
+          .validator=${{validate:e=>{if(e.value==="")return[{...e.validity,valid:!1,customError:!0},"Custom error message"];if(e.value.length!==12||isNaN(e.value))return[{...e.validity,valid:!1,customError:!0},"The value must be 12 characters long."]}}}
+        >
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+        <gds-input
+          label="Label"
+          supporting-text="Label support text"
+          value=" "
+          .invalid=${!0}
+          .validator=${{validate:e=>{if(e.value==="")return[{...e.validity,valid:!1,customError:!0},"Custom error message"];if(e.value.length!==12||isNaN(e.value))return[{...e.validity,valid:!1,customError:!0},"The value must be 12 characters long."]}}}
+          clearable
+        >
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+      </gds-flex>
+
+      <gds-flex padding="xl 0 0 0" flex-direction="column">
+        <gds-text>Disabled</gds-text>
+        <gds-divider></gds-divider>
+      </gds-flex>
+      <gds-flex gap="xl">
+        <gds-input label="Label" disabled supporting-text="Label support text">
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+        <gds-input
+          label="Label"
+          disabled
+          supporting-text="Label support text"
+          value=" "
+          clearable
+        >
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+      </gds-flex>
+      <gds-flex padding="xl 0 0 0" flex-direction="column">
+        <gds-text tag="h2">Small</gds-text>
+        <gds-divider></gds-divider>
+      </gds-flex>
+      <gds-flex gap="xl">
+        <gds-input
+          label="Label"
+          size="small"
+          supporting-text="Label support text"
+        >
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+        <gds-input
+          label="Label"
+          size="small"
+          supporting-text="Label support text"
+          value=" "
+          clearable
+        >
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+      </gds-flex>
+      <gds-flex padding="xl 0 0 0" flex-direction="column">
+        <gds-text>Validation</gds-text>
+        <gds-divider></gds-divider>
+      </gds-flex>
+      <gds-flex gap="xl">
+        <gds-input
+          label="Label"
+          size="small"
+          supporting-text="Label support text"
+          value=" "
+          .invalid=${!0}
+          .validator=${{validate:e=>{if(e.value==="")return[{...e.validity,valid:!1,customError:!0},"Custom error message"];if(e.value.length!==12||isNaN(e.value))return[{...e.validity,valid:!1,customError:!0},"The value must be 12 characters long."]}}}
+        >
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+        <gds-input
+          label="Label"
+          size="small"
+          supporting-text="Label support text"
+          value=" "
+          .invalid=${!0}
+          .validator=${{validate:e=>{if(e.value==="")return[{...e.validity,valid:!1,customError:!0},"Custom error message"];if(e.value.length!==12||isNaN(e.value))return[{...e.validity,valid:!1,customError:!0},"The value must be 12 characters long."]}}}
+          clearable
+        >
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+      </gds-flex>
+
+      <gds-flex padding="xl 0 0 0" flex-direction="column">
+        <gds-text>Disabled</gds-text>
+        <gds-divider></gds-divider>
+      </gds-flex>
+      <gds-flex gap="xl">
+        <gds-input
+          label="Label"
+          size="small"
+          disabled
+          supporting-text="Label support text"
+        >
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+        <gds-input
+          label="Label"
+          size="small"
+          disabled
+          supporting-text="Label support text"
+          value=" "
+          clearable
+        >
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+      </gds-flex>
+
+      <gds-flex padding="xl 0 0 0" flex-direction="column">
+        <gds-text>Search & Badge</gds-text>
+        <gds-divider></gds-divider>
+      </gds-flex>
+
+      <gds-flex gap="xl">
+        <gds-input label="Label" supporting-text="Label support text">
+          <gds-icon-magnifying-glass slot="lead"></gds-icon-magnifying-glass>
+        </gds-input>
+        <gds-input
+          label="Label"
+          supporting-text="Label support text"
+          value="Input"
+        >
+          <gds-badge variant="information" slot="trail">KR</gds-badge>
+        </gds-input>
+      </gds-flex>
+      <gds-flex gap="xl">
+        <gds-input
+          label="Label"
+          .invalid="${!0}"
+          supporting-text="Label support text"
+        >
+          <gds-icon-magnifying-glass slot="lead"></gds-icon-magnifying-glass>
+        </gds-input>
+        <gds-input
+          label="Label"
+          .invalid="${!0}"
+          supporting-text="Label support text"
+          value="Input"
+        >
+          <gds-badge variant="error" slot="trail">KR</gds-badge>
+        </gds-input>
+      </gds-flex>
+      <gds-flex padding="xl 0 0 0" flex-direction="column">
+        <gds-text tag="h2">Multiline</gds-text>
+        <gds-divider></gds-divider>
+      </gds-flex>
+      <gds-flex gap="xl">
+        <gds-input
+          label="Label"
+          supporting-text="Label support text"
+          maxlength="140"
+          multiline
+        >
+        </gds-input>
+        <gds-input
+          label="Label"
+          supporting-text="Label support text"
+          maxlength="140"
+          multiline
+        >
+          <gds-badge variant="error" slot="trail">KR</gds-badge>
+        </gds-input>
+      </gds-flex>
+      <gds-flex gap="xl">
+        <gds-input
+          label="Label"
+          supporting-text="Label support text"
+          maxlength="140"
+          .invalid=${!0}
+          multiline
+        >
+        </gds-input>
+        <gds-input
+          label="Label"
+          supporting-text="Label support text"
+          maxlength="140"
+          .invalid=${!0}
+          multiline
+        >
+          <gds-badge variant="error" slot="trail">KR</gds-badge>
+        </gds-input>
+      </gds-flex>
+    </gds-flex>
+  `};var l,d,a;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  ...DefaultParams,
+  name: 'All',
+  render: () => html\`
+    <gds-flex flex-direction="column" gap="2xl">
+      <gds-flex gap="xl">
+        <gds-input label="Label" supporting-text="Label support text">
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+        <gds-input
+          label="Label"
+          supporting-text="Label support text"
+          value=" "
+          clearable
+        >
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+      </gds-flex>
+      <gds-flex padding="xl 0 0 0" flex-direction="column">
+        <gds-text>Validation</gds-text>
+        <gds-divider></gds-divider>
+      </gds-flex>
+      <gds-flex gap="xl">
+        <gds-input
+          label="Label"
+          supporting-text="Label support text"
+          value=" "
+          .invalid=\${true}
+          .validator=\${{
+    validate: (el: any) => {
+      if (el.value === '') return [{
+        ...el.validity,
+        valid: false,
+        customError: true
+      }, 'Custom error message'];else if (el.value.length !== 12 || isNaN(el.value)) return [{
+        ...el.validity,
+        valid: false,
+        customError: true
+      }, 'The value must be 12 characters long.'];
+    }
+  }}
+        >
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+        <gds-input
+          label="Label"
+          supporting-text="Label support text"
+          value=" "
+          .invalid=\${true}
+          .validator=\${{
+    validate: (el: any) => {
+      if (el.value === '') return [{
+        ...el.validity,
+        valid: false,
+        customError: true
+      }, 'Custom error message'];else if (el.value.length !== 12 || isNaN(el.value)) return [{
+        ...el.validity,
+        valid: false,
+        customError: true
+      }, 'The value must be 12 characters long.'];
+    }
+  }}
+          clearable
+        >
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+      </gds-flex>
+
+      <gds-flex padding="xl 0 0 0" flex-direction="column">
+        <gds-text>Disabled</gds-text>
+        <gds-divider></gds-divider>
+      </gds-flex>
+      <gds-flex gap="xl">
+        <gds-input label="Label" disabled supporting-text="Label support text">
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+        <gds-input
+          label="Label"
+          disabled
+          supporting-text="Label support text"
+          value=" "
+          clearable
+        >
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+      </gds-flex>
+      <gds-flex padding="xl 0 0 0" flex-direction="column">
+        <gds-text tag="h2">Small</gds-text>
+        <gds-divider></gds-divider>
+      </gds-flex>
+      <gds-flex gap="xl">
+        <gds-input
+          label="Label"
+          size="small"
+          supporting-text="Label support text"
+        >
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+        <gds-input
+          label="Label"
+          size="small"
+          supporting-text="Label support text"
+          value=" "
+          clearable
+        >
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+      </gds-flex>
+      <gds-flex padding="xl 0 0 0" flex-direction="column">
+        <gds-text>Validation</gds-text>
+        <gds-divider></gds-divider>
+      </gds-flex>
+      <gds-flex gap="xl">
+        <gds-input
+          label="Label"
+          size="small"
+          supporting-text="Label support text"
+          value=" "
+          .invalid=\${true}
+          .validator=\${{
+    validate: (el: any) => {
+      if (el.value === '') return [{
+        ...el.validity,
+        valid: false,
+        customError: true
+      }, 'Custom error message'];else if (el.value.length !== 12 || isNaN(el.value)) return [{
+        ...el.validity,
+        valid: false,
+        customError: true
+      }, 'The value must be 12 characters long.'];
+    }
+  }}
+        >
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+        <gds-input
+          label="Label"
+          size="small"
+          supporting-text="Label support text"
+          value=" "
+          .invalid=\${true}
+          .validator=\${{
+    validate: (el: any) => {
+      if (el.value === '') return [{
+        ...el.validity,
+        valid: false,
+        customError: true
+      }, 'Custom error message'];else if (el.value.length !== 12 || isNaN(el.value)) return [{
+        ...el.validity,
+        valid: false,
+        customError: true
+      }, 'The value must be 12 characters long.'];
+    }
+  }}
+          clearable
+        >
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+      </gds-flex>
+
+      <gds-flex padding="xl 0 0 0" flex-direction="column">
+        <gds-text>Disabled</gds-text>
+        <gds-divider></gds-divider>
+      </gds-flex>
+      <gds-flex gap="xl">
+        <gds-input
+          label="Label"
+          size="small"
+          disabled
+          supporting-text="Label support text"
+        >
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+        <gds-input
+          label="Label"
+          size="small"
+          disabled
+          supporting-text="Label support text"
+          value=" "
+          clearable
+        >
+          <gds-icon-credit-card slot="lead"></gds-icon-credit-card>
+        </gds-input>
+      </gds-flex>
+
+      <gds-flex padding="xl 0 0 0" flex-direction="column">
+        <gds-text>Search & Badge</gds-text>
+        <gds-divider></gds-divider>
+      </gds-flex>
+
+      <gds-flex gap="xl">
+        <gds-input label="Label" supporting-text="Label support text">
+          <gds-icon-magnifying-glass slot="lead"></gds-icon-magnifying-glass>
+        </gds-input>
+        <gds-input
+          label="Label"
+          supporting-text="Label support text"
+          value="Input"
+        >
+          <gds-badge variant="information" slot="trail">KR</gds-badge>
+        </gds-input>
+      </gds-flex>
+      <gds-flex gap="xl">
+        <gds-input
+          label="Label"
+          .invalid="\${true}"
+          supporting-text="Label support text"
+        >
+          <gds-icon-magnifying-glass slot="lead"></gds-icon-magnifying-glass>
+        </gds-input>
+        <gds-input
+          label="Label"
+          .invalid="\${true}"
+          supporting-text="Label support text"
+          value="Input"
+        >
+          <gds-badge variant="error" slot="trail">KR</gds-badge>
+        </gds-input>
+      </gds-flex>
+      <gds-flex padding="xl 0 0 0" flex-direction="column">
+        <gds-text tag="h2">Multiline</gds-text>
+        <gds-divider></gds-divider>
+      </gds-flex>
+      <gds-flex gap="xl">
+        <gds-input
+          label="Label"
+          supporting-text="Label support text"
+          maxlength="140"
+          multiline
+        >
+        </gds-input>
+        <gds-input
+          label="Label"
+          supporting-text="Label support text"
+          maxlength="140"
+          multiline
+        >
+          <gds-badge variant="error" slot="trail">KR</gds-badge>
+        </gds-input>
+      </gds-flex>
+      <gds-flex gap="xl">
+        <gds-input
+          label="Label"
+          supporting-text="Label support text"
+          maxlength="140"
+          .invalid=\${true}
+          multiline
+        >
+        </gds-input>
+        <gds-input
+          label="Label"
+          supporting-text="Label support text"
+          maxlength="140"
+          .invalid=\${true}
+          multiline
+        >
+          <gds-badge variant="error" slot="trail">KR</gds-badge>
+        </gds-input>
+      </gds-flex>
+    </gds-flex>
+  \`
+}`,...(a=(d=t.parameters)==null?void 0:d.docs)==null?void 0:a.source}}};const U=["All"];export{t as All,U as __namedExportsOrder,P as default};
