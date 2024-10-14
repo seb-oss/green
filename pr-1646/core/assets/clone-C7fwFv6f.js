febacb3bb1b79a299f96fb317fbdca2b90047649
splashdust <splashdust@users.noreply.github.com>
2024-10-14 13:20:22 +0000

    Deploying to gh-pages from @ seb-oss/green@a71fd2ef7461011bb6ee420bd999a6fb96424a2d 🚀

--- pr-1646/core/assets/clone-C7fwFv6f.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./graph-Bh7t9LgO.js";var e=4;function a(o){return r(o,e)}export{a as c};
--- pr-1646/core/assets/filter-chip.stories-CVF_8ELL.js
+++
@@ -1,7 +0,0 @@
-import"./filter-chips-D5jXd5hH.js";import"./gds-element-CNG85WuL.js";import"./lit-element-BoQqPHl6.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./query-p8xgzTDt.js";import"./class-map-BBGuaYF3.js";import"./directive-Ctav8iJK.js";import"./button-D0X5FAjw.js";import"./static-C_6dgFWo.js";import"./when-BxLAFfhK.js";import"./if-defined-OiR-h1RO.js";import"./transitional-styles-CR09P99V.js";import"./tokens.style-DuOE18tp.js";import"./runtime-CMQcyTl6.js";import"./watch-tFciLXSI.js";import"./resize-observer-B9k8v2TZ.js";import"./attribute-converters-CdI0trPX.js";import"./query-async-MEroNOeJ.js";import"./checkmark-CJZ0AXZ-.js";import"./icon-DqepV8Oa.js";const S={title:"Docs/Components/Filter Chips/Filter Chip",component:"gds-filter-chip",parameters:{layout:"centered",docs:{description:{component:`[Source code](https://github.com/seb-oss/green/tree/main/libs/core/src/components/filter-chips)
-&nbsp;|&nbsp;
-[Usage guidelines](https://designlibrary.sebgroup.com/components/component-filter-chip)
-
-The filter chip is a sub-component of the filter chips component, and should generally not be used on its own.`}}},tags:["autodocs"]},s={parameters:{docs:{source:{format:!0,type:"dynamic"}},controls:{expanded:!0}},args:{innerText:"Filter chip"}},t={...s};var o,e,r;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
-  ...DefaultParams
-}`,...(r=(e=t.parameters)==null?void 0:e.docs)==null?void 0:r.source}}};const T=["Basic"];export{t as Basic,T as __namedExportsOrder,S as default};
