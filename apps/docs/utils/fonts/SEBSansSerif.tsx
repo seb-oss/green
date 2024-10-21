968eceed35b0e8bebabf1de7ca26960bcfe7accb
astrit <am@astrit.co>
2024-10-21 21:50:20 +0200

    Use new fonts

--- apps/docs/utils/fonts/SEBSansSerif.tsx
+++ apps/docs/utils/fonts/SEBSansSerif.tsx
@@ -3,52 +3,52 @@ import localFont from 'next/font/local'
 const SEBSansSerif = localFont({
   src: [
     {
-      path: '../../public/fonts/SEBSansSerif-Bold.woff2',
+      path: '../../public/fonts/SEBSansSerifGDS/SEBSansSerifGDS-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/SEBSansSerif-BoldItalic.woff2',
+      path: '../../public/fonts/SEBSansSerifGDS/SEBSansSerifGDS-BoldItalic.woff2',
       weight: '700',
       style: 'italic',
     },
     {
-      path: '../../public/fonts/SEBSansSerif-Book.woff2',
+      path: '../../public/fonts/SEBSansSerifGDS/SEBSansSerifGDS-Book.woff2',
       weight: '450',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/SEBSansSerif-BookItalic.woff2',
+      path: '../../public/fonts/SEBSansSerifGDS/SEBSansSerifGDS-BookItalic.woff2',
       weight: '450',
       style: 'italic',
     },
     {
-      path: '../../public/fonts/SEBSansSerif-Light.woff2',
+      path: '../../public/fonts/SEBSansSerifGDS/SEBSansSerifGDS-Light.woff2',
       weight: '300',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/SEBSansSerif-LightItalic.woff2',
+      path: '../../public/fonts/SEBSansSerifGDS/SEBSansSerifGDS-LightItalic.woff2',
       weight: '300',
       style: 'italic',
     },
     {
-      path: '../../public/fonts/SEBSansSerif-Medium.woff2',
+      path: '../../public/fonts/SEBSansSerifGDS/SEBSansSerifGDS-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/SEBSansSerif-MediumItalic.woff2',
+      path: '../../public/fonts/SEBSansSerifGDS/SEBSansSerifGDS-MediumItalic.woff2',
       weight: '500',
       style: 'italic',
     },
     {
-      path: '../../public/fonts/SEBSansSerif-Regular.woff2',
+      path: '../../public/fonts/SEBSansSerifGDS/SEBSansSerifGDS-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../../public/fonts/SEBSansSerif-RegularItalic.woff2',
+      path: '../../public/fonts/SEBSansSerifGDS/SEBSansSerifGDS-RegularItalic.woff2',
       weight: '400',
       style: 'italic',
     },
