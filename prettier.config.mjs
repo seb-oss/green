3c9ef5ad6843038193429b8735af6380cb83e26a
Vilhelm Sjölander <vilhelm.sjolander@seb.se>
2024-10-22 15:29:37 +0200

    updated prettier config

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -2,6 +2,21 @@
 const config = {
   semi: false,
   singleQuote: true,
+  importOrder: [
+    '^(react)',
+    '^(lit)|^(@lit)',
+    '<THIRD_PARTY_MODULES>',
+    '',
+    '<TYPES>',
+    '',
+    '^[./|../]',
+    '',
+    '<TYPES>^[./|../]',
+  ],
+  importOrderSeparation: true,
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderMergeDuplicateImports: true,
+  plugins: ['@ianvs/prettier-plugin-sort-imports'],
 }
 
 export default config
