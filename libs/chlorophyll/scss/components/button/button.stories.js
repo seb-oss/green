d82c804566e8beaf3c9be419889b69a916a4c442
Vilhelm Sjölander <vilhelm.sjolander@seb.se>
2024-10-22 21:15:15 +0200

    fixing chlorophyll stories

--- libs/chlorophyll/scss/components/button/button.stories.js
+++ libs/chlorophyll/scss/components/button/button.stories.js
@@ -1,37 +1,39 @@
-const Template = ({ type, variant, text }) => {
-  const attrType = type ? `type="${type}" ` : ''
-  const attrClass = variant ? `class="${variant}" ` : ''
-  return `<button ${attrType}${attrClass}>${text}</button>`
-}
-
 export default {
-  title: 'Components/Form/Elements/Button',
-
+  title: 'Components/Button',
+  tags: ['autodocs'],
   parameters: {
     componentIds: ['component-button'],
   },
-
   argTypes: {
     type: {
       control: 'select',
       options: ['button', 'submit', 'reset'],
     },
-
     variant: {
       control: 'select',
-      options: ['', 'primary', 'secondary', 'ghost', 'danger'],
+      options: ['', 'primary', 'secondary', 'tertiary', 'ghost', 'danger'],
     },
-
+    size: {
+      control: 'select',
+      options: ['', 'small', 'large'],
+    },
+    disabled: {
+      control: 'boolean',
+    },
     text: {
       control: 'text',
     },
   },
 }
 
+/**
+ *  [Source code](https://github.com/seb-oss/green/tree/main/libs/chlorophyll/scss/components/button)
+ * &nbsp;|&nbsp;
+ * [Usage guidelines](https://designlibrary.sebgroup.com/components/component-button)
+ */
 export const Button = {
-  render: Template.bind({}),
-  name: 'Button',
-
+  render: (args) =>
+    `<button type="${args.type}" class="${args.variant} ${args.size}" ${args.disabled ? 'disabled' : ''}>${args.text}</button>`,
   args: {
     type: 'button',
     text: 'Click me!',
