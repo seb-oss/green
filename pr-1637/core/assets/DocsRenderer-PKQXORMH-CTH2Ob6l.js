2606923321f6257d74a403677ec10c3ec4e7fcaf
astrit <astrit@users.noreply.github.com>
2024-10-24 09:02:26 +0000

    Deploying to gh-pages from @ seb-oss/green@41f62db63f56a2adf8224685713025d27e041a48 🚀

--- pr-1637/core/assets/DocsRenderer-PKQXORMH-CTH2Ob6l.js
+++
@@ -1,1 +0,0 @@
-import{D as d,d as n}from"./chunk-HLWAVYOI-B2VR_7zo.js";import"./iframe-DuxAIMF5.js";import"../sb-preview/runtime.js";import"./index-BmJMCxvM.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-Df9so8jS.js";import"./index-Cef7vbu6.js";import"./index-DrFu-skq.js";export{d as DocsRenderer,n as defaultComponents};
---
+++ pr-1637/core/assets/signal.stories-rknB23dB.js
@@ -0,0 +1,17 @@
+import"./signal-BS57dUgh.js";import"./fab-ZJCWhGKz.js";import"./flex-BtTjsVG1.js";import{x as e}from"./lit-element-C_s9q329.js";import"./static-_ukc2i0J.js";import"./custom-element-scoping-CpYtbs9r.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./tokens.style-RalixZsd.js";import"./style-expression-property-oYgpoEZE.js";import"./button-DhIk8LdE.js";import"./query-p8xgzTDt.js";import"./class-map-Bz98xO8-.js";import"./directive-CF8sV3Lr.js";import"./if-defined-Ct9lF4W9.js";import"./when-BR7zwNJC.js";import"./transitional-styles-Cc5JwJ9o.js";import"./runtime-CMQcyTl6.js";import"./watch-tFciLXSI.js";import"./container-BIIXSCQx.js";const v={title:"Components/Signal",component:"gds-signal",tags:["autodocs"]},i={parameters:{docs:{source:{format:!0,type:"dynamic"}},controls:{expanded:!0}}},t={...i,name:"Signal",render:m=>e`
+    <gds-flex height="200px">
+      <gds-fab inset="auto 40px 40px auto">
+        Primary <gds-signal slot="trail"></gds-signal>
+      </gds-fab>
+    </gds-flex>
+  `};var o,a,r,s,n;t.parameters={...t.parameters,docs:{...(o=t.parameters)==null?void 0:o.docs,source:{originalSource:`{
+  ...DefaultParams,
+  name: 'Signal',
+  render: args => html\`
+    <gds-flex height="200px">
+      <gds-fab inset="auto 40px 40px auto">
+        Primary <gds-signal slot="trail"></gds-signal>
+      </gds-fab>
+    </gds-flex>
+  \`
+}`,...(r=(a=t.parameters)==null?void 0:a.docs)==null?void 0:r.source},description:{story:"The `gds-signal` component is used to indicate that there is something new or updated.\nIt is used in conjunction with the `gds-fab` component to draw attention to important actions or notifications.\n\nThe signal's style matches the button's rank and variant by default.",...(n=(s=t.parameters)==null?void 0:s.docs)==null?void 0:n.description}}};const C=["Signal"];export{t as Signal,C as __namedExportsOrder,v as default};
