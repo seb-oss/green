ce9663dee9de3bc9499147c40b004db0038a547b
Joacim Magnusson <joacim.magnusson@seb.se>
2024-10-29 16:17:28 +0100

    Container: Add focus style

--- libs/core/src/components/container/container.style.ts
+++ libs/core/src/components/container/container.style.ts
@@ -4,5 +4,11 @@ const style = css`
   :host {
     box-sizing: border-box;
   }
+  :host(:focus-visible) {
+    outline-color: color-mix(in srgb, currentcolor, #000 100%);
+    outline-offset: var(--gds-space-3xs);
+    outline-style: solid;
+    outline-width: var(--gds-space-3xs);
+  }
 `
 export default style
