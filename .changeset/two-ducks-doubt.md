2c90cdfe1447f2311c1c2e18324fb3c4f0c8272d
astrit <am@astrit.co>
2024-10-30 11:00:00 +0100

    `Text`: Add `text-decoration` prop (#1719)
    * Add `gds-text` text decoration prop and storybook
    
    * Add changeset
    
    * Hard code the underline offset and change selector to `[tag]` instead

---
+++ .changeset/two-ducks-doubt.md
@@ -0,0 +1,5 @@
+---
+'@sebgroup/green-core': minor
+---
+
+**Text:** Add text decoration property with default CSS values and offset
--- libs/core/src/components/text/text.style.ts
+++ libs/core/src/components/text/text.style.ts
@@ -8,6 +8,7 @@ const style = css`
   [tag] {
     margin: 0;
     padding: 0;
+    text-underline-offset: 0.2lh;
   }
 
   h1.no-size-set {
