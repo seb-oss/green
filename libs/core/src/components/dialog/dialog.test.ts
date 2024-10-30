00439ea041346c99b120737f4eaf89f2aef42b67
Joacim Magnusson <joacim.magnusson@seb.se>
2024-10-30 18:06:14 +0100

    Update tests

--- libs/core/src/components/dialog/dialog.test.ts
+++ libs/core/src/components/dialog/dialog.test.ts
@@ -3,7 +3,7 @@ import { fixture, html as testingHtml, waitUntil } from '@open-wc/testing'
 import { sendKeys, sendMouse } from '@web/test-runner-commands'
 import sinon from 'sinon'
 
-import { clickOnElement, conditionToBeTrue, timeout } from '../../utils/testing'
+import { clickOnElement, timeout } from '../../utils/testing'
 
 import '@sebgroup/green-core/components/dialog'
 
@@ -24,7 +24,9 @@ describe('<gds-dialog>', () => {
 
   describe('Accessibility', () => {
     it('is accessible', async () => {
-      const el = await fixture<GdsDialog>(html`<gds-dialog></gds-dialog>`)
+      const el = await fixture<GdsDialog>(
+        html`<gds-dialog open heading="Test">Content</gds-dialog>`,
+      )
       await el.updateComplete
       await expect(el).to.be.accessible()
     })
@@ -32,15 +34,74 @@ describe('<gds-dialog>', () => {
 
   describe('API', () => {
     // Add actual API tests here!
-    it('should have API tests', async () => {
-      expect(false).to.be.true
+    it('should emit events when show() and close() are called with a parameter', async () => {
+      const showSpy = sinon.spy()
+      const closeSpy = sinon.spy()
+      const el = await fixture<GdsDialog>(
+        html`<gds-dialog
+          heading="Test"
+          @gds-show=${showSpy}
+          @gds-close=${closeSpy}
+        >
+          Content
+        </gds-dialog>`,
+      )
+
+      el.show('test')
+      await waitUntil(() => showSpy.called)
+      expect(showSpy).to.have.been.called
+
+      el.close('test')
+      await waitUntil(() => closeSpy.called)
+      expect(closeSpy).to.have.been.called
+    })
+
+    it('should not emit events when show() and close() are called without a parameter', async () => {
+      const showSpy = sinon.spy()
+      const closeSpy = sinon.spy()
+      const el = await fixture<GdsDialog>(
+        html`<gds-dialog
+          heading="Test"
+          @gds-show=${showSpy}
+          @gds-close=${closeSpy}
+        >
+          Content
+        </gds-dialog>`,
+      )
+
+      el.show()
+      await waitUntil(() => el.open)
+      expect(showSpy).to.not.have.been.called
+
+      el.close()
+      await waitUntil(() => !el.open)
+      expect(closeSpy).to.not.have.been.called
     })
   })
 
   describe('Interactions', () => {
     // Add actual interaction tests here! (things like keyboard nav, clicking on things behave as expected, etc)
-    it('should have Interaction tests', async () => {
-      expect(false).to.be.true
+    it('should open dialog when trigger is clicked', async () => {
+      const el = await fixture<GdsDialog>(
+        html`<gds-dialog heading="Test">Content</gds-dialog>`,
+      )
+      const trigger = document.createElement('button')
+      trigger.slot = 'trigger'
+      trigger.textContent = 'Open Dialog'
+      el.appendChild(trigger)
+      await el.updateComplete
+      await clickOnElement(trigger)
+      await waitUntil(() => el.open)
+    })
+
+    it('should close dialog when close button is clicked', async () => {
+      const el = await fixture<GdsDialog>(
+        html`<gds-dialog open heading="Test">Content</gds-dialog>`,
+      )
+
+      const closeBtn = el.shadowRoot?.querySelector('#close-btn')
+      await clickOnElement(closeBtn)
+      await waitUntil(() => !el.open)
     })
   })
 })
