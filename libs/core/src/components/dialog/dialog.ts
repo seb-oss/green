00c2e43a62fc0ca14e4c7fe95d29f7393d62f7c5
Joacim Magnusson <joacim.magnusson@seb.se>
2024-10-31 13:21:22 +0100

    Update beta status

--- libs/core/src/components/dialog/dialog.ts
+++ libs/core/src/components/dialog/dialog.ts
@@ -25,7 +25,7 @@ registerGlobalScrollLockStyles()
 
 /**
  * @element gds-dialog
- * @beta
+ * @status beta
  *
  * @event gds-ui-state - Fired when the dialog is opened or closed
  * @event gds-close - Fired when the dialog is closed
