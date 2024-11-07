23a69530d49c09ee11d9639ccb0d98f7923bc4f2
astrit <astrit@users.noreply.github.com>
2024-11-07 14:38:00 +0000

    Deploying to gh-pages from @ seb-oss/green@edec4a2f8c1db21cec03de096dbf5b841dee8772 🚀

---
+++ pr-1766/core/assets/DocsRenderer-PKQXORMH-MuwEE4wg.js
@@ -0,0 +1,1 @@
+import{D as d,d as n}from"./chunk-HLWAVYOI-Bzq5OE_u.js";import"./iframe-D4IUqoAM.js";import"../sb-preview/runtime.js";import"./index-DWuFxELy.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-Df9so8jS.js";import"./index-Cef7vbu6.js";import"./index-DrFu-skq.js";export{d as DocsRenderer,n as defaultComponents};
---
+++ pr-1766/core/assets/rich-text-BYQy9s7c.js
@@ -0,0 +1,219 @@
+import{g as d,G as g}from"./custom-element-scoping-b4lD3laR.js";import{t as h}from"./tokens.style-BMleC81K.js";import{o as c}from"./observe-light-dom-CmJPHUQ4.js";import{d as p}from"./text-BqFZE6UQ.js";import{i as m}from"./lit-element-C_s9q329.js";const u=m`
+  :host {
+    font-size: var(--gds-text-size-body-m);
+    line-height: var(--gds-text-line-height-body-m);
+    --default-border: 1px solid
+      color-mix(in srgb, currentColor, transparent 80%);
+    letter-spacing: 0.0125rem;
+  }
+
+  :host > :first-child {
+    margin-top: 0;
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    margin-top: var(--gds-space-2xl);
+    margin-bottom: var(--gds-space-xs);
+  }
+
+  h1 + h2 {
+    margin-top: 0;
+  }
+
+  h2 + h3 {
+    margin-top: 0;
+  }
+
+  h3 + h4 {
+    margin-top: 0;
+  }
+
+  h4 + h5 {
+    margin-top: 0;
+  }
+
+  h4 + h5 {
+    margin-top: 0;
+  }
+
+  h5 + h6 {
+    margin-top: 0;
+  }
+
+  p {
+    margin: 0;
+    margin-bottom: var(--gds-space-xs);
+    max-width: 80ch;
+  }
+
+  blockquote {
+    margin: unset;
+    margin-block: 2lh;
+    border-left: 0.2rem solid currentColor;
+    padding-left: 2ch;
+    max-width: 40ch;
+  }
+
+  s {
+    text-decoration: underline;
+    text-decoration-skip-ink: none;
+    text-underline-offset: -0.22lh;
+  }
+
+  mark {
+    color: currentColor;
+    background-color: color-mix(in srgb, currentColor, transparent 80%);
+  }
+
+  /*
+  * lists
+  * -----------------------------------------------------------
+  */
+  ul,
+  ol,
+  li {
+    margin: 0 0 0 1ch;
+    padding: 0 0 0.3lh 0;
+  }
+  li:last-child {
+    padding: 0;
+  }
+
+  ul li::marker {
+    content: '– ';
+  }
+
+  /*
+  * figure
+  * -----------------------------------------------------------
+  */
+  figure {
+    margin: 0;
+    padding: 0;
+    margin-top: var(--gds-space-xl);
+    & img {
+      display: block;
+    }
+  }
+  figcaption {
+    font-size: smaller;
+    margin-block-start: var(--gds-space-xs);
+  }
+
+  /*
+  * table
+  * -----------------------------------------------------------
+  */
+  table {
+    border-collapse: separate;
+    border-spacing: 0;
+    width: 100%;
+    border: var(--default-border);
+    border-radius: var(--gds-space-s);
+    margin-top: var(--gds-space-xl);
+  }
+
+  th {
+    font-weight: var(--gds-text-weight-book);
+    text-align: left;
+    border-bottom: var(--default-border);
+    border-left: transparent;
+    border-right: transparent;
+    color: currentColor;
+  }
+
+  th:not(:last-child) {
+    border-right: var(--default-border);
+  }
+
+  th,
+  td {
+    padding: 0.3lh 2ch;
+  }
+  td:not(:last-child) {
+    border-right: var(--default-border);
+  }
+  tr:nth-child(even) td {
+    background: color-mix(in srgb, currentColor, transparent 95%);
+  }
+
+  /*
+  * Details
+  * -----------------------------------------------------------
+  */
+  details {
+    border-top: solid var(--gds-space-4xs) var(--gds-color-l3-border-tertiary);
+    font-size: var(--gds-text-size-body-m);
+    line-height: var(--gds-text-line-height-body-m);
+  }
+
+  summary {
+    font-weight: var(--gds-text-weight-regular);
+    list-style: none;
+    padding: 0.4lh 0.2ch;
+    font-size: var(--gds-text-size-body-l);
+    line-height: var(--gds-text-line-height-body-l);
+    color: currentColor;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    cursor: pointer;
+    user-select: none;
+    outline-offset: var(--gds-space-2xs);
+    outline-color: currentColor;
+    padding-inline: var(--gds-space-m);
+
+    &:focus-visible {
+      border-radius: var(--gds-space-2xs);
+    }
+  }
+
+  details:hover summary {
+    background: color-mix(in srgb, currentColor, transparent 95%);
+  }
+
+  summary::after {
+    content: '+';
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 24px;
+    height: 24px;
+    font-weight: var(--gds-text-weight-regular);
+    font-size: var(--gds-text-size-body-l);
+  }
+
+  details[open] summary::after {
+    content: '-';
+  }
+
+  details[open] {
+    border-bottom: var(--gds-space-4xs) solid
+      var(--gds-color-l3-border-tertiary);
+    padding-bottom: 0.4lh;
+    background: color-mix(in srgb, currentColor, transparent 95%);
+  }
+
+  details[open] > *:not(summary) {
+    margin-left: var(--gds-space-m);
+  }
+
+  details[open] summary {
+    margin-bottom: 0.4lh;
+  }
+
+  details[open] + details {
+    border: none;
+  }
+
+  details:last-of-type {
+    border-bottom: var(--gds-space-4xs) solid
+      var(--gds-color-l3-border-tertiary);
+    margin-bottom: var(--gds-space-xl);
+  }
+`;var b=Object.defineProperty,f=Object.getOwnPropertyDescriptor,l=(r,e,a,o)=>{for(var t=o>1?void 0:o?f(e,a):e,i=r.length-1,n;i>=0;i--)(n=r[i])&&(t=(o?n(e,a,t):n(t))||t);return o&&t&&b(e,a,t),t};let s=class extends g{querySelectorAll(r){return this.shadowRoot?this.shadowRoot.querySelectorAll(r):new NodeList}querySelector(r){return this.shadowRoot?this.shadowRoot.querySelector(r):null}get innerHTML(){var r;return((r=this.shadowRoot)==null?void 0:r.innerHTML)||""}set innerHTML(r){this.shadowRoot&&(this.shadowRoot.innerHTML=r)}connectedCallback(){super.connectedCallback(),this._captureDOM()}_captureDOM(){!this.shadowRoot||this.childNodes.length===0||this.shadowRoot.replaceChildren(...Array.from(this.childNodes))}};s.styles=[h,p,u];l([c()],s.prototype,"_captureDOM",1);s=l([d("gds-rich-text")],s);
