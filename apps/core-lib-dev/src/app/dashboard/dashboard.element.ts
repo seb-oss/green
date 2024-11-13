82eb22ee3792f592e53cf57d9cd00040415b28f7
astrit <am@astrit.co>
2024-11-13 11:07:52 +0100

    Add some card content

--- apps/core-lib-dev/src/app/dashboard/dashboard.element.ts
+++ apps/core-lib-dev/src/app/dashboard/dashboard.element.ts
@@ -12,6 +12,8 @@ import '@sebgroup/green-core/components/icon/icons/plus-large.js'
 import '@sebgroup/green-core/components/icon/icons/calendar.js'
 import '@sebgroup/green-core/components/icon/icons/chevron-right.js'
 import '@sebgroup/green-core/components/icon/icons/credit-card.js'
+import '@sebgroup/green-core/components/icon/icons/knife-spoon.js'
+import '@sebgroup/green-core/components/icon/icons/arrow-out-of-box.js'
 import '@sebgroup/green-core/components/mask/mask.js'
 import '@sebgroup/green-core/components/icon/icons/chevron-bottom.js'
 import './dashboard.css'
@@ -55,10 +57,12 @@ export class CardExample extends LitElement {
         </gds-flex>
         <gds-grid columns="3" gap="xl">
           <tp-card>
+            <!-- Header -->
             <gds-text slot="header" font-size="heading-s">Konton</gds-text>
             <gds-button slot="action" size="small" rank="tertiary">
               <gds-icon-dot-grid-one-horizontal></gds-icon-dot-grid-one-horizontal>
             </gds-button>
+            <!-- /Header -->
             <tp-list-item>
               <gds-icon-credit-card
                 width="20"
@@ -91,12 +95,66 @@ export class CardExample extends LitElement {
                 </gds-badge>
               </gds-flex>
             </tp-list-item>
-            <tp-list-item> </tp-list-item>
-            <tp-list-item></tp-list-item>
-            <tp-list-item></tp-list-item>
-            <tp-list-item></tp-list-item>
-            <tp-list-item></tp-list-item>
-            <tp-list-item></tp-list-item>
+            <tp-list-item>
+              <gds-icon-arrow-out-of-box
+                width="20"
+                height="20"
+                slot="icon"
+              ></gds-icon-arrow-out-of-box>
+              <gds-flex
+                flex-direction="column"
+                gap="xs"
+                border="0 0 4xs/primary 0"
+                padding="0 0 m 0"
+                flex="1"
+              >
+                <gds-flex align-items="center" justify-content="space-between">
+                  <gds-flex flex-direction="column">
+                    <gds-text font-size="detail-m">John Doe</gds-text>
+                    <gds-text font-size="detail-s" color="secondary">
+                      5896 13 000 20
+                    </gds-text>
+                  </gds-flex>
+                  <gds-flex flex-direction="column" align-items="flex-end">
+                    <gds-text font-size="detail-m">47 300,00</gds-text>
+                    <gds-text font-size="detail-s" color="secondary">
+                      5 december
+                    </gds-text>
+                  </gds-flex>
+                </gds-flex>
+              </gds-flex>
+            </tp-list-item>
+            <tp-list-item>
+              <gds-icon-knife-spoon
+                width="20"
+                height="20"
+                slot="icon"
+              ></gds-icon-knife-spoon>
+              <gds-flex
+                flex-direction="column"
+                gap="xs"
+                border="0 0 4xs/primary 0"
+                padding="0 0 m 0"
+                flex="1"
+              >
+                <gds-flex align-items="center" justify-content="space-between">
+                  <gds-flex flex-direction="column">
+                    <gds-text font-size="detail-m">John Doe</gds-text>
+                    <gds-text font-size="detail-s" color="secondary">
+                      5896 13 000 20
+                    </gds-text>
+                  </gds-flex>
+                  <gds-flex flex-direction="column" align-items="flex-end">
+                    <gds-text font-size="detail-m">2 010 000,00</gds-text>
+                    <gds-text font-size="detail-s" color="secondary">
+                      5 december
+                    </gds-text>
+                  </gds-flex>
+                </gds-flex>
+              </gds-flex>
+            </tp-list-item>
+
+            <!-- Footer -->
             <gds-button slot="footer">Se alla</gds-button>
           </tp-card>
           <gds-card>[CONTENT GOES HERE]</gds-card>
