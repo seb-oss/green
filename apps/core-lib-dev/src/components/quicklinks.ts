e29dab14a2fe4e26ee55e21b724c7ee59c4df094
Ghanipour, Aram <aram.ghanipour@seb.se>
2024-11-13 12:24:03 +0100

    gds-link secondary variant

--- apps/core-lib-dev/src/components/quicklinks.ts
+++ apps/core-lib-dev/src/components/quicklinks.ts
@@ -1,4 +1,4 @@
-import { css, LitElement, unsafeCSS } from 'lit'
+import { LitElement } from 'lit'
 import { customElement } from 'lit/decorators.js'
 
 import { html } from '@sebgroup/green-core/scoping'
@@ -22,7 +22,7 @@ export class TpQuicklinks extends LitElement {
         border-bottom-right-radius="m"
         max-width="max-content"
       >
-        <!-- <gds-link>
+        <gds-link variant="secondary">
           <gds-flex
             level="3"
             padding="m"
@@ -38,7 +38,7 @@ export class TpQuicklinks extends LitElement {
           </gds-flex>
         </gds-link>
 
-        <gds-link href="#">
+        <gds-link variant="secondary">
           <gds-flex
             level="3"
             padding="m"
@@ -54,7 +54,7 @@ export class TpQuicklinks extends LitElement {
           </gds-flex>
         </gds-link>
 
-        <gds-link href="#">
+        <gds-link variant="secondary">
           <gds-flex
             level="3"
             padding="m"
@@ -68,55 +68,7 @@ export class TpQuicklinks extends LitElement {
             <gds-icon-growth></gds-icon-growth>
             <gds-text margin="4px 0 0">Framtiden</gds-text>
           </gds-flex>
-        </gds-link> -->
-
-        <a href="#">
-          <gds-flex
-            level="3"
-            padding="m"
-            align-items="center"
-            justify-content="center"
-            flex-direction="column"
-            background="secondary"
-            border-radius="s"
-            color="tertiary"
-          >
-            <gds-icon-credit-card></gds-icon-credit-card>
-            <gds-text margin="4px 0 0">Kort</gds-text>
-          </gds-flex>
-        </a>
-
-        <a href="#">
-          <gds-flex
-            level="3"
-            padding="m"
-            align-items="center"
-            justify-content="center"
-            flex-direction="column"
-            background="secondary"
-            border-radius="s"
-            color="tertiary"
-          >
-            <gds-icon-arrow-rotate-right-left></gds-icon-arrow-rotate-right-left>
-            <gds-text margin="4px 0 0">Flytta pengar</gds-text>
-          </gds-flex>
-        </a>
-
-        <a href="#">
-          <gds-flex
-            level="3"
-            padding="m"
-            align-items="center"
-            justify-content="center"
-            flex-direction="column"
-            background="secondary"
-            border-radius="s"
-            color="tertiary"
-          >
-            <gds-icon-growth></gds-icon-growth>
-            <gds-text margin="4px 0 0">Framtiden</gds-text>
-          </gds-flex>
-        </a>
+        </gds-link>
       </gds-grid>
     `
   }
