52952cf061049ce1d83aa9944c2d2fa33ede715e
astrit <am@astrit.co>
2024-11-14 10:42:14 +0100

    Add empty state

--- apps/core-lib-dev/src/components/empty-card.ts
+++ apps/core-lib-dev/src/components/empty-card.ts
@@ -1,5 +1,5 @@
-import { LitElement } from 'lit'
-import { customElement } from 'lit/decorators.js'
+import { LitElement, nothing } from 'lit'
+import { customElement, state } from 'lit/decorators.js'
 
 import { html } from '@sebgroup/green-core/scoping'
 
@@ -12,40 +12,67 @@ import '@sebgroup/green-core/components/icon/icons/brand-seb.js'
 
 @customElement('tp-empty-card')
 export class TPEmptyCard extends LitElement {
+  @state()
+  accessor isVisible = true
+
+  private handleCloseClick() {
+    this.isVisible = false
+  }
+
   render() {
-    return html`
-      <style>
+    return html` <style>
         .card {
           aspect-ratio: 1.58 / 1;
         }
+
+        .empty {
+          aspect-ratio: 3 / 2;
+        }
       </style>
-      <gds-card variant="secondary" shadow="s">
-        <gds-flex
-          padding="2xl; m{4xl}"
-          width="100%"
-          flex-direction="column"
-          align-items="center"
-          gap="l"
-        >
-          <gds-card class="card" variant="copper-01" width="100%; m{60%}">
-            <gds-icon-brand-seb width="55" height="24"></gds-icon-brand-seb>
-          </gds-card>
+      ${this.isVisible
+        ? html`
+            <gds-card variant="secondary" shadow="s">
+              <gds-flex justify-content="flex-end">
+                <gds-button
+                  size="small"
+                  rank="secondary"
+                  @click=${this.handleCloseClick}
+                >
+                  <gds-icon-cross-small></gds-icon-cross-small>
+                </gds-button>
+              </gds-flex>
+              <gds-flex
+                padding="2xl; m{4xl}"
+                width="100%"
+                flex-direction="column"
+                align-items="center"
+                gap="l"
+              >
+                <gds-card class="card" variant="copper-01" width="100%; m{60%}">
+                  <gds-icon-brand-seb
+                    width="55"
+                    height="24"
+                  ></gds-icon-brand-seb>
+                </gds-card>
 
-          <gds-text
-            text-wrap="balance"
-            max-width="40"
-            font-weight="book"
-            font-size="heading-xs"
-            color="primary"
-            text-align="center"
-          >
-            Få en översikt av dina konton i SEB, transaktioner, debit och
-            kreditkort.
-          </gds-text>
+                <gds-text
+                  text-wrap="balance"
+                  max-width="40"
+                  font-weight="book"
+                  font-size="heading-xs"
+                  color="primary"
+                  text-align="center"
+                >
+                  Få en översikt av dina konton i SEB, transaktioner, debit och
+                  kreditkort.
+                </gds-text>
 
-          <gds-button> Skaffa kreditkort </gds-button>
-        </gds-flex>
-      </gds-card>
-    `
+                <gds-button> Skaffa kreditkort </gds-button>
+              </gds-flex>
+            </gds-card>
+          `
+        : html`
+          <gds-card variant="information" class="empty" ></gds-card>
+        `}`
   }
 }
