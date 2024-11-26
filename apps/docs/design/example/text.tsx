9f6741059215be72187153c27817103b35630d10
astrit <am@astrit.co>
2024-11-26 13:34:19 +0100

    Convert to string the number of lines

--- apps/docs/design/example/text.tsx
+++ apps/docs/design/example/text.tsx
@@ -6,7 +6,7 @@ const Text = () => (
   <GdsFlex flex-direction="column">
     <GdsText font-weight="bold">Bold</GdsText>
     <GdsText font-size="body-s">Size: Body Small</GdsText>
-    <GdsText font-size="display-s" lines={2}>
+    <GdsText font-size="display-s" lines="2">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua.
     </GdsText>
