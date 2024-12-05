c405cc2d5c515db843aa203773a7077fb2128864
astrit <am@astrit.co>
2024-12-05 13:03:42 +0100

    Add page types

---
+++ apps/docs/utils/content/types/Page.tsx
@@ -0,0 +1,39 @@
+import { defineDocumentType } from '@contentlayer/source-files'
+
+import { getLastEditedDate, urlFromFilePath } from '../utils'
+
+export type DocHeading = { level: 1 | 2 | 3; title: string }
+
+export const Page = defineDocumentType(() => ({
+  name: 'Page',
+  filePathPattern: `pages/**/*.mdx`,
+  contentType: 'mdx',
+  fields: {
+    title: { type: 'string', required: true },
+  },
+  computedFields: {
+    url_path: {
+      type: 'string',
+      description: 'The URL path of the page',
+      resolve: (page) => {
+        if (page._id.startsWith('pages/index.mdx')) return '/'
+        return urlFromFilePath(page)
+      },
+    },
+    pathSegments: {
+      type: 'json',
+      resolve: (page) =>
+        urlFromFilePath(page)
+          .split('/')
+          .slice(2)
+          .map((dirName) => {
+            const re = /^((\d+)-)?(.*)$/
+            const [, , orderStr, pathName] = dirName.match(re) ?? []
+            const order = orderStr ? parseInt(orderStr) : 0
+            return { order, pathName }
+          }),
+    },
+    last_edited: { type: 'date', resolve: getLastEditedDate },
+  },
+  extensions: {},
+}))
