ae1e701f729fe8dc72ffda08a493bae5b971c391
Joacim Magnusson <joacim@isogram.se>
2024-12-05 16:43:59 +0100

    Update angular-lib-dev

--- apps/angular-lib-dev/src/app/form/form.component.ts
+++ apps/angular-lib-dev/src/app/form/form.component.ts
@@ -1,10 +1,23 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms'
 
 import '@sebgroup/green-core/components/button/index.js'
 import '@sebgroup/green-core/components/input/index.js'
 import '@sebgroup/green-core/components/textarea/index.js'
+import '@sebgroup/green-core/components/dropdown/index.js'
+import '@sebgroup/green-core/components/datepicker/index.js'
+import '@sebgroup/green-core/components/form/summary/index.js'
 import '@sebgroup/green-core/components/flex/index.js'
+import '@sebgroup/green-core/components/card/index.js'
+import '@sebgroup/green-core/components/rich-text/index.js'
+import '@sebgroup/green-core/components/text/index.js'
+import '@sebgroup/green-core/components/theme/index.js'
 
 @Component({
   selector: 'green-form',
@@ -16,5 +29,15 @@ export class FormComponent {
     name: new FormControl(''),
     email: new FormControl(''),
     description: new FormControl(''),
+    dropdown: new FormControl(''),
+    date: new FormControl<Date | undefined>(undefined),
   })
+
+  getError(control: AbstractControl | null) {
+    if (control?.hasError('required')) return 'Field is required'
+    if (control?.hasError('minlength')) return 'Field is too short'
+    if (control?.hasError('maxlength')) return 'Field is too long'
+    if (control?.hasError('email')) return 'Invalid email'
+    return ''
+  }
 }
