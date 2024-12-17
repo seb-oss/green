ca631225d2e63677b40deeca40b28674f514387d
Isabelle Löfgren <104595550+IsabelleLof@users.noreply.github.com>
2024-12-17 13:13:30 +0100

    Simplify cookie consent logic, added aria-hidden support to hide the consent, and update styling for responsiveness.

--- apps/docs/utils/consent/consent.tsx
+++ apps/docs/utils/consent/consent.tsx
@@ -1,45 +1,19 @@
-import { useEffect } from 'react';
-import { GdsTheme } from '$/import/components';
-import * as CookieConsent from 'vanilla-cookieconsent';
+import { useEffect } from 'react'
+import { GdsTheme } from '$/import/components'
+import * as CookieConsent from 'vanilla-cookieconsent'
 
-import pluginConfig from './config';
-import './consentnew.css';
+import pluginConfig from './config'
+
+// import 'vanilla-cookieconsent/dist/cookieconsent.css'
+// import './consent.css'
+import './consentnew.css'
 
 const Consent = () => {
   useEffect(() => {
-    // Run the cookie consent plugin
-    const cc = CookieConsent.run({
-      ...pluginConfig,
-      onConsent: () => {
-        console.log('User provided consent'); // Log the action
-        hideCookieConsentModal(); // Hide the modal
-      },
-    });
+    CookieConsent.run(pluginConfig)
+  }, [CookieConsent, pluginConfig])
 
-    // Function to hide the modal
-    const hideCookieConsentModal = () => {
-      const modalElement = document.querySelector('.cm[role="dialog"]'); // Update selector if needed
-      if (modalElement) {
-        modalElement.style.opacity = '0'; // Fade out
-        modalElement.style.pointerEvents = 'none'; // Disable interaction
-        setTimeout(() => {
-          modalElement.style.display = 'none'; // Remove from layout
-        }, 300); // Delay for transition effect
-      } else {
-        console.error('Modal element not found');
-      }
-    };
-
-    return () => {
-      // Clean up
-      if (cc && typeof cc.destroy === 'function') {
-        cc.destroy();
-      }
-    };
-  }, []);
-
-  return <GdsTheme id="cookie-consent"></GdsTheme>;
-};
+  return <GdsTheme id="cookie-consent"></GdsTheme>
+}
 
 export default Consent;
-
