e5db64736eb7a8b0bdb314155c288f97d292baaf
splashdust <splashdust@users.noreply.github.com>
2025-01-23 15:51:18 +0000

    Deploying to gh-pages from @ seb-oss/green@77430d9e98d5630bb4af09bac7e7208d519b1884 🚀

---
+++ pr-1844/core/assets/card-C_7-ZTPG.js
@@ -0,0 +1,11 @@
+import{i as n}from"./lit-element-C_s9q329.js";import{t as i}from"./tokens.style-Czhztukr.js";import{s as c}from"./style-expression-property-Dbto4vYM.js";import{g as m,h as v}from"./custom-element-scoping-BRwcB39g.js";import{G as u}from"./container-CU5Ve22v.js";var h=Object.defineProperty,b=Object.getOwnPropertyDescriptor,p=(o,r,l,t)=>{for(var s=t>1?void 0:t?b(r,l):r,a=o.length-1,d;a>=0;a--)(d=o[a])&&(s=(t?d(r,l,s):d(s))||s);return t&&s&&h(r,l,s),s};let e=class extends u{constructor(){super(),this.variant="primary",this.padding="s; m{l}",this["border-radius"]="xs; m{s}",this["border-width"]="4xs"}render(){return v`<slot></slot>`}};e.styles=[i,n`
+      :host {
+        border-color: var(--_border-color);
+        background-color: var(--_background-color);
+        color: var(--_color);
+      }
+    `];p([c({property:"box-shadow",valueTemplate:o=>`var(--gds-shadow-${o})`})],e.prototype,"shadow",2);p([c({valueTemplate:o=>o,styleTemplate:function(o,r){return`
+      --_border-color: var(--gds-color-l${this.level}-background-${r});
+      --_background-color: var(--gds-color-l${this.level}-background-${r});
+      --_color: var(--gds-color-l${this.level}-content-${r});
+      `}})],e.prototype,"variant",2);e=p([m("gds-card")],e);
--- pr-1844/core/assets/clone-CAHXsUo2.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./graph-BI3ZfLS5.js";var e=4;function a(o){return r(o,e)}export{a as c};
