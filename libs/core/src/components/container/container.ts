76a20963946f9f7ed70cab68f4ae0d644e2f810f
Joacim Magnusson <joacim.magnusson@seb.se>
2025-02-13 17:16:38 +0100

    Clean up container

--- libs/core/src/components/container/container.ts
+++ libs/core/src/components/container/container.ts
@@ -1,35 +1,13 @@
-import { property } from 'lit/decorators.js'
-
-import { GdsElement } from '../../gds-element'
-import { tokens } from '../../tokens.style'
-import { styleExpressionProperty } from '../../utils/decorators/style-expression-property'
-import {
-  forColorTokens,
-  forSpaceTokens,
-  forSpaceTokensSupportingNegative,
-  GdsColorLevel,
-  parseColorValue,
-} from '../../utils/helpers'
-import {
-  gdsCustomElement,
-  html,
-} from '../../utils/helpers/custom-element-scoping'
+import { gdsCustomElement } from '../../scoping'
 import { GdsDiv } from '../div'
-import ContainerCSS from './container.style'
 
 /**
- * `gds-container` is the base element in the declarative layout system. You can think of it as a `<div>` in html.
- *
- * Container is set to `display: block` by default, and adds all of the basic layoutout related properties, like padding, margin, border, etc.
- *
- *
- *
  * @element gds-container
  * @status beta
+ * @deprecated
+ *
+ * `gds-container` is an alias for gds-div. Prefer using `gds-div` directly instead.
  *
  */
-
 @gdsCustomElement('gds-container')
-export class GdsContainer extends GdsDiv {
-  static styles = [tokens, ContainerCSS]
-}
+export class GdsContainer extends GdsDiv {}
