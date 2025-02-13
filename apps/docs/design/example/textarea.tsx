62f525ec636c90e64049bc036a619faea321b319
astrit <am@astrit.co>
2025-02-13 09:27:26 +0100

    Textarea to public and example

--- apps/docs/design/example/textarea.tsx
+++ apps/docs/design/example/textarea.tsx
@@ -1,7 +1,28 @@
 'use client'
 
-import { GdsTextarea } from '$/import/components'
+import { GdsFlex, GdsTextarea } from '$/import/components'
+import { IconCreditCard } from '$/import/icons'
 
-const Textarea = () => <GdsTextarea label="Textarea" />
+const Textarea = ({ hero }: { hero?: boolean }) => (
+  <>
+    {hero ? (
+      <GdsFlex max-width="300px">
+        <GdsTextarea
+          label="Textarea"
+          supportingText="This is a supporting text."
+          clearable
+        >
+          <IconCreditCard slot="lead" />
+          <span slot="extended-supporting-text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </span>
+        </GdsTextarea>
+      </GdsFlex>
+    ) : (
+      <GdsTextarea label="Textarea" />
+    )}
+  </>
+)
 
 export default Textarea
