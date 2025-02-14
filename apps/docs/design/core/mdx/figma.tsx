39a5d2267b2aff181bef62a5839bbca9536cfe8b
astrit <am@astrit.co>
2025-02-14 11:09:28 +0100

    Fix the  image sizing

--- apps/docs/design/core/mdx/figma.tsx
+++ apps/docs/design/core/mdx/figma.tsx
@@ -78,6 +78,7 @@ export default function Figma({
             @scope {
               svg {
                 width: 100%;
+                height: 100%;
               }
             }
           `,
