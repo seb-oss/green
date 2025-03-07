ae7792f06be7f408fba5c87b4b39761411d9717d
Vilhelm Sjölander <vilhelm.sjolander@seb.se>
2025-03-07 14:26:20 +0100

    Comments updated

--- libs/chlorophyll/.storybook/main.ts
+++ libs/chlorophyll/.storybook/main.ts
@@ -13,6 +13,7 @@ const config: StorybookConfig = {
 
 export default config
 
-// To customize your webpack configuration you can use the webpackFinal field.
-// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
-// and https://nx.dev/recipes/storybook/custom-builder-configs
+/* To customize your webpack configuration you can use the webpackFinal field.
+ * Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
+ * and https://nx.dev/recipes/storybook/custom-builder-configs
+ */
