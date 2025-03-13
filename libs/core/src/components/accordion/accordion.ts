02e10bb76ae2772f36c2cdeb16af30e1f602ee64
astrit <am@astrit.co>
2025-03-13 22:23:59 +0100

    Simplify

--- libs/core/src/components/accordion/accordion.ts
+++ libs/core/src/components/accordion/accordion.ts
@@ -2,6 +2,7 @@ import { property, state } from 'lit/decorators.js'
 
 import { GdsElement } from '../../gds-element'
 import { tokens } from '../../tokens.style'
+import { watch } from '../../utils/decorators/watch'
 import {
   gdsCustomElement,
   html,
@@ -40,46 +41,11 @@ export class GdsAccordion extends GdsElement {
   @property({ type: String })
   size: 'large' | 'small' = 'large'
 
-  @state()
-  private isOpen = false
-  connectedCallback(): void {
-    super.connectedCallback()
-    this.addEventListener('toggle', this.handleToggle)
-    this.isOpen = this.open
-  }
-
-  disconnectedCallback() {
-    this.removeEventListener('toggle', this.handleToggle)
-    super.disconnectedCallback()
-  }
-
-  updated(changedProperties: Map<string, any>) {
-    if (changedProperties.has('open')) {
-      this.isOpen = this.open
-    }
-  }
-
-  private handleToggle = (event: Event) => {
-    const details = event.target as HTMLDetailsElement
-    this.isOpen = details.open
-    this.open = details.open
-
-    if (this.isOpen && this.name) {
-      const others = document.querySelectorAll('gds-accordion')
-      others.forEach((accordion) => {
-        const acc = accordion as GdsAccordion
-        if (acc !== this && acc.name === this.name) {
-          acc.open = false
-        }
-      })
-    }
-  }
-
   render() {
     return html`<details
       ?open=${this.open}
-      ?name=${this.name || ''}
-      @toggle=${this.handleToggle}
+      @toggle=${(e: Event) =>
+        (this.open = (e.target as HTMLDetailsElement).open)}
       id="details"
     >
       <summary>
@@ -89,17 +55,17 @@ export class GdsAccordion extends GdsElement {
           <line x1="10" y1="4" x2="10" y2="16">
             <animate
               attributeName="y1"
-              dur="120ms"
-              from="${this.isOpen ? '4' : '10'}"
-              to="${this.isOpen ? '10' : '4'}"
+              dur="240ms"
+              from="${this.open ? '4' : '10'}"
+              to="${this.open ? '10' : '4'}"
               begin="details.toggle"
               fill="freeze"
             />
             <animate
               attributeName="y2"
-              dur="120ms"
-              from="${this.isOpen ? '16' : '10'}"
-              to="${this.isOpen ? '10' : '16'}"
+              dur="240ms"
+              from="${this.open ? '16' : '10'}"
+              to="${this.open ? '10' : '16'}"
               begin="details.toggle"
               fill="freeze"
             />
