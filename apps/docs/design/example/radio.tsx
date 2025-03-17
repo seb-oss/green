dc0fc4e87199b8c48a1006d6319eba1bbaa943cb
astrit <am@astrit.co>
2025-03-17 20:57:09 +0100

    Update example

--- apps/docs/design/example/radio.tsx
+++ apps/docs/design/example/radio.tsx
@@ -7,28 +7,26 @@ const Radio = ({ hero }: { hero?: boolean }) => (
     {hero ? (
       <GdsFlex max-width="340px" flex="1">
         <GdsRadioGroup
-          label="Label"
-          supportingText="This is a supporting text."
+          label={'Select a option'}
           value="1"
+          error-message="Please select an option"
+          supporting-text="Please select one of the following options"
         >
-          <span slot="extended-supporting-text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
-          </span>
           <GdsRadio
             value="1"
-            label="Label one"
-            supportingText="Supporting text one"
+            label="Option 1"
+            supportingText="Supporting text for option 1"
           ></GdsRadio>
           <GdsRadio
             value="2"
-            label="Label two"
-            supportingText="Supporting text two"
+            label="Option 2"
+            supportingText="Supporting text for option 2"
+            disabled={true}
           ></GdsRadio>
           <GdsRadio
             value="3"
-            label="Label three"
-            supportingText="Supporting text three"
+            label="Option 3"
+            supportingText="Supporting text for option 3"
           ></GdsRadio>
         </GdsRadioGroup>
       </GdsFlex>
