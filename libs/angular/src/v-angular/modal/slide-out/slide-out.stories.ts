f5b695bf7e80189193088c7581759590490f7be0
Martina Öqvist <49610436+maoq01@users.noreply.github.com>
2025-03-24 16:06:48 +0100

    Inputs/Dropdown/Textarea: Fixed various spacing within fields and after (#2078)
    * fix: various spacing within fields and after
    
    * fix: added changeset
    
    * fix: changeset
    
    ---------
    
    Co-authored-by: Martina Öqvist <martina.oqvist@seb.se>

--- libs/angular/src/v-angular/modal/slide-out/slide-out.stories.ts
+++ libs/angular/src/v-angular/modal/slide-out/slide-out.stories.ts
@@ -39,9 +39,9 @@ export default {
   ],
   argTypes: {
     side: {
+      options: ['left', 'right'],
       control: {
         type: 'inline-radio',
-        options: ['left', 'right'],
       },
     },
     action: {
