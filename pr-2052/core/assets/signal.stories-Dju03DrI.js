cbbabebdb9283585139fa97d6f16ca5f018f49ab
splashdust <splashdust@users.noreply.github.com>
2025-03-26 18:24:09 +0000

    Deploying to gh-pages from @ seb-oss/green@996b08095680d8074bded8deeb7d3443f70b6970 🚀

---
+++ pr-2052/core/assets/signal.stories-Dju03DrI.js
@@ -0,0 +1,17 @@
+import{x as r}from"./lit-element-BTGJPccZ.js";import{a as i}from"./argTableProps-BOQIcpJQ.js";import"./custom-elements-y0QWBBgU.js";const c={title:"Components/Signal",component:"gds-signal",tags:["autodocs"],argTypes:{...i("gds-signal")}},d={parameters:{docs:{source:{format:!0,type:"dynamic"}},controls:{expanded:!0}}},a={...d,name:"Signal",render:g=>r`
+    <gds-flex height="200px">
+      <gds-fab inset="auto 40px 40px auto">
+        Primary <gds-signal slot="trail"></gds-signal>
+      </gds-fab>
+    </gds-flex>
+  `};var t,s,n,e,o;a.parameters={...a.parameters,docs:{...(t=a.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  ...DefaultParams,
+  name: 'Signal',
+  render: args => html\`
+    <gds-flex height="200px">
+      <gds-fab inset="auto 40px 40px auto">
+        Primary <gds-signal slot="trail"></gds-signal>
+      </gds-fab>
+    </gds-flex>
+  \`
+}`,...(n=(s=a.parameters)==null?void 0:s.docs)==null?void 0:n.source},description:{story:"The `gds-signal` component is used to indicate that there is something new or updated.\nIt is used in conjunction with the `gds-fab` component to draw attention to important actions or notifications.\n\nThe signal's style matches the button's rank and variant by default.",...(o=(e=a.parameters)==null?void 0:e.docs)==null?void 0:o.description}}};const u=["Signal"];export{a as Signal,u as __namedExportsOrder,c as default};
