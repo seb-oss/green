9654ba20de470dce852cf379c60428ea53834568
Joacim Magnusson <joacim.magnusson@seb.se>
2025-04-02 16:47:45 +0200

    `core-lib-dev`: Add view for testing `plain` form controls

--- apps/core-lib-dev/src/app/app.element.ts
+++ apps/core-lib-dev/src/app/app.element.ts
@@ -6,24 +6,7 @@ import { html } from '@sebgroup/green-core/scoping.js'
 
 // import { registerTransitionalStyles } from '@sebgroup/green-core/transitional-styles'
 
-import '@sebgroup/green-core/components/text/index.js'
-import '@sebgroup/green-core/components/context-menu/index.js'
-import '@sebgroup/green-core/components/icon/icons/arrow-right.js'
-import '@sebgroup/green-core/components/icon/icons/flag.js'
-import '@sebgroup/green-core/components/icon/icons/growth.js'
-import '@sebgroup/green-core/components/icon/icons/seb.js'
-import '@sebgroup/green-core/components/icon/icons/bars-three.js'
-import '@sebgroup/green-core/components/icon/icons/globus.js'
-import '@sebgroup/green-core/components/input/index.js'
-import '@sebgroup/green-core/components/theme/index.js'
-import '@sebgroup/green-core/components/card/index.js'
-import '@sebgroup/green-core/components/flex/index.js'
-import '@sebgroup/green-core/components/grid/index.js'
-import '@sebgroup/green-core/components/img/index.js'
-import '@sebgroup/green-core/components/video/index.js'
-import '@sebgroup/green-core/components/menu-button/index.js'
-import '@sebgroup/green-core/components/segmented-control/index.js'
-import '@sebgroup/green-core/components/spinner/index.js'
+import '../../../../libs/core/src/components/index'
 import '../components/header'
 import './chlorophyll.scss'
 import './form-validation.element'
@@ -31,6 +14,7 @@ import './datepicker.element'
 import './calendar.element'
 import './spinner.element'
 import './login/login.element'
+import './plain-form-controls.element'
 
 export class AppElement extends LitElement {
   static styles = css`
@@ -85,6 +69,10 @@ export class AppElement extends LitElement {
               ],
               ['calendar', () => html`<calendar-example></calendar-example>`],
               ['spinner', () => html`<spinner-example></spinner-example>`],
+              [
+                'plain-form-controls',
+                () => html`<plain-form-controls></plain-form-controls>`,
+              ],
             ],
             () => html`No view selected`,
           )}
