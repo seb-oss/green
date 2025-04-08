f1530193558c8aeab486176cb84cf7f277a96cdc
Mathias Wikenbrant <matte955@live.se>
2025-04-08 15:42:30 +0200

    fix

--- libs/core/src/components/formatted-text/number/number-formatter.ts
+++ libs/core/src/components/formatted-text/number/number-formatter.ts
@@ -8,16 +8,8 @@ type NumberFormatter = (
 
 export const numberFormats = {
   decimalsAndThousands: (value, locale, currency) => {
-    const parsed = Number(value)
-    const options: Intl.NumberFormatOptions = {}
-
-    /*
-      If the number has a fractional part, force two decimals (e.g. 123.4 becomes "123.40").
-      If there are no fractions, the number is formatted normally (e.g. 123 remains "123").
-    */
-    if (!isNaN(parsed) && parsed % 1 !== 0) {
-      options.minimumFractionDigits = 2
-      options.maximumFractionDigits = 2
+    const options: Intl.NumberFormatOptions = {
+      maximumFractionDigits: 2,
     }
 
     if (currency) {
@@ -25,9 +17,9 @@ export const numberFormats = {
         ...options,
         style: 'currency',
         currency: currency,
-      }).format(parsed)
+      }).format(Number(value))
     }
 
-    return new Intl.NumberFormat(locale, options).format(parsed)
+    return new Intl.NumberFormat(locale, options).format(Number(value))
   },
 } satisfies Record<string, NumberFormatter>
