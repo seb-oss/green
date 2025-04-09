71173b768384d93b77fc2cbb7b9b1b1cfe53d450
Joacim Magnusson <joacim.magnusson@seb.se>
2025-04-09 13:23:21 +0200

    Update file structure

---
+++ libs/core/src/components/checkbox/checkbox-group/checkbox-group.component.ts
@@ -0,0 +1,161 @@
+// checkbox-group.ts
+import { localized } from '@lit/localize'
+import { property, query } from 'lit/decorators.js'
+import { classMap } from 'lit/directives/class-map.js'
+
+import { GdsFormControlFooter } from '../../../primitives/form-control-footer/form-control-footer.component'
+import { GdsFormControlHeader } from '../../../primitives/form-control-header/form-control-header.component'
+import { gdsCustomElement, html } from '../../../scoping'
+import { watch } from '../../../utils/decorators/watch'
+import {
+  withLayoutChildProps,
+  withMarginProps,
+  withSizeXProps,
+} from '../../../utils/mixins/declarative-layout-mixins'
+import { GdsFormControlElement } from '../../form/form-control'
+import { styles } from './checkbox-group.styles'
+
+import type { GdsCheckbox } from '../checkbox.component'
+
+@localized()
+class CheckboxGroup extends GdsFormControlElement<string> {
+  static styles = [styles]
+
+  /**
+   * Controls the font-size of label and supporting text on group form control header and single checkbox buttons respectively.
+   */
+  @property()
+  size: 'large' | 'small' = 'large'
+
+  /**
+   * The direction in which checkbox buttons are displayed.
+   */
+  @property()
+  direction: 'row' | 'column' = 'column'
+
+  /**
+   * The supporting text displayed between the label and the field.
+   * This text provides additional context or information to the user.
+   */
+  @property({ attribute: 'supporting-text' })
+  supportingText = ''
+
+  /**
+   * Whether the supporting text should be displayed or not.
+   */
+  @property({
+    attribute: 'show-extended-supporting-text',
+    type: Boolean,
+    reflect: true,
+  })
+  showExtendedSupportingText = false
+
+  /**
+   * Returns an array of `<gds-checkbox>` elements in the checkbox group.
+   *
+   * @readonly
+   */
+  get checkboxs(): GdsCheckbox[] {
+    return Array.from(
+      this.querySelectorAll('[gds-element=gds-checkbox]'),
+    ) as GdsCheckbox[]
+  }
+
+  @query('.content')
+  private _contentElement!: HTMLElement
+
+  private _isConnected = false
+
+  connectedCallback() {
+    super.connectedCallback()
+    this._isConnected = true
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback()
+    this._isConnected = false
+  }
+
+  protected _getValidityAnchor(): HTMLElement {
+    return this._contentElement
+  }
+
+  @watch('value')
+  private _handleValueChange() {
+    if (!this._isConnected) return
+  }
+
+  render() {
+    const classes = {
+      'checkbox-group': true,
+      'direction-row': this.direction === 'row',
+      'direction-column': this.direction === 'column',
+    }
+
+    return html`<div
+      role="group"
+      id="checkboxgroup"
+      class=${classMap(classes)}
+      aria-labelledby="group-label"
+      aria-describedby="supporting-text extended-supporting-text footer"
+    >
+      ${this.#renderCheckboxGroupContents()}
+    </div>`
+  }
+
+  #renderCheckboxGroupContents() {
+    const elements = [
+      this.#renderFieldControlHeader(),
+      this.#renderCheckboxs(),
+      this.#renderFieldControlFooter(),
+    ]
+
+    return elements.map((element) => html`${element}`)
+  }
+
+  #renderFieldControlHeader() {
+    if (this.label) {
+      return html` <gds-form-control-header class="size-${this.size}">
+        <label id="group-label" slot="label">${this.label}</label>
+        <span slot="supporting-text" id="supporting-text">
+          ${this.supportingText}
+        </span>
+        <slot
+          id="extended-supporting-text"
+          name="extended-supporting-text"
+          slot="extended-supporting-text"
+        ></slot>
+      </gds-form-control-header>`
+    }
+  }
+
+  #renderCheckboxs() {
+    return html` <div class="content">
+      <slot></slot>
+    </div>`
+  }
+
+  #renderFieldControlFooter() {
+    return html` <gds-form-control-footer
+      id="footer"
+      class="size-${this.size}"
+      .validationMessage=${this.invalid &&
+      (this.errorMessage || this.validationMessage)}
+    >
+    </gds-form-control-footer>`
+  }
+}
+
+/**
+ * @summary A form control component for grouping checkbox buttons.
+ * @status beta
+ *
+ * @element gds-checkbox-group
+ *
+ */
+@gdsCustomElement('gds-checkbox-group', {
+  dependsOn: [GdsFormControlHeader, GdsFormControlFooter],
+})
+export class GdsCheckboxGroup extends withLayoutChildProps(
+  withMarginProps(withSizeXProps(CheckboxGroup)),
+) {}
