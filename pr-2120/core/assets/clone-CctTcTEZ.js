8b958c8a93289af0b40d3fa0e9f174925b0b6e28
astrit <astrit@users.noreply.github.com>
2025-04-10 19:14:56 +0000

    Deploying to gh-pages from @ seb-oss/green@e8a847efc523848bf9b791d1db19503f7dc3bbfd 🚀

--- pr-2120/core/assets/clone-CctTcTEZ.js
+++
@@ -1,1 +0,0 @@
-import{b as r}from"./_baseUniq-Bf0N9321.js";var e=4;function a(o){return r(o,e)}export{a as c};
---
+++ pr-2120/core/assets/sensitive-number.stories-DqttQnnT.js
@@ -0,0 +1,26 @@
+import{x as d}from"./lit-element-Bx14lxc-.js";import{a as h}from"./argTableProps-BDhrbSmu.js";import{G as f,n as b,D as g}from"./formatted-number.stories-CxftkpPo.js";import{h as v,n as y,g as w}from"./custom-element-scoping-b4c89-hi.js";import{n as $}from"./when-BR7zwNJC.js";import{G as D}from"./blur.component-By-0qWsb.js";import"./formatted-account-BAB5dGm-.js";import"./formatted-date-DE64ECLg.js";import"./custom-elements-CiWuRNku.js";import"./formatted-text-XC3ZcYtr.js";import"./query-p8xgzTDt.js";import"./text-BgDT2iSM.js";import"./text.component-CmSKS74a.js";import"./static-B8S6DEnV.js";import"./tokens.style-C9bLefPj.js";import"./declarative-layout-mixins-CxeMR8ZJ.js";import"./div.component-Diofk0s1.js";import"./flex-B8AQUA7n.js";import"./flex.component-BDILfbvS.js";import"./_commonjsHelpers-Cpj98o6Y.js";var _=Object.defineProperty,P=Object.getOwnPropertyDescriptor,c=(e,r,a,s)=>{for(var t=s>1?void 0:s?P(r,a):r,i=e.length-1,m;i>=0;i--)(m=e[i])&&(t=(s?m(r,a,t):m(t))||t);return s&&t&&_(r,a,t),t};let o=class extends f{constructor(){super(...arguments),this.hide=!1}get formattedValue(){return this.hide?b[this.format](0,this.locale,this.currency):super.formattedValue}render(){return $(this.hide,()=>v`<gds-blur>${super.render()}</gds-blur>`,()=>super.render())}updated(e){super.updated(e),this.element&&(this.element.ariaLabel=super.formattedValue)}};c([y({type:Boolean})],o.prototype,"hide",2);o=c([w("gds-sensitive-number",{dependsOn:[D]})],o);o.define();const M={title:"Components/Sensitive/Number",component:"gds-sensitive-number",tags:["autodocs"],argTypes:{...h("gds-sensitive-number"),currency:{type:"string"}},parameters:{docs:{description:{component:`[Source code](https://github.com/seb-oss/green/tree/main/libs/core/src/components/sensitiv/number)
+
+Coachmarks are contextual tips that focus on making the user
+aware of a new feature, the benefits of an existing one or a moved feature within an interface.
+
+The component is primarily a container that follows the targeted element.
+The coachmark will be invisible and disabled if another element covered the targeted element or its simply out of view.
+The coachmark will close and dispatch a CustomEvent of \`gds-ui-state\`
+
+Note: the component can only view one coachmark at the time therefore the tooltip won't be
+rendered in second storybook panel and testing the tooltip behaviour will be in the first panel only
+
+@status beta`}}}},x={parameters:{docs:{source:{format:!0,type:"dynamic"}}},args:{...g.args,hide:!0}},n={...x,render:e=>d`<gds-sensitive-number
+      .hide=${e.hide}
+      .value=${e.value}
+      .locale=${e.locale}
+      .currency=${e.currency}
+    ></gds-sensitive-number>`};var u,l,p;n.parameters={...n.parameters,docs:{...(u=n.parameters)==null?void 0:u.docs,source:{originalSource:`{
+  ...DefaultParams,
+  render: args => html\`<gds-sensitive-number
+      .hide=\${args.hide}
+      .value=\${args.value}
+      .locale=\${args.locale}
+      .currency=\${args.currency}
+    ></gds-sensitive-number>\`
+}`,...(p=(l=n.parameters)==null?void 0:l.docs)==null?void 0:p.source}}};const Q=["Default"];export{n as Default,Q as __namedExportsOrder,M as default};
