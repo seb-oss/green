ca3799edaad15cb109092cb1c45f06daec4aaf3c
Leopold Roos <Leopold Roos>
2025-04-15 13:37:30 +0200

    Reset line formating

--- libs/core/src/generated/locales/sv.ts
+++ libs/core/src/generated/locales/sv.ts
@@ -1,51 +1,54 @@
-// Do not modify this file by hand!
-// Re-generate this file by running lit-localize
 
-import { str } from '@lit/localize'
+    // Do not modify this file by hand!
+    // Re-generate this file by running lit-localize
 
-/* eslint-disable no-irregular-whitespace */
-/* eslint-disable @typescript-eslint/no-explicit-any */
+    
+    import {str} from '@lit/localize';
 
-export const templates = {
-  s15a16ae9f99f19bf: `Juli`,
-  s15a852e9f9a50fa5: `Juni`,
-  s17e3edbe032f79ec: `Öppna kalender`,
-  s1a2dda8f763a68bd: `Januari`,
-  s1e118319b5202c41: `Mån`,
-  s1e187019b5262aa6: `Maj`,
-  s281846ef0421c71f: `November`,
-  s2ceb11be2290bb1b: `Avbryt`,
-  s39938ecdae568740: `September`,
-  s407a88a592a0987c: `Augusti`,
-  s46d6587089bd0356: `Nästa månad`,
-  s49730f3d5751a433: `Laddar...`,
-  s5052d10b539eabf0: `Mars`,
-  s58bfb494feb8eb02: str`${0} valda`,
-  s5d929ff1619ac0c9: `Sök`,
-  s5e8250fb85d64c23: `Stäng`,
-  s5febcd9b311c16fe: `Scrolla höger`,
-  s62931b2a50168976: `Föregående månad`,
-  s63d040e37887f17e: `Idag`,
-  s6cfed919e21ac6ac: `Tor`,
-  s6d24cd19e23b8185: `Tis`,
-  s74aa0319e62b2eb7: `Ons`,
-  s7793b811d20184fe: `December`,
-  s7fd3469f78111589: `Öppna meny`,
-  s83c436ed1bc16dfb: `Visa mer information`,
-  s8ade4e9d5e804d6a: `Första tillgängliga datum`,
-  s9836b719fa8ef857: `Lör`,
-  s987ac119fac8d621: `Sön`,
-  s9af3c72c33088826: str`Det finns ${0} fel att åtgärda innan du kan fortsätta:`,
-  sa6f2645578b2d2bc: `År`,
-  sa7cd5c2ff6d266e7: `Oktober`,
-  sb47daaf9e1c4a905: `Månad`,
-  sb4f1dffbb6be6302: `Rensa`,
-  sb77f4dce69a005ac: `Felmeddelande.`,
-  sbc083a1dbee9af73: `Februari`,
-  scb6b9cb49cf93277: `April`,
-  sd898d99fd9c13de6: `Sök i listan av val`,
-  sdc42f1da42d6411a: `Rensa text`,
-  se0955919920ee87d: `Dag`,
-  se4129fd6b9d7b4c2: `Sista tillgängliga datum`,
-  sf295af199c723ec8: `Fre`,
-}
+    /* eslint-disable no-irregular-whitespace */
+    /* eslint-disable @typescript-eslint/no-explicit-any */
+
+    export const templates = {
+      's15a16ae9f99f19bf': `Juli`,
+'s15a852e9f9a50fa5': `Juni`,
+'s17e3edbe032f79ec': `Öppna kalender`,
+'s1a2dda8f763a68bd': `Januari`,
+'s1e118319b5202c41': `Mån`,
+'s1e187019b5262aa6': `Maj`,
+'s281846ef0421c71f': `November`,
+'s2ceb11be2290bb1b': `Avbryt`,
+'s39938ecdae568740': `September`,
+'s407a88a592a0987c': `Augusti`,
+'s46d6587089bd0356': `Nästa månad`,
+'s49730f3d5751a433': `Laddar...`,
+'s5052d10b539eabf0': `Mars`,
+'s58bfb494feb8eb02': str`${0} valda`,
+'s5d929ff1619ac0c9': `Sök`,
+'s5e8250fb85d64c23': `Stäng`,
+'s5febcd9b311c16fe': `Scrolla höger`,
+'s62931b2a50168976': `Föregående månad`,
+'s63d040e37887f17e': `Idag`,
+'s6cfed919e21ac6ac': `Tor`,
+'s6d24cd19e23b8185': `Tis`,
+'s74aa0319e62b2eb7': `Ons`,
+'s7793b811d20184fe': `December`,
+'s7fd3469f78111589': `Öppna meny`,
+'s83c436ed1bc16dfb': `Visa mer information`,
+'s8ade4e9d5e804d6a': `Första tillgängliga datum`,
+'s9836b719fa8ef857': `Lör`,
+'s987ac119fac8d621': `Sön`,
+'s9af3c72c33088826': str`Det finns ${0} fel att åtgärda innan du kan fortsätta:`,
+'sa6f2645578b2d2bc': `År`,
+'sa7cd5c2ff6d266e7': `Oktober`,
+'sb47daaf9e1c4a905': `Månad`,
+'sb4f1dffbb6be6302': `Rensa`,
+'sb77f4dce69a005ac': `Felmeddelande.`,
+'sbc083a1dbee9af73': `Februari`,
+'scb6b9cb49cf93277': `April`,
+'sd898d99fd9c13de6': `Sök i listan av val`,
+'sdc42f1da42d6411a': `Rensa text`,
+'se0955919920ee87d': `Dag`,
+'se4129fd6b9d7b4c2': `Sista tillgängliga datum`,
+'sf295af199c723ec8': `Fre`,
+    };
+  
