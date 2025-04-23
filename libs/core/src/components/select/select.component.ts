da9839470251817ae76b3936674166d3d2cc4f91
astrit <am@astrit.co>
2025-04-23 11:32:59 +0200

    Update components using form control header

--- libs/core/src/components/select/select.component.ts
+++ libs/core/src/components/select/select.component.ts
@@ -123,7 +123,6 @@ class Select<ValueT = string> extends GdsFormControlElement<ValueT | ValueT[]> {
         () =>
           html`<gds-form-control-header
             class="size-${this.size}"
-            .disabled=${this.disabled}
             .showExtendedSupportingText=${this.showExtendedSupportingText}
           >
             <label for="select" slot="label" id="label-text">
