84a2b98e5e0b7f87e0ae79d6dc8f07856e9662fc
Joacim Magnusson <joacim.magnusson@seb.se>
2025-04-24 15:17:08 +0200

    Add test for CoreCheckboxDirective

--- libs/angular/src/lib/shared/core-control/core-checkbox.directive.ts
+++ libs/angular/src/lib/shared/core-control/core-checkbox.directive.ts
@@ -4,7 +4,7 @@ import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms'
 import { NggCoreControlDirective } from './core-control.directive'
 
 @Directive({
-  selector: `gds-checkbox:not([ngDefaultControl])`,
+  selector: `gds-checkbox, [nggCoreCheckboxControl]`,
   providers: [
     {
       provide: NG_VALUE_ACCESSOR,
@@ -17,14 +17,13 @@ export class NggCoreCheckboxDirective
   extends NggCoreControlDirective
   implements ControlValueAccessor, AfterViewInit
 {
-  // ControlValueAccessor implementation
-  writeValue(value: any): void {
+  override writeValue(value: any): void {
     if (value !== undefined) {
-      this.el.nativeElement.checked = value
+      this.renderer.setProperty(this.el.nativeElement, 'checked', value)
     }
   }
 
-  registerOnChange(fn: (_: any) => void): void {
+  override registerOnChange(fn: (_: any) => void): void {
     this.el.nativeElement.addEventListener('input', (event: any) => {
       fn(event.target.checked)
       this.cdr.markForCheck()
