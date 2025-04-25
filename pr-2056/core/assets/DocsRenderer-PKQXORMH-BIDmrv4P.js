be22e758c39f814bfac7646658f10f470799a709
splashdust <splashdust@users.noreply.github.com>
2025-04-25 16:07:13 +0000

    Deploying to gh-pages from @ seb-oss/green@56552ef315ca9979c62205c1000b3c8b78695ef0 🚀

--- pr-2056/core/assets/DocsRenderer-PKQXORMH-BIDmrv4P.js
+++
@@ -1,1 +0,0 @@
-import{D as d,d as n}from"./chunk-HLWAVYOI-HJPjOMyx.js";import"./iframe-C-5RWoLZ.js";import"../sb-preview/runtime.js";import"./index-BRHg4sdz.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-Df9so8jS.js";import"./index-DxKRhftL.js";import"./index-DrFu-skq.js";export{d as DocsRenderer,n as defaultComponents};
---
+++ pr-2056/core/assets/size-Bd-3MsOk.js
@@ -0,0 +1,18 @@
+import{j as e}from"./jsx-runtime-BjG_zV1W.js";import{u as p}from"./index-CNMj-hLt.js";import"./chunk-HLWAVYOI-DmbqFcJz.js";import{ae as x}from"./index-NUSIiLxo.js";import"./container-DkkI3j3k.js";import"./card-DuGTOxlC.js";import"./flex-BDm6SjQC.js";import"./text-DTUWanMa.js";import"./grid-B2Chh_p-.js";import{d as o}from"./density.comfortable-BkcZcpNd.js";import"./iframe-rSoha_FB.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-Df9so8jS.js";import"./index-DxKRhftL.js";import"./index-DrFu-skq.js";import"./container.component-COcXr3sl.js";import"./custom-element-scoping-b4c89-hi.js";import"./lit-element-Bx14lxc-.js";import"./div.component-DDYeyHVz.js";import"./tokens.style-OWC-W9l-.js";import"./declarative-layout-mixins-DCzV-Sjo.js";import"./card.component-BJRQ7Qi8.js";import"./flex.component-BAUFwSAu.js";import"./text.component-BQ_keSYA.js";import"./static-B8S6DEnV.js";import"./grid.component-DI1NhOE1.js";function c(i){const r={code:"code","gds-card":"gds-card","gds-flex":"gds-flex","gds-text":"gds-text",h1:"h1",h3:"h3",p:"p",pre:"pre",...p(),...i.components},s=r["gds-card"],n=r["gds-flex"],t=r["gds-text"];return e.jsxs(e.Fragment,{children:[e.jsx(x,{title:"Style/Size"}),`
+`,e.jsx(r.h1,{id:"size",children:"Size"}),`
+`,e.jsx(r.h3,{id:"the-spacing-scale-is-based-on-a-4px-grid",children:"The spacing scale is based on a 4px grid."}),`
+`,e.jsx(r.p,{children:"The spacing scale is used for margin, padding, gap, radius and all the spacing values."}),`
+`,e.jsx(r.h3,{id:"spacing-scale",children:"Spacing Scale"}),`
+`,e.jsx("gds-grid",{columns:Object.keys(o.sys.space).length,gap:"xs",width:"100%","auto-columns":"80",children:Object.entries(o.sys.space).map(([d,{value:a}])=>e.jsx(s,{padding:"s","border-radius":"l",children:e.jsxs(n,{gap:"l","align-items":"center","flex-direction":"column",children:[e.jsx(s,{variant:"secondary",padding:"s",width:"100%","box-sizing":"border-box",children:e.jsxs(n,{"align-items":"center","justify-content":"center","flex-direction":"column",children:[e.jsx(t,{children:d.toLowerCase()}),e.jsxs(t,{tag:"small",color:"secondary/0.4",children:[a,"px"]})]})}),e.jsx(s,{height:a+"px","max-height":"400px",padding:"0",width:"100%",border:"0",variant:"tertiary"})]})},d))}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx(r.h3,{id:"max",children:"Max"}),`
+`,e.jsxs(r.p,{children:["The ",e.jsx(r.code,{children:"max"})," size is the maximum size of the spacing scale used when you want to create a circular shape."]}),`
+`,e.jsx(r.p,{children:"Example"}),`
+`,e.jsx(r.pre,{children:e.jsx(r.code,{children:`    <gds-card border-radius="max">max</gds-card>
+`})}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx("br",{}),`
+`,e.jsx("gds-card",{variant:"tertiary","border-radius":"max",width:"200px",children:e.jsx("gds-flex",{"align-items":"center","justify-content":"center",height:"200px",children:e.jsx("gds-text",{tag:"h5",children:"max (999px)"})})})]})}function B(i={}){const{wrapper:r}={...p(),...i.components};return r?e.jsx(r,{...i,children:e.jsx(c,{...i})}):c(i)}export{B as default};
