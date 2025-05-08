4052cbc917560b700cd4c27ea73866f0851b7dc3
astrit <am@astrit.co>
2025-05-08 15:16:22 +0200

    Improve examples

--- libs/core/src/components/breadcrumb/breadcrumb.component.ts
+++ libs/core/src/components/breadcrumb/breadcrumb.component.ts
@@ -1,14 +1,38 @@
 import { html } from 'lit'
 
+import { styleExpressionProperty } from '../../utils/decorators/style-expression-property'
 import { gdsCustomElement } from '../../utils/helpers/custom-element-scoping'
 import { GdsFlex } from '../flex/'
 
 @gdsCustomElement('gds-breadcrumb')
 export class GdsBreadcrumb extends GdsFlex {
+  /**
+   * Style Expression Property that controls the `font-size` property.
+   * Supports all typography size tokens from the design system.
+   */
+  @styleExpressionProperty({
+    styleTemplate: (_prop, values) => {
+      const size = values[0]
+      const styleSize = `font-size: var(--gds-sys-text-size-${size});`
+      const styleLine = `line-height: var(--gds-sys-text-line-height-${size});`
+      return styleSize + styleLine
+    },
+  })
+  'font-size'?: string
+
+  /**
+   * Style Expression Property that controls the `font-weight` property.
+   * Supports all typography weight tokens from the design system.
+   */
+  @styleExpressionProperty({
+    valueTemplate: (v) => `var(--gds-sys-text-weight-${v})`,
+  })
+  'font-weight'?: string
+
   constructor() {
     super()
     this['align-items'] = 'center'
-    this.gap = '2xs'
+    this.gap = 's'
   }
 
   render() {
--- libs/core/src/components/breadcrumb/breadcrumb.stories.ts
+++ libs/core/src/components/breadcrumb/breadcrumb.stories.ts
@@ -4,6 +4,8 @@ import type { Meta, StoryObj } from '@storybook/web-components'
 
 import './breadcrumb'
 import '../link'
+import '../card'
+import '../divider'
 import '../text'
 import '../button'
 import '../context-menu'
@@ -27,7 +29,7 @@ type Story = StoryObj
  */
 export const Simple: Story = {
   render: () => html`
-    <gds-breadcrumb gap="s">
+    <gds-breadcrumb>
       <gds-icon-chevron-right
         size="s"
         slot="separator"
@@ -44,7 +46,7 @@ export const Simple: Story = {
  */
 export const WithLeadIcons: Story = {
   render: () => html`
-    <gds-breadcrumb gap="s">
+    <gds-breadcrumb>
       <gds-icon-chevron-right
         size="s"
         slot="separator"
@@ -73,7 +75,7 @@ export const WithLeadIcons: Story = {
  */
 export const MixedElements: Story = {
   render: () => html`
-    <gds-breadcrumb gap="s">
+    <gds-breadcrumb>
       <gds-icon-chevron-right
         size="s"
         slot="separator"
@@ -105,7 +107,7 @@ export const MixedElements: Story = {
  */
 export const WithTrailingIcons: Story = {
   render: () => html`
-    <gds-breadcrumb gap="s">
+    <gds-breadcrumb>
       <span slot="separator">·</span>
 
       <gds-link href="/home">
@@ -125,7 +127,7 @@ export const WithTrailingIcons: Story = {
  */
 export const TextSeparator: Story = {
   render: () => html`
-    <gds-breadcrumb gap="s">
+    <gds-breadcrumb>
       <gds-text slot="separator" color="secondary"> · </gds-text>
       <gds-link href="/home">Home</gds-link>
       <gds-link href="/products">Products</gds-link>
@@ -139,7 +141,7 @@ export const TextSeparator: Story = {
  */
 export const WithOverflow: Story = {
   render: () => html`
-    <gds-breadcrumb gap="s">
+    <gds-breadcrumb>
       <gds-icon-chevron-right slot="separator"></gds-icon-chevron-right>
 
       <gds-link href="/home">
@@ -155,5 +157,144 @@ export const WithOverflow: Story = {
 
       <gds-link href="/current">Current Page</gds-link>
     </gds-breadcrumb>
+  `,
+}
+
+/**
+ * The breadcrumb component supports different font sizes for its text.
+ * Font size sets automatically line height.
+ *
+ * All the typography tokens are available for use.
+ */
+export const fontSize: Story = {
+  render: () => html`
+    <gds-flex flex-direction="column" gap="xl">
+      <gds-flex gap="m" align-items="center">
+        <gds-flex width="200px" gap="m">
+          <gds-text>Font size: Default/inherited</gds-text>
+        </gds-flex>
+        <gds-card flex="1">
+          <gds-breadcrumb>
+            <gds-text slot="separator">/</gds-text>
+            <gds-link href="#">
+              <gds-icon-home-open slot="lead"></gds-icon-home-open>
+            </gds-link>
+            <gds-link href="#"> Blog </gds-link>
+            <gds-link href="#"> Changelog </gds-link>
+            <gds-text opacity="0.4">v2.0</gds-text>
+          </gds-breadcrumb>
+        </gds-card>
+      </gds-flex>
+      <gds-divider opacity="0.2"></gds-divider>
+      <gds-flex gap="m" align-items="center">
+        <gds-flex width="200px" gap="m">
+          <gds-text>Font size: Body S</gds-text>
+        </gds-flex>
+        <gds-card flex="1">
+          <gds-breadcrumb font-size="body-s">
+            <gds-text slot="separator">/</gds-text>
+            <gds-link href="#">
+              <gds-icon-home-open slot="lead"></gds-icon-home-open>
+            </gds-link>
+            <gds-link href="#"> Blog </gds-link>
+            <gds-link href="#"> Changelog </gds-link>
+            <gds-text opacity="0.4">v2.0</gds-text>
+          </gds-breadcrumb>
+        </gds-card>
+      </gds-flex>
+      <gds-flex gap="m" align-items="center">
+        <gds-flex width="200px" gap="m">
+          <gds-text>Font size: Body M</gds-text>
+        </gds-flex>
+        <gds-card flex="1">
+          <gds-breadcrumb font-size="body-m">
+            <gds-text slot="separator">/</gds-text>
+            <gds-link href="#">
+              <gds-icon-home-open slot="lead"></gds-icon-home-open>
+            </gds-link>
+            <gds-link href="#"> Blog </gds-link>
+            <gds-link href="#"> Changelog </gds-link>
+            <gds-text opacity="0.4">v2.0</gds-text>
+          </gds-breadcrumb>
+        </gds-card>
+      </gds-flex>
+      <gds-flex gap="m" align-items="center">
+        <gds-flex width="200px" gap="m">
+          <gds-text>Font size: Body L</gds-text>
+        </gds-flex>
+        <gds-card flex="1">
+          <gds-breadcrumb font-size="body-l">
+            <gds-text slot="separator">/</gds-text>
+            <gds-link href="#">
+              <gds-icon-home-open slot="lead"></gds-icon-home-open>
+            </gds-link>
+            <gds-link href="#"> Blog </gds-link>
+            <gds-link href="#"> Changelog </gds-link>
+            <gds-text opacity="0.4">v2.0</gds-text>
+          </gds-breadcrumb>
+        </gds-card>
+      </gds-flex>
+    </gds-flex>
+  `,
+}
+
+/**
+ * The breadcrumb gap between its elements can be set using the `gap` property.
+ * The gap property accepts all the spacing tokens from the design system.
+ * The default value is `2xs`.
+ */
+export const Gap: Story = {
+  render: () => html`
+    <gds-flex flex-direction="column" gap="xl">
+      <gds-flex gap="m" align-items="center">
+        <gds-flex width="200px" gap="m">
+          <gds-text>Gap: Default(s)</gds-text>
+        </gds-flex>
+        <gds-card flex="1">
+          <gds-breadcrumb>
+            <gds-text slot="separator">/</gds-text>
+            <gds-link href="#">
+              <gds-icon-home-open slot="lead"></gds-icon-home-open>
+            </gds-link>
+            <gds-link href="#"> Blog </gds-link>
+            <gds-link href="#"> Changelog </gds-link>
+            <gds-text opacity="0.4">v2.0</gds-text>
+          </gds-breadcrumb>
+        </gds-card>
+      </gds-flex>
+      <gds-divider opacity="0.2"></gds-divider>
+      <gds-flex gap="m" align-items="center">
+        <gds-flex width="200px" gap="m">
+          <gds-text>Gap: L</gds-text>
+        </gds-flex>
+        <gds-card flex="1">
+          <gds-breadcrumb gap="l">
+            <gds-text slot="separator">/</gds-text>
+            <gds-link href="#">
+              <gds-icon-home-open slot="lead"></gds-icon-home-open>
+            </gds-link>
+            <gds-link href="#"> Blog </gds-link>
+            <gds-link href="#"> Changelog </gds-link>
+            <gds-text opacity="0.4">v2.0</gds-text>
+          </gds-breadcrumb>
+        </gds-card>
+      </gds-flex>
+      <gds-flex gap="m" align-items="center">
+        <gds-flex width="200px" gap="m">
+          <gds-text>Gap: XL</gds-text>
+        </gds-flex>
+        <gds-card flex="1">
+          <gds-breadcrumb gap="xl">
+            <gds-text slot="separator">/</gds-text>
+            <gds-link href="#">
+              <gds-icon-home-open slot="lead"></gds-icon-home-open>
+            </gds-link>
+            <gds-link href="#"> Blog </gds-link>
+            <gds-link href="#"> Changelog </gds-link>
+            <gds-text opacity="0.4">v2.0</gds-text>
+          </gds-breadcrumb>
+        </gds-card>
+      </gds-flex>
+    </gds-flex>
   `,
 }
