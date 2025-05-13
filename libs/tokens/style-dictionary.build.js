f0c94cfc45de74de42662cb75f5461ab0e3a0b92
Vilhelm Sjölander <vilhelm.sjolander@seb.se>
2025-05-13 15:55:42 +0200

    Adding android platform support to tokens (#2164)

--- libs/tokens/style-dictionary.build.js
+++ libs/tokens/style-dictionary.build.js
@@ -223,57 +223,36 @@ await Promise.all(
               },
             ],
           },
-          // TODO: Add Android support
-          // android: {
-          //   buildPath: __dirname + `/../../dist/libs/tokens/${theme}/android/`,
-          //   sourcePath: swiftSourcePath,
-          //   transformGroup: 'compose',
-          //   files: [
-          //     {
-          //       destination: `Colors/${capitalize(colorScheme)}ModeColors.kt`,
-          //       format: 'compose/object',
-          //       filter: 'is-color-no-ref',
-          //       options: {
-          //         import: ['androidx.compose.ui.graphics.Color'],
-          //         packageName: ['se.seb.gds.tokens'],
-          //         className: `${capitalize(colorScheme)}ModeColors`,
-          //         objectType: 'data class',
-          //       },
-          //     },
-          //     {
-          //       destination: 'Colors/Colors.kt',
-          //       format: 'green/android-kotlin-class-tree',
-          //       filter: 'is-color-no-ref',
-          //       options: {
-          //         objectType: 'struct',
-          //         import: ['SwiftUI'],
-          //         uiKitObjectName: 'UIColors',
-          //         className: 'Colors',
-          //         colorType: 'swiftUiReferenceToUiKit',
-          //       },
-          //     },
-          //     {
-          //       destination: 'Dimensions.kt',
-          //       format: 'compose/object',
-          //       filter: 'is-dimension',
-          //       options: {
-          //         import: ['UIKit'],
-          //         objectType: 'struct',
-          //         className: 'Dimensions',
-          //       },
-          //     },
-          //     {
-          //       destination: 'Shape.kt',
-          //       format: 'compose/object',
-          //       filter: 'is-shape',
-          //       options: {
-          //         import: ['UIKit'],
-          //         objectType: 'struct',
-          //         className: 'Shape',
-          //       },
-          //     },
-          //   ],
-          // },
+          android: {
+            buildPath: __dirname + `/../../dist/libs/tokens/${theme}/android/`,
+            sourcePath: swiftSourcePath,
+            transformGroup: 'compose',
+            files: [
+              {
+                destination: `Colors/${capitalize(colorScheme)}ModeColors.kt`,
+                format: 'compose/color-scheme',
+                filter: 'is-color-no-ref',
+                options: {
+                  import: ['androidx.compose.ui.graphics.Color'],
+                  packageName: ['se.seb.gds.tokens'],
+                  className: `GdsColorTokens`,
+                  variableName: `${colorScheme}ModeColors`,
+                  objectType: 'val',
+                },
+              },
+              {
+                destination: 'Colors/GdsColorTokens.kt',
+                format: 'compose/class',
+                filter: 'is-color-no-ref',
+                options: {
+                  objectType: 'data',
+                  className: 'GdsColorTokens',
+                  import: ['androidx.compose.ui.graphics.Color'],
+                  packageName: ['se.seb.gds.tokens'],
+                },
+              },
+            ],
+          },
         },
       }
 
