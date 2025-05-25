e2d894660875fcce74d30f0ce704f872f87ba378
astrit <am@astrit.co>
2025-05-25 18:08:30 +0200

    Remove content and app and stuff

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -2,9 +2,6 @@
 
 import Script from 'next/script'
 
-import { Root } from '../design/layout/root/root'
-import { SettingsProvider as App } from '../settings/provider'
-
 export const dynamic = 'force-dynamic'
 
 export default function RootLayout({
@@ -15,9 +12,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <App>
-          <Root>{children}</Root>
-        </App>
+        {children}
         <Script id="data-layer">
           {`window["dataLayer"] = {
             "pageName":"seb.io",
