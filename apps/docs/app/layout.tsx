6171e7110a60f2d97119c3d2e0281affed4032bd
astrit <am@astrit.co>
2025-06-11 00:01:16 +0200

    Service worker

--- apps/docs/app/layout.tsx
+++ apps/docs/app/layout.tsx
@@ -1,7 +1,17 @@
+import { Metadata } from 'next'
 import Script from 'next/script'
 
 import { Root } from '../design/layout/root/root'
 import { SettingsProvider as App } from '../settings/provider'
+
+export const metadata: Metadata = {
+  title: 'Green Design System',
+  description:
+    'Green Design System is more than a polished user interface, its places the user at the very forefront of design, usability and accessibility.',
+  manifest: '/manifest.json',
+  themeColor: '#003824',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
+}
 
 export default function RootLayout({
   children,
@@ -11,14 +21,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-            window.navigator = window.navigator || {};
-            window.document = window.document || {};
-          `,
-          }}
-        />
         <App>
           <Root>{children}</Root>
         </App>
--- apps/docs/settings/provider.tsx
+++ apps/docs/settings/provider.tsx
@@ -29,7 +29,14 @@ export function SettingsProvider({
 }: SettingsProviderProps) {
   const settings = useSettings()
   const toast = useToast()
-
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then((registration) => console.log('SW registered:', registration))
+        .catch((error) => console.log('SW registration failed:', error))
+    }
+  }, [])
   const value = useMemo<SettingsContextType>(
     () => ({
       settings: {
