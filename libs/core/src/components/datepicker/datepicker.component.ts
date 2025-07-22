1c0528c190d174e95dda52a1e2510c1c5f0b2269
EldRoos <leopold.roos@seb.se>
2025-07-22 09:22:04 +0200

    Typo

--- libs/core/src/components/datepicker/datepicker.component.ts
+++ libs/core/src/components/datepicker/datepicker.component.ts
@@ -78,7 +78,7 @@ class Datepicker extends GdsFormControlElement<Date> {
   max = new Date(new Date().getFullYear() + 10, 0, 1)
 
   /**
-   * Controls wheter the datepicker popover is open.
+   * Controls whether the datepicker popover is open.
    */
   @property({ type: Boolean })
   open = false
@@ -914,7 +914,7 @@ class Datepicker extends GdsFormControlElement<Date> {
   }
 
   /**
-   * The spinner state keeps track of the spinner values regardless of wheter a complete date has been enter yet.
+   * The spinner state keeps track of the spinner values regardless of whether a complete date has been enter yet.
    */
   #spinnerState = {
     year: 'yyyy',
