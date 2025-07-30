43f287c2be5c672ba04e853016b521fecb0bcec9
Joacim Magnusson <joacim.magnusson@seb.se>
2025-07-30 11:21:22 +0200

    Update executor.ts

--- libs/repo-tools/src/executors/publish-snapshot/executor.ts
+++ libs/repo-tools/src/executors/publish-snapshot/executor.ts
@@ -36,6 +36,15 @@ export default async function publishSnapshot(
     return { success: false }
   }
 
+  if (!process.env.NODE_AUTH_TOKEN) {
+    console.error('NODE_AUTH_TOKEN not set')
+    process.exit(1)
+  }
+
+  console.log(
+    `NODE_AUTH_TOKEN starts: ${process.env.NODE_AUTH_TOKEN.slice(0, 4)}...`,
+  )
+
   // Create local .npmrc in libs/<libName>
   const npmrcPath = join('libs', libName, '.npmrc')
   const npmrcContent = `//registry.npmjs.org/:_authToken=${process.env.NODE_AUTH_TOKEN}\nregistry=https://registry.npmjs.org\nalways-auth=true`
@@ -48,16 +57,28 @@ export default async function publishSnapshot(
     process.exit(1)
   }
 
+  await execAsync(
+    `npm config set //registry.npmjs.org/:_authToken ${process.env.NODE_AUTH_TOKEN}`,
+    { cwd: join('libs', libName) },
+  )
+
   const npmConfigProcess = spawn('npm', ['config', 'ls'], {
     cwd: join('libs', libName),
-    env: { ...process.env },
+    //env: { ...process.env },
     stdio: ['inherit', 'pipe', 'pipe'],
+  })
+
+  npmConfigProcess.stdout.on('data', (data) => {
+    console.log(`npm config ls: ${data.toString().trim()}`)
+  })
+  npmConfigProcess.stderr.on('data', (data) => {
+    console.error(`npm config ls error: ${data.toString().trim()}`)
   })
 
   // Run npm whoami to verify authentication
   const whoamiProcess = spawn('npm', ['whoami', '--no-workspaces'], {
     cwd: join('libs', libName),
-    env: { ...process.env },
+    //env: { ...process.env },
     stdio: ['inherit', 'pipe', 'pipe'],
   })
 
@@ -131,4 +152,7 @@ export default async function publishSnapshot(
 
 function getLibPkgJson(libName: string): PackageJsonPartial {
   return JSON.parse(readFileSync(`libs/${libName}/package.json`).toString())
+}
+function execAsync(arg0: string, arg1: { cwd: string }) {
+  throw new Error('Function not implemented.')
 }
