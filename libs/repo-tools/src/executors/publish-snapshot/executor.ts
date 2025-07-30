55135ae26fed72b70c4cb28bf9b9691fe90682fd
Joacim Magnusson <joacim.magnusson@seb.se>
2025-07-30 11:08:56 +0200

    Update executor.ts

--- libs/repo-tools/src/executors/publish-snapshot/executor.ts
+++ libs/repo-tools/src/executors/publish-snapshot/executor.ts
@@ -38,7 +38,7 @@ export default async function publishSnapshot(
 
   // Create local .npmrc in libs/<libName>
   const npmrcPath = join('libs', libName, '.npmrc')
-  const npmrcContent = `//registry.npmjs.org/:_authToken=${process.env.NODE_AUTH_TOKEN}\nregistry=https://registry.npmjs.org`
+  const npmrcContent = `//registry.npmjs.org/:_authToken=${process.env.NODE_AUTH_TOKEN}\nregistry=https://registry.npmjs.org\nalways-auth=true`
   try {
     writeFileSync(npmrcPath, npmrcContent, { mode: 0o600 })
     console.log(`Created .npmrc at ${npmrcPath}`)
