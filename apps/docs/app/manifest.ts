5d1080f49d563c8ec9404c36477bb92bb80aae99
astrit <am@astrit.co>
2025-08-12 12:28:28 +0200

    Favicon and theme issue

--- apps/docs/app/manifest.ts
+++ apps/docs/app/manifest.ts
@@ -11,7 +11,6 @@ export default function manifest(): MetadataRoute.Manifest {
     start_url: '/',
     display: 'standalone',
     background_color: '#ffffff',
-    theme_color: '#003824',
     icons: [
       {
         src: '/icons/icon-192x192.png',
