dfecbac99cc1df6c570ea4d00fad3071ba790ae4
astrit <am@astrit.co>
2025-08-18 11:18:24 +0200

    Link logo on top

--- apps/docs/design/atoms/command/command.tsx
+++ apps/docs/design/atoms/command/command.tsx
@@ -8,8 +8,6 @@ import * as Core from '@sebgroup/green-core/react'
 import { useSettingsContext, useSettingsValue } from '../../../settings'
 import { useContentContext } from '../../../settings/content'
 
-import './style.css'
-
 interface SearchResult {
   title: string
   href: string
@@ -137,20 +135,38 @@ export default function Command() {
     }
   }, [isOpen])
 
-  // if (!isOpen) return null
-
   return (
     <React.Fragment>
+      <Core.GdsFlex display="none; s{contents}">
+        <Core.GdsFab
+          inset="40px 40px auto auto"
+          rank="secondary"
+          size="small"
+          onClick={handleToggleCommand}
+        >
+          <Core.IconMagnifyingGlass></Core.IconMagnifyingGlass>
+        </Core.GdsFab>
+      </Core.GdsFlex>
+
       {isOpen && (
         <Core.GdsDialog
           onGdsClose={handleClosePanel}
           width="620px"
           min-width="620px"
           heading="Search"
-          margin-top="10vh"
+          min-block-size="80vh"
           open
         >
-          <Core.GdsFlex flex-direction="column" gap="m" height="100%">
+          <Core.GdsFlex
+            flex-direction="column"
+            gap="m"
+            height="100%"
+            background="primary"
+            padding="s"
+            border-radius="s"
+            border-color="primary"
+            border-width="4xs"
+          >
             <Core.GdsInput
               ref={inputRef}
               plain
@@ -166,52 +182,61 @@ export default function Command() {
             </Core.GdsInput>
 
             <Core.GdsFlex flex-direction="column" gap="xs" overflow="auto">
-              {query &&
-                searchResults.map((result, index) => (
-                  <a
-                    className={'search-result'}
-                    key={result.href}
-                    padding="s"
-                    width="100%"
-                    href={result.href}
-                  >
-                    <Core.GdsFlex flex-direction="column" gap="2xs">
-                      <Core.GdsFlex
-                        justify-content="flex-start"
-                        gap="s"
-                        align-items="center"
-                      >
-                        <Core.GdsText font-weight="book">
-                          {result.title}
-                        </Core.GdsText>
-                        <Core.GdsFlex gap="xs" align-items="center">
-                          {result.beta && (
-                            <Core.GdsBadge variant="notice" size="small">
-                              BETA
-                            </Core.GdsBadge>
-                          )}
-                          <Core.GdsBadge
-                            variant={
-                              result.type === 'component'
-                                ? 'primary'
-                                : result.type === 'template'
-                                  ? 'notice'
-                                  : 'secondary'
-                            }
-                            size="small"
-                          >
-                            {result.type}
+              {searchResults.map((result, index) => (
+                <Core.GdsCard
+                  key={result.href}
+                  padding="s"
+                  variant={selectedIndex === index ? 'secondary' : 'primary'}
+                  width="100%"
+                  onClick={() => {
+                    router.push(result.href)
+                    handleClosePanel()
+                  }}
+                  tabIndex={0}
+                  onKeyDown={(e) => {
+                    if (e.key === 'Enter') {
+                      router.push(result.href)
+                      handleClosePanel()
+                    }
+                  }}
+                >
+                  <Core.GdsFlex flex-direction="column" gap="2xs">
+                    <Core.GdsFlex
+                      justify-content="flex-start"
+                      gap="s"
+                      align-items="center"
+                    >
+                      <Core.GdsText font-weight="book">
+                        {result.title}
+                      </Core.GdsText>
+                      <Core.GdsFlex gap="xs" align-items="center">
+                        {result.beta && (
+                          <Core.GdsBadge variant="notice" size="small">
+                            BETA
                           </Core.GdsBadge>
-                        </Core.GdsFlex>
+                        )}
+                        <Core.GdsBadge
+                          variant={
+                            result.type === 'component'
+                              ? 'primary'
+                              : result.type === 'template'
+                                ? 'notice'
+                                : 'secondary'
+                          }
+                          size="small"
+                        >
+                          {result.type}
+                        </Core.GdsBadge>
                       </Core.GdsFlex>
-                      {result.summary && (
-                        <Core.GdsText font-size="body-s" color="secondary">
-                          {result.summary}
-                        </Core.GdsText>
-                      )}
                     </Core.GdsFlex>
-                  </a>
-                ))}
+                    {result.summary && (
+                      <Core.GdsText font-size="body-s" color="secondary">
+                        {result.summary}
+                      </Core.GdsText>
+                    )}
+                  </Core.GdsFlex>
+                </Core.GdsCard>
+              ))}
 
               {query && searchResults.length === 0 && (
                 <Core.GdsText color="secondary" text-align="center" padding="l">
@@ -219,7 +244,7 @@ export default function Command() {
                 </Core.GdsText>
               )}
 
-              {!query && (
+              {!query && searchResults.length === 0 && (
                 <Core.GdsText color="secondary" text-align="center" padding="l">
                   Start typing to search...
                 </Core.GdsText>
--- apps/docs/design/atoms/topbar/topbar.tsx
+++ apps/docs/design/atoms/topbar/topbar.tsx
@@ -1,12 +1,15 @@
 'use client'
 
 import * as Core from '@sebgroup/green-core/react'
+import { Link } from '../link/link'
 
 export function Topbar() {
   return (
     <header>
       <Core.GdsFlex padding="m" align-content="center" justify-content="center">
-        <Core.IconBrandSeb size="l"></Core.IconBrandSeb>
+        <Link href="/">
+          <Core.IconBrandSeb size="l"></Core.IconBrandSeb>
+        </Link>
       </Core.GdsFlex>
     </header>
   )
