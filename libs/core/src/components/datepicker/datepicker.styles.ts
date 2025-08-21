1d6b2b7128a934e4c5016457aa4f1205cd4dd3d0
EldRoos <leopold.roos@seb.se>
2025-08-21 15:55:31 +0200

    Fixed focus for calendar button, and spinners should have regular not book

--- libs/core/src/components/datepicker/datepicker.styles.ts
+++ libs/core/src/components/datepicker/datepicker.styles.ts
@@ -4,17 +4,21 @@ const style = css`
   @layer base, reset;
 
   @layer base {
+    #calendar-button::part(_button) {
+      outline-offset: -1px;
+    }
+
     .spinners {
       display: flex;
       align-items: center;
       justify-content: flex-start;
       inline-size: 12ch;
       gap: var(--gds-sys-space-4xs);
-      font: var(--gds-sys-text-detail-book-m);
+      font: var(--gds-sys-text-detail-regular-m);
     }
 
     :host([size='small']) .spinners {
-      font: var(--gds-sys-text-detail-book-s);
+      font: var(--gds-sys-text-detail-regular-s);
     }
 
     .spinner {
