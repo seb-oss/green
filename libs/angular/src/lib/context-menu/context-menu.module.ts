5c022a467d8717b391cbea92dd004b686d397b0c
Joacim Magnusson <joacim.magnusson@seb.se>
2025-08-28 17:14:16 +0200

    Add deprecation warnings to some Angular components

--- libs/angular/src/lib/context-menu/context-menu.module.ts
+++ libs/angular/src/lib/context-menu/context-menu.module.ts
@@ -4,6 +4,11 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core'
 import { NggCoreWrapperModule } from '@sebgroup/green-angular/src/lib/shared'
 import { NggContextMenuComponent } from './context-menu.component'
 
+/**
+ * @deprecated
+ * NggContextMenu is deprecated in favor of GdsContextMenu found in the Core package
+ * https://storybook.seb.io/latest/core/?path=/docs/components-context-menu--docs
+ */
 @NgModule({
   declarations: [NggContextMenuComponent],
   imports: [CommonModule, NggCoreWrapperModule],
--- libs/angular/src/lib/datepicker/datepicker.module.ts
+++ libs/angular/src/lib/datepicker/datepicker.module.ts
@@ -4,6 +4,11 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core'
 import { NggCoreWrapperModule } from '@sebgroup/green-angular/src/lib/shared'
 import { NggDatepickerComponent } from './datepicker.component'
 
+/**
+ * @deprecated
+ * NggDatepicker is deprecated in favor of GdsDatepicker found in the Core package
+ * https://storybook.seb.io/latest/core/?path=/docs/components-datepicker--docs
+ */
 @NgModule({
   declarations: [NggDatepickerComponent],
   imports: [CommonModule, NggCoreWrapperModule],
