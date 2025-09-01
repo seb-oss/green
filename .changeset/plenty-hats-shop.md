19690601ae0e9d59e5b5d143b609d73abfe0a1ab
Joacim Magnusson <428824+splashdust@users.noreply.github.com>
2025-09-01 16:42:00 +0200

    Angular: Add scope resolver provider (#2385)
    * Add ability to use scope resolver imported on the consuming side
    
    * Add tests
    
    * Update shared.module.ts
    
    * Update angular.mdx
    
    * Add version debug property
    
    * Update gds-element.ts
    
    * Add changesets
    
    * Make sure NggCoreWrapper works alongside core renderender
    
    and that they can use different scope resolvers
    
    * Remove console logs
    
    * Update installing.mdx

---
+++ .changeset/plenty-hats-shop.md
@@ -0,0 +1,5 @@
+---
+'@sebgroup/green-angular': minor
+---
+
+**Scoping:** Add scope resolver provider:
--- libs/angular/src/lib/dropdown/dropdown.module.ts
+++ libs/angular/src/lib/dropdown/dropdown.module.ts
@@ -1,7 +1,11 @@
 import { CommonModule } from '@angular/common'
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core'
 
-import { NggCoreWrapperModule } from '@sebgroup/green-angular/src/lib/shared'
+import {
+  NggCoreWrapperModule,
+  SCOPE_RESOLVER,
+} from '@sebgroup/green-angular/src/lib/shared'
+import { getScopedTagName } from '@sebgroup/green-core/scoping'
 import { NggDropdownButtonDirective } from './dropdown-button.directive'
 import { NggDropdownOptionDirective } from './dropdown-option.directive'
 import { NggDropdownComponent } from './dropdown.component'
@@ -17,6 +21,12 @@ import { NggDropdownComponent } from './dropdown.component'
     NggDropdownComponent,
     NggDropdownOptionDirective,
     NggDropdownButtonDirective,
+  ],
+  providers: [
+    {
+      provide: SCOPE_RESOLVER,
+      useValue: { getScopedTagName },
+    },
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
