b22a76b4919871a62f5500f5c0aecb79dd7f1ee7
astrit <am@astrit.co>
2025-09-04 15:57:19 +0200

    Fonts and sub icon fix

--- apps/docs/design/studio/tools/icons/icons.sub.tsx
+++ apps/docs/design/studio/tools/icons/icons.sub.tsx
@@ -7,14 +7,14 @@ import * as Core from '@sebgroup/green-core/react'
 import { Icon } from '../../../../hooks'
 import { useContent } from '../../../../settings/content'
 
-type TabValue = 'code' | 'info'
-type CardVariant = 'secondary' | 'positive' | 'negative' | 'notice' | 'warning'
+type TabValue = 'code' | 'details'
+type CardVariant = 'neutral' | 'positive' | 'negative' | 'notice' | 'warning'
 
 export default function IconDetail({ ID }: { ID: string }) {
   const { actions } = useContent()
   const [isSolid, setIsSolid] = useState(false)
-  const [activeTab, setActiveTab] = useState<TabValue>('code')
-  const [cardVariant, setCardVariant] = useState<CardVariant>('secondary')
+  const [activeTab, setActiveTab] = useState<TabValue>('details')
+  const [cardVariant, setCardVariant] = useState<CardVariant>('neutral')
 
   const iconData = useMemo(() => actions.getIcon(ID), [actions, ID])
 
@@ -31,20 +31,18 @@ export default function IconDetail({ ID }: { ID: string }) {
     >
       <Core.GdsCard
         padding="s"
-        variant="secondary"
+        variant={cardVariant}
         height="280px"
         justify-content="center"
         align-items="center"
-        border-radius="l"
+        border-radius="m"
         position="relative"
       >
-        <Core.GdsFlex color={cardVariant}>
-          <Icon
-            name={`Icon${iconData.displayName.replace(/\s+/g, '')}`}
-            size="2xl"
-            solid={isSolid}
-          />
-        </Core.GdsFlex>
+        <Icon
+          name={`Icon${iconData.displayName.replace(/\s+/g, '')}`}
+          size="2xl"
+          solid={isSolid}
+        />
         <Core.GdsText font="heading-s">{iconData.displayName}</Core.GdsText>
 
         <Core.GdsFlex
@@ -57,11 +55,24 @@ export default function IconDetail({ ID }: { ID: string }) {
             level="3"
             width="m"
             height="m"
-            border-color="positive-01"
+            border-color="strong"
+            background={cardVariant === 'neutral' ? 'neutral-01' : 'secondary'}
             border-width="2xs"
-            border-radius="xs"
+            border-radius="max"
+            data-variant="neutral"
+            onClick={() => setCardVariant('neutral')}
+          />
+          <Core.GdsDiv
+            level="3"
+            width="m"
+            height="m"
+            border-color="positive-01"
+            background={
+              cardVariant === 'positive' ? 'positive-01' : 'secondary'
+            }
+            border-width="2xs"
+            border-radius="max"
             data-variant="positive"
-            background={cardVariant === 'positive' ? 'positive' : 'secondary'}
             onClick={() => setCardVariant('positive')}
           />
           <Core.GdsDiv
@@ -69,8 +80,9 @@ export default function IconDetail({ ID }: { ID: string }) {
             width="m"
             height="m"
             border-color="notice-01"
+            background={cardVariant === 'notice' ? 'notice-01' : 'secondary'}
             border-width="2xs"
-            border-radius="xs"
+            border-radius="max"
             data-variant="notice"
             onClick={() => setCardVariant('notice')}
           />
@@ -79,8 +91,11 @@ export default function IconDetail({ ID }: { ID: string }) {
             width="m"
             height="m"
             border-color="negative-01"
+            background={
+              cardVariant === 'negative' ? 'negative-01' : 'secondary'
+            }
             border-width="2xs"
-            border-radius="xs"
+            border-radius="max"
             data-variant="negative"
             onClick={() => setCardVariant('negative')}
           />
@@ -88,122 +103,129 @@ export default function IconDetail({ ID }: { ID: string }) {
             level="3"
             width="m"
             height="m"
-            border-color="negative-01"
+            border-color="warning-01"
+            background={cardVariant === 'warning' ? 'warning-01' : 'secondary'}
             border-width="2xs"
-            border-radius="xs"
-            data-variant="negative"
+            border-radius="max"
+            data-variant="warning"
             onClick={() => setCardVariant('warning')}
           />
         </Core.GdsFlex>
       </Core.GdsCard>
 
-      <Core.GdsSegmentedControl
-        size="small"
-        value={activeTab}
-        onchange={(e: Event) =>
-          setActiveTab((e.target as HTMLSelectElement).value as TabValue)
-        }
-        width="max-content"
-      >
-        <Core.GdsSegment value="code">Code</Core.GdsSegment>
-        <Core.GdsSegment value="info">Info</Core.GdsSegment>
-      </Core.GdsSegmentedControl>
-      <Core.GdsButton
-        rank="tertiary"
-        size="small"
-        onClick={() => setIsSolid(!isSolid)}
-      >
-        {isSolid ? 'Regular' : 'Solid'}
-      </Core.GdsButton>
-
-      {/* Content Section */}
-      {activeTab === 'code' ? (
-        <Core.GdsFlex flex-direction="column" gap="m">
-          {/* React */}
-          <Core.GdsCard variant="secondary" padding="m">
-            <Core.GdsFlex flex-direction="column" gap="s">
+      <Core.GdsFlex justify-content="space-between" align-items="center">
+        <Core.GdsSegmentedControl
+          size="small"
+          value={activeTab}
+          onchange={(e: Event) =>
+            setActiveTab((e.target as HTMLSelectElement).value as TabValue)
+          }
+          width="max-content"
+        >
+          <Core.GdsSegment value="details">
+            <Core.GdsFlex gap="xs" align-items="center">
+              <Core.IconCircleInfo size="m" />
+              <Core.GdsText font="body-regular-s">Details</Core.GdsText>
+            </Core.GdsFlex>
+          </Core.GdsSegment>
+          <Core.GdsSegment value="code">
+            <Core.GdsFlex gap="xs" align-items="center">
+              <Core.IconCodeBrackets size="m" />
+              <Core.GdsText font="body-regular-s">Code</Core.GdsText>
+            </Core.GdsFlex>
+          </Core.GdsSegment>
+        </Core.GdsSegmentedControl>
+        <Core.GdsButton
+          rank="secondary"
+          size="small"
+          onClick={() => setIsSolid(!isSolid)}
+        >
+          {isSolid ? 'Regular' : 'Solid'}
+        </Core.GdsButton>
+      </Core.GdsFlex>
+      <Core.GdsCard variant="secondary" border-radius="m" padding="xs">
+        {activeTab === 'code' ? (
+          <Core.GdsFlex flex-direction="column" gap="xl">
+            <Core.GdsFlex flex-direction="column" gap="xs">
               <Core.GdsText font="heading-xs">React</Core.GdsText>
-              <Core.GdsText font="detail-book-s" color="subtle-02">
-                {iconData.framework.react.import}
-              </Core.GdsText>
-              <Core.GdsText font="detail-book-s">
-                {iconData.framework.react.component}
-              </Core.GdsText>
+              <Core.GdsCard padding="m" border-color="subtle-01">
+                <Core.GdsText font="detail-book-s" color="subtle-02">
+                  {iconData.framework.react.import}
+                </Core.GdsText>
+                <Core.GdsText font="detail-book-s">
+                  {iconData.framework.react.component}
+                </Core.GdsText>
+              </Core.GdsCard>
             </Core.GdsFlex>
-          </Core.GdsCard>
 
-          {/* Web Component */}
-          <Core.GdsCard variant="secondary" padding="m">
-            <Core.GdsFlex flex-direction="column" gap="s">
+            <Core.GdsFlex flex-direction="column" gap="xs">
               <Core.GdsText font="heading-xs">Web Component</Core.GdsText>
-              <Core.GdsText font="detail-book-s" color="subtle-02">
-                {iconData.framework.web.import}
-              </Core.GdsText>
-              <Core.GdsText font="detail-book-s">
-                {iconData.framework.web.component}
-              </Core.GdsText>
+              <Core.GdsCard padding="m" border-color="subtle-01">
+                <Core.GdsText font="detail-book-s" color="subtle-02">
+                  {iconData.framework.web.import}
+                </Core.GdsText>
+                <Core.GdsText font="detail-book-s">
+                  {iconData.framework.web.component}
+                </Core.GdsText>
+              </Core.GdsCard>
             </Core.GdsFlex>
-          </Core.GdsCard>
 
-          {/* Angular */}
-          <Core.GdsCard variant="secondary" padding="m">
-            <Core.GdsFlex flex-direction="column" gap="s">
+            <Core.GdsFlex flex-direction="column" gap="xs">
               <Core.GdsText font="heading-xs">Angular</Core.GdsText>
-              <Core.GdsText font="detail-book-s" color="subtle-02">
-                {iconData.framework.angular.import}
-              </Core.GdsText>
-              <Core.GdsText font="detail-book-s">
-                {iconData.framework.angular.component}
-              </Core.GdsText>
-            </Core.GdsFlex>
-          </Core.GdsCard>
-        </Core.GdsFlex>
-      ) : (
-        <Core.GdsFlex flex-direction="column" gap="xl">
-          {/* Categories */}
-          <Core.GdsFlex flex-direction="column" gap="xs">
-            <Core.GdsText font="heading-s">Categories</Core.GdsText>
-            <Core.GdsFlex gap="s" flex-wrap="wrap">
-              {iconData.meta.categories.map((category) => (
-                <Core.GdsBadge key={category} size="small">
-                  {category}
-                </Core.GdsBadge>
-              ))}
+              <Core.GdsCard padding="m" border-color="subtle-01">
+                <Core.GdsText font="detail-book-s" color="subtle-02">
+                  {iconData.framework.angular.import}
+                </Core.GdsText>
+                <Core.GdsText font="detail-book-s">
+                  {iconData.framework.angular.component}
+                </Core.GdsText>
+              </Core.GdsCard>
             </Core.GdsFlex>
           </Core.GdsFlex>
+        ) : (
+          <Core.GdsFlex flex-direction="column" gap="l" padding="m">
+            <Core.GdsFlex flex-direction="column" gap="xs">
+              <Core.GdsText font="heading-xs">Categories</Core.GdsText>
+              <Core.GdsFlex gap="s" flex-wrap="wrap">
+                {iconData.meta.categories.map((category) => (
+                  <Core.GdsBadge key={category} size="small">
+                    {category}
+                  </Core.GdsBadge>
+                ))}
+              </Core.GdsFlex>
+            </Core.GdsFlex>
 
-          {/* Tags */}
-          <Core.GdsFlex flex-direction="column" gap="xs">
-            <Core.GdsText font="heading-s">Tags</Core.GdsText>
-            <Core.GdsFlex gap="xs" flex-wrap="wrap">
-              {iconData.meta.tags[0].split(',').map((tag) => (
-                <Core.GdsBadge key={tag} color="subtle-02">
-                  {tag.trim()}
-                </Core.GdsBadge>
-              ))}
+            <Core.GdsFlex flex-direction="column" gap="xs">
+              <Core.GdsText font="heading-xs">Tags</Core.GdsText>
+              <Core.GdsFlex gap="xs" flex-wrap="wrap">
+                {iconData.meta.tags[0].split(',').map((tag) => (
+                  <Core.GdsBadge key={tag} color="subtle-02">
+                    {tag.trim()}
+                  </Core.GdsBadge>
+                ))}
+              </Core.GdsFlex>
+            </Core.GdsFlex>
+
+            <Core.GdsFlex flex-direction="column" gap="xs">
+              <Core.GdsText font="heading-xs">Specifications</Core.GdsText>
+              <Core.GdsGrid columns="3" gap="m">
+                <Core.GdsFlex flex-direction="column" gap="0">
+                  <Core.GdsText font="heading-xs">Width</Core.GdsText>
+                  <Core.GdsText>{iconData.meta.width}px</Core.GdsText>
+                </Core.GdsFlex>
+                <Core.GdsFlex flex-direction="column" gap="0">
+                  <Core.GdsText font="heading-xs">Height</Core.GdsText>
+                  <Core.GdsText>{iconData.meta.height}px</Core.GdsText>
+                </Core.GdsFlex>
+                <Core.GdsFlex flex-direction="column" gap="0">
+                  <Core.GdsText font="heading-xs">ViewBox</Core.GdsText>
+                  <Core.GdsText>{iconData.meta.viewBox}</Core.GdsText>
+                </Core.GdsFlex>
+              </Core.GdsGrid>
             </Core.GdsFlex>
           </Core.GdsFlex>
-
-          {/* Specifications */}
-          <Core.GdsFlex flex-direction="column" gap="m">
-            <Core.GdsText font="heading-s">Specifications</Core.GdsText>
-            <Core.GdsGrid columns="3" gap="m">
-              <Core.GdsFlex flex-direction="column" gap="0">
-                <Core.GdsText font="heading-xs">Width</Core.GdsText>
-                <Core.GdsText>{iconData.meta.width}px</Core.GdsText>
-              </Core.GdsFlex>
-              <Core.GdsFlex flex-direction="column" gap="0">
-                <Core.GdsText font="heading-xs">Height</Core.GdsText>
-                <Core.GdsText>{iconData.meta.height}px</Core.GdsText>
-              </Core.GdsFlex>
-              <Core.GdsFlex flex-direction="column" gap="0">
-                <Core.GdsText font="heading-xs">ViewBox</Core.GdsText>
-                <Core.GdsText>{iconData.meta.viewBox}</Core.GdsText>
-              </Core.GdsFlex>
-            </Core.GdsGrid>
-          </Core.GdsFlex>
-        </Core.GdsFlex>
-      )}
+        )}
+      </Core.GdsCard>
     </Core.GdsFlex>
   )
 }
