f1069dcbf2c135da6deaf8a548f8e03975758439
Jaroslavas-1 <Jaroslavas-1@users.noreply.github.com>
2025-09-11 08:41:59 +0000

    Deploying to gh-pages from @ seb-oss/green@7d6e83b260aeb5ca63b1f6c2754bb5dc59a09706 🚀

---
+++ pr-2411/core/assets/index-CvZyMaGH.js
@@ -0,0 +1,1 @@
+import{M as a,u as n}from"./index-DPhyK_gG.js";import"./index-BH_eK0mp.js";import"./iframe-BxNm82cz.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-DxKRhftL.js";import"./index-DrFu-skq.js";export{a as MDXProvider,n as useMDXComponents};
