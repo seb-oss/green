499b2344dc4d998f43d1380b21767517170ff537
vsjolander <vsjolander@users.noreply.github.com>
2025-09-21 09:16:42 +0000

    Deploying to gh-pages from @ seb-oss/green@443ea9d31af032a479eef3b668345f13b1079251 🚀

---
+++ pr-2433/core/assets/blur.stories-QzBYgXKD.js
@@ -0,0 +1,17 @@
+import{x as s}from"./lit-element-DurN2HL2.js";import{a as n}from"./argTableProps-DJKnKK2M.js";import"./text-jcXmMPmS.js";import{G as a}from"./blur.component-C22b0dE9.js";import"./custom-elements-0Ll1kQ0F.js";import"./Reflect-BIo-zK4X.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./static-Bn3NESYl.js";import"./tokens.style-Boei6TBQ.js";import"./gds-element-4C_whPou.js";import"./declarative-layout-mixins-TUy19cvx.js";import"./div.component-BwtVtTeM.js";a.define();const y={title:"Components/Blur",component:"gds-blur",tags:["autodocs"],argTypes:{...n("gds-blur")},parameters:{docs:{description:{component:`[Source code](https://github.com/seb-oss/green/tree/main/libs/core/src/components/blur)
+
+The Blur component is used to apply a blur effect over content,
+often used to obscure sensitive or background information.
+
+@status beta`}}}},m={parameters:{docs:{source:{format:!0,type:"dynamic"}},controls:{expanded:!0}}},r={...m,render:()=>s`
+    <gds-blur>
+      <gds-text>Blur</gds-text>
+    </gds-blur>
+  `};var e,t,o;r.parameters={...r.parameters,docs:{...(e=r.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  ...DefaultParams,
+  render: () => html\`
+    <gds-blur>
+      <gds-text>Blur</gds-text>
+    </gds-blur>
+  \`
+}`,...(o=(t=r.parameters)==null?void 0:t.docs)==null?void 0:o.source}}};const D=["Default"];export{r as Default,D as __namedExportsOrder,y as default};
--- pr-2433/core/assets/index-CVby7S5W.js
+++
@@ -1,1 +0,0 @@
-import{M as a,u as n}from"./index-CtaPBkV0.js";import"./index-JFO-jAak.js";import"./iframe-B0NhYDb3.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-DxKRhftL.js";import"./index-DrFu-skq.js";export{a as MDXProvider,n as useMDXComponents};
