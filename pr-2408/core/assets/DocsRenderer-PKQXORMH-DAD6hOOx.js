4470ebd28aeb2cad07bce867ba17c87f482e1c36
astrit <astrit@users.noreply.github.com>
2025-09-22 13:13:18 +0000

    Deploying to gh-pages from @ seb-oss/green@443ea9d31af032a479eef3b668345f13b1079251 🚀

---
+++ pr-2408/core/assets/DocsRenderer-PKQXORMH-DAD6hOOx.js
@@ -0,0 +1,1 @@
+import{D as d,d as n}from"./chunk-HLWAVYOI-BTLkYtEr.js";import"./iframe-C5mhbc5i.js";import"../sb-preview/runtime.js";import"./index-DZ-rwKCE.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-DxKRhftL.js";import"./index-DrFu-skq.js";export{d as DocsRenderer,n as defaultComponents};
