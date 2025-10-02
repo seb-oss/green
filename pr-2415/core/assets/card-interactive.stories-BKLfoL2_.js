762302b53f76dbf1b926558330a6b93cc82e6272
astrit <astrit@users.noreply.github.com>
2025-10-02 07:08:04 +0000

    Deploying to gh-pages from @ seb-oss/green@da5fc0aa0a6de2aef9f8fd1e59ed4597c61f2807 🚀

---
+++ pr-2415/core/assets/card-interactive.stories-BKLfoL2_.js
@@ -0,0 +1,84 @@
+import{x as u}from"./lit-element-Bx14lxc-.js";import{a as g}from"./argTableProps-qcj9WYc6.js";import"./card-interactive-B6cTIGvu.js";import"./custom-elements-DS7O2T4h.js";import"./Reflect-DJ7r0WLU.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./if-defined-CVqwUuaf.js";import"./when-BR7zwNJC.js";import"./gds-element-DTROifYq.js";import"./tokens.style-CJKwYBen.js";import"./card.component-_FybjgYi.js";import"./declarative-layout-mixins-DFPXvh_g.js";import"./div.component-B56RLpJM.js";import"./flex.component-CRnwBFQj.js";import"./chain-link-CK6_-QUd.js";import"./icon-DeOpVb_j.js";import"./unsafe-html-CYO4avEf.js";import"./directive-CF8sV3Lr.js";import"./img.component-DR91qXzr.js";import"./text.component-3_iRXBRQ.js";import"./static-B8S6DEnV.js";import"./default-typography.styles-DG7u6j1x.js";const _={title:"Components/Card Interactive",component:"gds-card-interactive",tags:["autodocs"],argTypes:{...g("gds-card-interactive")}},l={parameters:{docs:{source:{format:!0,type:"dynamic"}},controls:{disable:!0}}},e={...l,args:{innerHTML:"Card content"}},t={...l,render:b=>u`
+    <gds-grid gap="l" columns="2">
+      <gds-card-interactive
+        href="#"
+        src="https://api.seb.io/assets/launch-hero.jpg"
+        title="Title"
+        excerpt="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        prompt="Read more"
+      >
+      </gds-card-interactive>
+      <gds-card-interactive
+        href="#"
+        src="https://api.seb.io/assets/launch-hero.jpg"
+        title="Title"
+        excerpt="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        prompt="Read more"
+        aspect-ratio="3/2"
+        plain
+      >
+      </gds-card-interactive>
+      <gds-card-interactive
+        href="#"
+        title="Title"
+        excerpt="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        prompt="Read more"
+      >
+      </gds-card-interactive>
+      <gds-card-interactive
+        href="#"
+        title="Title"
+        excerpt="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        prompt="Read more"
+        plain
+      >
+      </gds-card-interactive>
+    </gds-grid>
+  `};var i,r,o,a,d;e.parameters={...e.parameters,docs:{...(i=e.parameters)==null?void 0:i.docs,source:{originalSource:`{
+  ...DefaultParams,
+  args: {
+    innerHTML: 'Card content'
+  }
+}`,...(o=(r=e.parameters)==null?void 0:r.docs)==null?void 0:o.source},description:{story:`TODO: Add documentation here. Also add the beta
+flag in green/libs/core/.storybook/manager.ts to
+mark the component as beta in the menu of the
+core storybook.`,...(d=(a=e.parameters)==null?void 0:a.docs)==null?void 0:d.description}}};var s,n,m,c,p;t.parameters={...t.parameters,docs:{...(s=t.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  ...DefaultParams,
+  render: args => html\`
+    <gds-grid gap="l" columns="2">
+      <gds-card-interactive
+        href="#"
+        src="https://api.seb.io/assets/launch-hero.jpg"
+        title="Title"
+        excerpt="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        prompt="Read more"
+      >
+      </gds-card-interactive>
+      <gds-card-interactive
+        href="#"
+        src="https://api.seb.io/assets/launch-hero.jpg"
+        title="Title"
+        excerpt="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        prompt="Read more"
+        aspect-ratio="3/2"
+        plain
+      >
+      </gds-card-interactive>
+      <gds-card-interactive
+        href="#"
+        title="Title"
+        excerpt="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        prompt="Read more"
+      >
+      </gds-card-interactive>
+      <gds-card-interactive
+        href="#"
+        title="Title"
+        excerpt="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        prompt="Read more"
+        plain
+      >
+      </gds-card-interactive>
+    </gds-grid>
+  \`
+}`,...(m=(n=t.parameters)==null?void 0:n.docs)==null?void 0:m.source},description:{story:"Examples of different color variants",...(p=(c=t.parameters)==null?void 0:c.docs)==null?void 0:p.description}}};const F=["CardInteractive","Variants"];export{e as CardInteractive,t as Variants,F as __namedExportsOrder,_ as default};
---
+++ pr-2415/core/assets/formatted-account.stories-Dc-YudYg.js
@@ -0,0 +1,11 @@
+import{x as c}from"./lit-element-Bx14lxc-.js";import{a as n}from"./argTableProps-qcj9WYc6.js";import{a as s}from"./formatted-account-1IyS51LY.js";import"./flex-DBGDy_Wa.js";import"./custom-elements-DS7O2T4h.js";import"./Reflect-DJ7r0WLU.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./gds-element-DTROifYq.js";import"./formatted-text-COgHNn0y.js";import"./query-p8xgzTDt.js";import"./text-DmYWJt0y.js";import"./text.component-3_iRXBRQ.js";import"./static-B8S6DEnV.js";import"./tokens.style-CJKwYBen.js";import"./declarative-layout-mixins-DFPXvh_g.js";import"./div.component-B56RLpJM.js";import"./default-typography.styles-DG7u6j1x.js";import"./flex.component-CRnwBFQj.js";const B={title:"Components/Formatted text/Accounts",component:"gds-formatted-account",tags:["autodocs"],argTypes:{...n("gds-formatted-account"),format:{control:{type:"select"},options:Object.keys(s)}},parameters:{docs:{description:{component:"[Source code](https://github.com/seb-oss/green/tree/main/libs/core/src/components/formatted-text/account)\n\n`gds-formatted-account` extends `gds-text` and formats the account to the desired format.\n\n@beta"}}}},m={parameters:{docs:{source:{format:!0,type:"dynamic"}},controls:{expanded:!0}},args:{account:"54400023423"}},t={...m,name:"SEB Account",render:o=>c`<gds-flex gap="m" flex-direction="column">
+      <gds-formatted-account .account=${o.account}></gds-formatted-account>
+      <gds-formatted-account>${o.account}</gds-formatted-account>
+    </gds-flex>`};var e,r,a;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`{
+  ...DefaultParams,
+  name: 'SEB Account',
+  render: args => html\`<gds-flex gap="m" flex-direction="column">
+      <gds-formatted-account .account=\${args.account}></gds-formatted-account>
+      <gds-formatted-account>\${args.account}</gds-formatted-account>
+    </gds-flex>\`
+}`,...(a=(r=t.parameters)==null?void 0:r.docs)==null?void 0:a.source}}};const O=["Default"];export{t as Default,O as __namedExportsOrder,B as default};
