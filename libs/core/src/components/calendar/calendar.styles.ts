9d7e498c951f4bab3741fa659d9903477a3df6aa
Joacim Magnusson <joacim.magnusson@seb.se>
2025-10-02 14:19:08 +0200

    Fix some calendar style issues

--- libs/core/src/components/calendar/calendar.styles.ts
+++ libs/core/src/components/calendar/calendar.styles.ts
@@ -12,10 +12,17 @@ const style = css`
     :host {
       display: flex;
       justify-content: center;
+      --columns: 7;
     }
 
-    :host table {
+    table {
+      display: grid;
+      justify-items: center;
+      grid-template-columns: repeat(var(--columns), 1fr);
+      gap: var(--gds-sys-space-xs);
       border-spacing: var(--gds-sys-space-xs);
+      flex-grow: 1;
+      width: 100%;
       padding: 1ch;
 
       &.small {
@@ -37,127 +44,135 @@ const style = css`
           border-spacing: var(--gds-sys-space-xs) var(--gds-sys-space-l);
         }
       }
-    }
 
-    thead {
-      th {
-        height: var(--gds-sys-space-2xl);
-        width: var(--gds-sys-space-2xl);
-        box-sizing: border-box;
-        text-align: center;
-        font-weight: normal;
+      &.show-week-numbers {
+        --columns: 8;
       }
     }
 
+    tr,
+    thead,
     tbody {
-      td {
-        position: relative;
-        width: var(--gds-sys-space-2xl);
-        height: var(--gds-sys-space-2xl);
-        box-sizing: border-box;
-        text-align: center;
-        user-select: none;
-        border-width: var(--gds-sys-space-4xs);
-        border-style: solid;
-        border-color: transparent;
-        border-radius: var(--gds-sys-radius-max);
-        transition: background 0.2s;
-        outline-style: solid;
-        outline-color: transparent;
-        outline-width: 0px;
-        outline-offset: var(--gds-sys-space-4xs);
+      display: contents;
+    }
+
+    th,
+    td {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      height: var(--gds-sys-space-2xl);
+      width: var(--gds-sys-space-2xl);
+      box-sizing: border-box;
+      user-select: none;
+    }
+
+    th {
+      font-weight: normal;
+    }
+
+    td {
+      position: relative;
+      border-width: var(--gds-sys-space-4xs);
+      border-style: solid;
+      border-color: transparent;
+      border-radius: var(--gds-sys-radius-max);
+      transition: background 0.2s;
+      outline-style: solid;
+      outline-color: transparent;
+      outline-width: 0px;
+      outline-offset: var(--gds-sys-space-4xs);
+
+      &.short {
+        width: var(--gds-sys-space-4xl);
+      }
+
+      &.wide {
+        width: var(--gds-sys-space-5xl);
+      }
+
+      &.long {
+        width: var(--gds-sys-space-7xl);
+      }
+
+      &:not(.disabled):hover,
+      &[aria-selected='true'] {
+        cursor: pointer;
+      }
+
+      &[aria-selected='true'] {
+        color: var(--gds-sys-color-content-neutral-03);
+        background: var(--gds-sys-color-l3-neutral-01);
+      }
+
+      &:not(.disabled):hover {
+        background: color-mix(
+          in srgb,
+          transparent,
+          var(--gds-sys-color-state-neutral-05)
+        );
+      }
+
+      &[aria-selected='true']:not(.disabled):hover {
+        background: color-mix(
+          in srgb,
+          var(--gds-sys-color-l3-neutral-01),
+          var(--gds-sys-color-state-neutral-01)
+        );
+      }
+
+      &.today {
+        border-color: var(--gds-sys-color-border-strong);
+      }
+
+      &.disabled:not(.week-number) {
+        background: var(--gds-sys-color-l3-disabled-01);
+        color: var(--gds-sys-color-content-disabled-01);
+        cursor: not-allowed;
+      }
+
+      &.disabled.week-number {
+        color: var(--gds-sys-color-content-neutral-02);
+        cursor: default;
+      }
+
+      &[aria-selected='false']:active:not(.disabled) {
+        background: color-mix(
+          in srgb,
+          transparent,
+          var(--gds-sys-color-state-neutral-06)
+        );
+      }
+
+      &[aria-selected='true']:active:not(.disabled) {
+        background: color-mix(
+          in srgb,
+          var(--gds-sys-color-l3-neutral-01),
+          var(--gds-sys-color-state-neutral-02)
+        );
+      }
+
+      &:focus-visible {
+        outline-color: var(--gds-sys-color-content-neutral-01);
+        outline-width: var(--gds-sys-space-4xs);
+      }
+
+      &.small {
+        width: var(--gds-sys-space-xl);
+        height: var(--gds-sys-space-xl);
+        font: var(--gds-sys-text-body-book-s);
+        line-height: var(--gds-sys-text-line-height-detail-s);
 
         &.short {
-          width: var(--gds-sys-space-4xl);
+          width: var(--gds-sys-space-2xl);
         }
 
         &.wide {
-          width: var(--gds-sys-space-5xl);
+          width: var(--gds-sys-space-3xl);
         }
 
         &.long {
-          width: var(--gds-sys-space-7xl);
-        }
-
-        &:not(.disabled):hover,
-        &[aria-selected='true'] {
-          cursor: pointer;
-        }
-
-        &[aria-selected='true'] {
-          color: var(--gds-sys-color-content-neutral-03);
-          background: var(--gds-sys-color-l3-neutral-01);
-        }
-
-        &:not(.disabled):hover {
-          background: color-mix(
-            in srgb,
-            transparent,
-            var(--gds-sys-color-state-neutral-05)
-          );
-        }
-
-        &[aria-selected='true']:not(.disabled):hover {
-          background: color-mix(
-            in srgb,
-            var(--gds-sys-color-l3-neutral-01),
-            var(--gds-sys-color-state-neutral-01)
-          );
-        }
-
-        &.today {
-          border-color: var(--gds-sys-color-border-strong);
-        }
-
-        &.disabled:not(.week-number) {
-          color: var(--gds-sys-color-content-disabled-01);
-          background: var(--gds-sys-color-l3-disabled-01);
-          cursor: not-allowed;
-        }
-
-        &.disabled.week-number {
-          color: var(--gds-sys-color-content-neutral-02);
-          cursor: default;
-        }
-
-        &[aria-selected='false']:active:not(.disabled) {
-          background: color-mix(
-            in srgb,
-            transparent,
-            var(--gds-sys-color-state-neutral-06)
-          );
-        }
-
-        &[aria-selected='true']:active:not(.disabled) {
-          background: color-mix(
-            in srgb,
-            var(--gds-sys-color-l3-neutral-01),
-            var(--gds-sys-color-state-neutral-02)
-          );
-        }
-
-        &:focus-visible {
-          outline-color: var(--gds-sys-color-content-neutral-01);
-          outline-width: var(--gds-sys-space-4xs);
-        }
-
-        &.small {
-          width: var(--gds-sys-space-xl);
-          height: var(--gds-sys-space-xl);
-          font: var(--gds-sys-text-body-book-s);
-          line-height: var(--gds-sys-text-line-height-detail-s);
-
-          &.short {
-            width: var(--gds-sys-space-2xl);
-          }
-
-          &.wide {
-            width: var(--gds-sys-space-3xl);
-          }
-
-          &.long {
-            width: var(--gds-sys-space-6xl);
-          }
+          width: var(--gds-sys-space-6xl);
         }
       }
 
