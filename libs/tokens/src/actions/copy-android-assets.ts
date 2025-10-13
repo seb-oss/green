eebd8b8f7c15c42f7626ecd20111b96286e2ec37
Vilhelm Sjölander <vilhelm.sjolander@seb.se>
2025-10-13 11:57:52 +0200

    renaming android font files to lower snake case

--- libs/tokens/src/actions/copy-android-assets.ts
+++ libs/tokens/src/actions/copy-android-assets.ts
@@ -3,20 +3,36 @@ import fs from 'fs-extra'
 
 export default {
   name: 'copy-android-assets',
-  do: function (dictionary, config) {
+  do: function (dictionary: unknown, config: { buildPath: string }) {
     console.log('Copying android assets')
 
     const sourceDir = path.resolve('./libs/fonts/fonts/SEBSansSerif')
     const destDir = path.join(config.buildPath, 'fonts')
 
     fs.ensureDirSync(destDir)
+    const toSnakeCase = (str: string) =>
+      str
+        // space or hyphen to underscore
+        .replace(/[\s-]+/g, '_')
+        // camelCase to snake_case
+        .replace(/([a-z0-9])([A-Z])/g, '$1_$2')
+        // non-word to underscore
+        .replace(/[^A-Za-z0-9_]+/g, '_')
+        // collapse multiple underscores
+        .replace(/_+/g, '_')
+        // trim leading/trailing underscores
+        .replace(/^_+|_+$/g, '')
+        .toLowerCase()
+
     fs.readdirSync(sourceDir)
       .filter((file) => file.endsWith('.ttf'))
       .forEach((file) => {
-        fs.copySync(path.join(sourceDir, file), path.join(destDir, file))
+        const parsed = path.parse(file)
+        const snakeName = `${toSnakeCase(parsed.name)}${parsed.ext.toLowerCase()}`
+        fs.copySync(path.join(sourceDir, file), path.join(destDir, snakeName))
       })
   },
-  undo: function (config) {
+  undo: function (config: { buildPath: string }) {
     console.log('Cleaning assets directory')
     fs.rmSync(path.join(config.buildPath, 'fonts'), {
       recursive: true,
