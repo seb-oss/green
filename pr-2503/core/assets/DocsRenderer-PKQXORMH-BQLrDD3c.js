d721818c915966ca5af064d36fad261e2560aedb
astrit <astrit@users.noreply.github.com>
2025-10-31 11:08:46 +0000

    Deploying to gh-pages from @ seb-oss/green@69a9d109b58cc337e410581249999ee4fecb9af2 🚀

--- pr-2503/core/assets/DocsRenderer-PKQXORMH-BQLrDD3c.js
+++
@@ -1,1 +0,0 @@
-import{D as d,d as n}from"./chunk-HLWAVYOI-BEgediIO.js";import"./iframe-Csrlvhv8.js";import"../sb-preview/runtime.js";import"./index-BB4SDMiW.js";import"./_commonjsHelpers-Cpj98o6Y.js";import"./index-D-8MO0q_.js";import"./index-DxKRhftL.js";import"./index-DrFu-skq.js";export{d as DocsRenderer,n as defaultComponents};
--- pr-2503/core/assets/query-p8xgzTDt.js
+++
@@ -1,9 +0,0 @@
-/**
- * @license
- * Copyright 2017 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */const c=(r,t,e)=>(e.configurable=!0,e.enumerable=!0,Reflect.decorate&&typeof t!="object"&&Object.defineProperty(r,t,e),e);/**
- * @license
- * Copyright 2017 Google LLC
- * SPDX-License-Identifier: BSD-3-Clause
- */function f(r,t){return(e,n,l)=>{const u=a=>{var o;return((o=a.renderRoot)==null?void 0:o.querySelector(r))??null};return c(e,n,{get(){return u(this)}})}}export{f as a,c as e};
