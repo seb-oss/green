01db5e9fb4f99f1dc908a8a15d140b68c29daba7
Joacim Magnusson <joacim.magnusson@seb.se>
2025-11-24 13:14:20 +0100

    Fix React storybook build

--- libs/react/.storybook/main.ts
+++ libs/react/.storybook/main.ts
@@ -1,18 +1,25 @@
 // This file has been automatically migrated to valid ESM format by Storybook.
-import { createRequire } from "node:module";
-import { dirname, join } from "node:path";
+import { createRequire } from 'node:module'
+import { dirname, join } from 'node:path'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
 /* eslint-disable @nx/enforce-module-boundaries */
 import type { StorybookConfig } from '@storybook/react-vite'
 
 import defaultConfig from '../../../.storybook/main'
 
-const require = createRequire(import.meta.url);
+const require = createRequire(import.meta.url)
 
 const config: StorybookConfig = {
   ...defaultConfig,
   framework: {
-    name: getAbsolutePath("@storybook/react-vite"),
+    name: getAbsolutePath('@storybook/react-vite'),
     options: {},
+  },
+  async viteFinal(config) {
+    config.plugins = config.plugins ?? []
+    config.plugins.push(tsconfigPaths())
+    return config
   },
 }
 
@@ -21,5 +28,5 @@ config.addons?.push('@nx/react/plugins/storybook')
 export default config
 
 function getAbsolutePath(value: string): any {
-  return dirname(require.resolve(join(value, "package.json")));
+  return dirname(require.resolve(join(value, 'package.json')))
 }
