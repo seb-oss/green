71a614362cb42882eb07d90b3272f25c09a099e7
Joacim Magnusson <joacim.magnusson@seb.se>
2025-11-24 14:05:22 +0100

    Remove unused app `angular-handle-legacy`

--- apps/angular-handle-legacy/src/app/bootstrap/bootstrap.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-    selector: 'green-bootstrap',
-    templateUrl: './bootstrap.component.html',
-    styleUrls: ['./bootstrap.component.scss'],
-    standalone: false
-})
-export class BootstrapComponent {}
