eccbdefbebbf4661048bb91a1a78e5d3b69f9a73
Joacim Magnusson <joacim.magnusson@seb.se>
2025-12-04 15:09:17 +0100

    Update angular-lib-dev

--- apps/angular-lib-dev/src/app/form/form.module.ts
+++ apps/angular-lib-dev/src/app/form/form.module.ts
@@ -1,11 +1,8 @@
 import { CommonModule } from '@angular/common'
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core'
+import { NgModule } from '@angular/core'
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
-import { NggCoreFormsModule } from '@sebgroup/green-angular/src/lib/shared'
-import { SCOPE_RESOLVER } from '@sebgroup/green-angular/src/lib/shared/scope-resolver'
 import { GreenCoreNgModule } from '@sebgroup/green-core-ng'
-import { getScopedTagName } from '@sebgroup/green-core/scoping'
 import { FormRoutingModule } from './form-routing.module'
 import { FormComponent } from './form.component'
 
